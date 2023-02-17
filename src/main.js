dc0a81a0b906ff2e62f5b6d00c78dbeab739d321
beakerandjake <beakerandjake@users.noreply.github.com>
2023-02-17 13:30:17 -0700

    remove unused code

--- src/main.js
+++
@@ -1,3 +0,0 @@
-import { test } from './binarySearchTree.js';
-
-test();
--- src/output.js
+++
@@ -1,8 +0,0 @@
-/**
- * Wraps the function with a log group.
- */
-export const logGroup = (name, fn) => {
-  console.group(name);
-  fn();
-  console.groupEnd();
-};
