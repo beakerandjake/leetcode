fa2cc142a114f09b6bed7a1becdcf37a0bb9472f
beakerandjake <beakerandjake@users.noreply.github.com>
2023-04-15 09:48:48 -0400

    insertionSort

---
+++ tests/insertionSort.test.js
@@ -0,0 +1,24 @@
+import { insertionSort } from '../src/insertionSort.js';
+
+test.each([
+  [[], []],
+  [[1], [1]],
+  [
+    [1, 2],
+    [1, 2],
+  ],
+  [
+    [3, 2, 1],
+    [1, 2, 3],
+  ],
+  [
+    [66, 33, 22, 55],
+    [22, 33, 55, 66],
+  ],
+  [
+    [97, 2, 33, 48, 12, 30, 291],
+    [2, 12, 30, 33, 48, 97, 291],
+  ],
+])('%s returns %s', (input, expected) => {
+  expect(insertionSort(input)).toEqual(expected);
+});
