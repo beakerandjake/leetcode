955349eb1e7ee6db441118aed9c1714f12e4a4d0
beakerandjake <beakerandjake@users.noreply.github.com>
2023-07-10 08:21:54 -0700

    is unique

--- src/interview/ch01/isUnique.js
+++ src/interview/ch01/isUnique.js
@@ -1,4 +1,6 @@
-const hashTable = (str) => {
+const uniqBySet = (str) => new Set(str).size === str.length;
+
+const uniqByHashTable = (str) => {
   const chars = {};
   return [...str].every((char) => {
     if (char in chars) {
@@ -9,6 +11,50 @@ const hashTable = (str) => {
   });
 };
 
-const set = (str) => new Set(str).size === str.length;
+const uniqByBitVector = (str) => {
+  const lookup = Array(256).fill(0);
+  return [...str].every((char) => {
+    const index = char.charCodeAt();
 
-export const isUnique = hashTable;
+    if (lookup[index]) {
+      return false;
+    }
+
+    lookup[index] = 1;
+    return true;
+  });
+};
+
+const uniqByBruteForce = (str) => {
+  for (let index = 0; index < str.length; index++) {
+    const char = str[index];
+    for (let next = 0; next < str.length; next++) {
+      if (index !== next && char === str[next]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const uniqBySort = (str) => {
+  const sorted = [...str].sort();
+  for (let index = 1; index < str.length; index++) {
+    if (sorted[index] === sorted[index - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export const isUnique = (str) => {
+  if (!str || str.length === 1) {
+    return true;
+  }
+
+  if (str.length > 256) {
+    return false;
+  }
+
+  return uniqBySort(str);
+};
--- tests/interview/ch01/isUnique.test.js
+++ tests/interview/ch01/isUnique.test.js
@@ -3,6 +3,10 @@ import { isUnique } from '../../../src/interview/ch01/isUnique.js';
 describe('isUnique()', () => {
   test.each([
     ['a', true],
+    ['aa', false],
+    ['ab', true],
+    ['abc', true],
+    ['abb', false],
     ['Wjk0eLRs8R', false],
     ['2AwlJjazxf', true],
     ['5bBYStnGRJ', true],
