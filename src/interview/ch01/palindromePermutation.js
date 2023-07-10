8e7676791d74a122097bb938061785df412d55e6
beakerandjake <beakerandjake@users.noreply.github.com>
2023-07-10 12:19:10 -0700

    clean up

--- src/interview/ch01/palindromePermutation.js
+++ src/interview/ch01/palindromePermutation.js
@@ -1,14 +1,23 @@
 const characterCounts = (str) =>
   [...str].reduce((acc, char) => {
-    acc[char.charCodeAt()] += 1;
+    if (!acc[char]) {
+      acc[char] = 1;
+    } else {
+      acc[char] += 1;
+    }
     return acc;
-  }, Array(256).fill(0));
+  }, {});
 
 const simple = (str) => {
-  const counts = characterCounts(str).filter(Boolean);
-  const oddCount = counts.filter((count) => count % 2 !== 0).length;
-  const evenCount = counts.filter((count) => count % 2 === 0).length;
-  return (oddCount === 0 && evenCount > 0) || (oddCount === 1 && evenCount > 0);
+  const counts = characterCounts(str);
+  const [even, odd] = Object.keys(counts).reduce(
+    (acc, char) => {
+      acc[counts[char] % 2 === 0 ? 0 : 1] += 1;
+      return acc;
+    },
+    [0, 0]
+  );
+  return (odd === 0 && even > 0) || (odd === 1 && even > 0);
 };
 
 export const palindromePermutation = (str) => {
