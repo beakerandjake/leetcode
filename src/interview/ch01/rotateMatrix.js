abf21775967d98584524c18523cba7cc9f6378c1
beakerandjake <beakerandjake@users.noreply.github.com>
2023-07-12 16:45:03 -0700

    swap via pivoting layers

--- src/interview/ch01/rotateMatrix.js
+++ src/interview/ch01/rotateMatrix.js
@@ -1,3 +1,53 @@
+/* eslint-disable no-param-reassign */
+
+const reverse = (array) => {
+  const mid = Math.floor(array.length / 2);
+  let end = array.length - 1;
+  for (let index = 0; index < mid; index++, end--) {
+    const tmp = array[index];
+    array[index] = array[end];
+    array[end] = tmp;
+  }
+};
+
+const getValue = (matrix, [y, x]) => matrix[y][x];
+
+const setValue = (matrix, [y, x], value) => {
+  matrix[y][x] = value;
+};
+
+const swapPoints = (matrix, sourceIndex, destIndex) => {
+  const sourceValue = getValue(matrix, sourceIndex);
+  setValue(matrix, sourceIndex, getValue(matrix, destIndex));
+  setValue(matrix, destIndex, sourceValue);
+};
+
+const pivot = (matrix, pY, pX) => {
+  let numSwaps = pX;
+  let sX = pX - 1;
+  let sY = pY + 1;
+  while (numSwaps--) {
+    swapPoints(matrix, [pY, sX--], [sY++, pX]);
+  }
+};
+
+const inPlace = (matrix) => {
+  if (matrix.length <= 1) {
+    return matrix;
+  }
+
+  let pY = 0;
+  let pX = matrix.length;
+
+  while (--pX) {
+    pivot(matrix, pY, pX);
+    pY++;
+  }
+
+  reverse(matrix);
+  return matrix;
+};
+
 const withCopy = (matrix) => {
   const copy = matrix.map((row) => [...row]);
   for (let y = 0; y < matrix.length; y++) {
@@ -9,4 +59,4 @@ const withCopy = (matrix) => {
   return copy;
 };
 
-export const rotateMatrix = (matrix) => withCopy(matrix);
+export const rotateMatrix = (matrix) => inPlace(matrix);
