799e13ec8f5f01300d359c60f708a8c2fac4dba8
beakerandjake <beakerandjake@users.noreply.github.com>
2023-08-31 13:04:40 -0700

    wip knapsack test

--- tests/dataStructures/knapsack.test.js
+++ tests/dataStructures/knapsack.test.js
@@ -2,8 +2,8 @@ import { knapsack } from '../../src/dataStructures/knapsack.js';
 
 describe('knapsack()', () => {
   test.each([null, undefined, {}, 0, 'ASDF', Promise.resolve(true), () => {}])(
-    'throws TypeError if items is: %s',
-    (items) => expect(() => knapsack(items)).toThrow(TypeError)
+    'items: %s, capacity: 0, throws TypeError',
+    (items) => expect(() => knapsack(items, 0)).toThrow(TypeError)
   );
 
   test.each([
@@ -16,26 +16,28 @@ describe('knapsack()', () => {
     () => {},
     NaN,
     Infinity,
-  ])('throws TypeError if capacity is: %s', (capacity) =>
+  ])('item count: 0, capacity: %s, throws TypeError', (capacity) =>
     expect(() => knapsack([], capacity)).toThrow(TypeError)
   );
 
-  test.each([
+  [
     [[], 10, 0],
     [[], 0, 0],
     [[], -10, 0],
     [[{ value: 10, weight: 1 }], 0, 0],
-    // [
-    //   [
-    //     { name: 'stereo', value: 3000, weight: 4 },
-    //     { name: 'laptop', value: 2000, weight: 3 },
-    //     { name: 'guitar', value: 1500, weight: 1 },
-    //   ],
-    //   4,
-    //   3500,
-    // ],
-  ])('knapsack(%s, %s) = %s', (items, capacity, expected) => {
-    const result = knapsack(items, capacity);
-    expect(result).toBe(expected);
+    [
+      [
+        { name: 'stereo', value: 3000, weight: 4 },
+        { name: 'laptop', value: 2000, weight: 3 },
+        { name: 'guitar', value: 1500, weight: 1 },
+      ],
+      4,
+      3500,
+    ],
+  ].forEach(([items, capacity, expected]) => {
+    test(`item count: ${items.length}, capacity: ${capacity} returns: ${expected}`, () => {
+      const result = knapsack(items, capacity);
+      expect(result).toBe(expected);
+    });
   });
 });
