550a3b2e1a5eadbd295fb129db710b1cccf58e70
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-13 15:57:29 -0700

    in place

--- src/leetcode/mergeSortedArray_88.js
+++ src/leetcode/mergeSortedArray_88.js
@@ -10,23 +10,17 @@
  * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
  * and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
  */
-
 export const merge = (nums1, num1Length, nums2, num2Length) => {
-  const toReturn = [...nums1];
-  const outputLength = num1Length + num2Length;
-  let num1Index = 0;
-  let num2Index = 0;
-  for (let index = 0; index < outputLength; index++) {
-    if (num1Index >= num1Length) {
-      toReturn[index] = nums2[num2Index++];
-    } else if (num2Index >= num2Length) {
-      toReturn[index] = nums1[num1Index++];
+  let num1Index = num1Length - 1;
+  let num2Index = num2Length - 1;
+  for (let i = num1Length + num2Length; i--; ) {
+    if (num1Index < 0) {
+      nums1[i] = nums2[num2Index--];
+    } else if (num2Index < 0) {
+      nums1[i] = nums1[num1Index--];
     } else {
-      toReturn[index] =
-        nums1[num1Index] <= nums2[num2Index] ? nums1[num1Index++] : nums2[num2Index++];
+      nums1[i] =
+        nums1[num1Index] >= nums2[num2Index] ? nums1[num1Index--] : nums2[num2Index--];
     }
   }
-  toReturn.forEach((x, i) => {
-    nums1[i] = x;
-  });
-};
+};;
