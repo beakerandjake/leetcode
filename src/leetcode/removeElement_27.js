4ffdfd5dc67807256dfcced1710a37eb39b45bb7
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-13 18:31:53 -0700

    clean up logic

--- src/leetcode/removeElement_27.js
+++ src/leetcode/removeElement_27.js
@@ -18,43 +18,11 @@
  * @return {number}
  */
 export const removeElement = (nums, val) => {
-  if (nums.length === 0) {
-    return 0;
+  let slowIndex = 0;
+  for (let index = 0; index < nums.length; index++) {
+    if (nums[index] !== val) {
+      nums[slowIndex++] = nums[index];
+    }
   }
-
-  let toReturn = 0;
-  let leftIndex = 0;
-  let rightIndex = nums.length - 1;
-
-  while (leftIndex <= rightIndex) {
-    if (leftIndex === rightIndex) {
-      if (nums[leftIndex] !== val) {
-        toReturn++;
-        break;
-      }
-    }
-
-    const left = nums[leftIndex];
-    // easy case left is not the vale, it's in the right spot, move on..
-    if (left !== val) {
-      toReturn++;
-      leftIndex++;
-      continue;
-    }
-
-    const right = nums[rightIndex];
-    // easy case left is the value and right is not the value, swap
-    if (right !== val) {
-      nums[leftIndex] = right;
-      leftIndex++;
-      rightIndex--;
-      toReturn++;
-      continue;
-    }
-
-    // hard case, left is the value and right is the value.
-    rightIndex--;
-  }
-
-  return toReturn;
+  return slowIndex;
 };
