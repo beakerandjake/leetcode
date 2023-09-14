eb88782bcb14d6c89953a02d5493ba8c20387aae
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-13 18:22:31 -0700

    first pass iterative

--- src/leetcode/removeElement_27.js
+++ src/leetcode/removeElement_27.js
@@ -18,9 +18,43 @@
  * @return {number}
  */
 export const removeElement = (nums, val) => {
-  const filtered = nums.filter((x) => x !== val);
-  filtered.forEach((x, i) => {
-    nums[i] = x;
-  });
-  return filtered.length;
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  let toReturn = 0;
+  let leftIndex = 0;
+  let rightIndex = nums.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    if (leftIndex === rightIndex) {
+      if (nums[leftIndex] !== val) {
+        toReturn++;
+        break;
+      }
+    }
+
+    const left = nums[leftIndex];
+    // easy case left is not the vale, it's in the right spot, move on..
+    if (left !== val) {
+      toReturn++;
+      leftIndex++;
+      continue;
+    }
+
+    const right = nums[rightIndex];
+    // easy case left is the value and right is not the value, swap
+    if (right !== val) {
+      nums[leftIndex] = right;
+      leftIndex++;
+      rightIndex--;
+      toReturn++;
+      continue;
+    }
+
+    // hard case, left is the value and right is the value.
+    rightIndex--;
+  }
+
+  return toReturn;
 };
