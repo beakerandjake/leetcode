674afa1d3ad4e95049dd4f2cd00da6d059c1ea59
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-14 16:53:50 -0700

    leetcode 80

--- src/leetcode/removeDuplicatesFromSortedArrayII_80.js
+++ src/leetcode/removeDuplicatesFromSortedArrayII_80.js
@@ -5,5 +5,24 @@
  * @return {number}
  */
 export const removeDuplicates = (array) => {
- 
+  if (array.length <= 2) {
+    return array.length;
+  }
+
+  let slowIndex = 1;
+  let fastIndex = 2;
+  while (fastIndex < array.length) {
+    const previous = array[slowIndex - 1];
+    const current = array[slowIndex];
+    const next = array[fastIndex];
+
+    if (previous === current && current === next) {
+      fastIndex++;
+      continue;
+    }
+
+    array[++slowIndex] = array[fastIndex++];
+  }
+
+  return slowIndex + 1;
 };
