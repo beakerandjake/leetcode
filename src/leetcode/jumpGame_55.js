8739d4cb90db7dd5bf008b26bdb63f9f189509d9
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-16 14:22:06 -0700

    iterative solution

--- src/leetcode/jumpGame_55.js
+++ src/leetcode/jumpGame_55.js
@@ -4,10 +4,59 @@
  * Return true if you can reach the last index, or false otherwise.
  */
 
+// const recursiveMemo = (nums) => {
+//   if (nums.length === 1) {
+//     return true;
+//   }
+
+//   const target = nums.length - 1;
+//   const memo = new Map();
+//   const test = (index) => {
+//     if (index === target) {
+//       return true;
+//     }
+
+//     if (memo.has(index)) {
+//       return memo.get(index);
+//     }
+
+//     let value = nums[index];
+//     while (value) {
+//       if (test(index + value--)) {
+//         memo.set(index, true);
+//         return true;
+//       }
+//     }
+//     memo.set(index, false);
+//     return false;
+//   };
+
+//   return test(0);
+// };
+
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
 export const canJump = (nums) => {
-  
+  if (nums.length === 1) {
+    return true;
+  }
+  let targetIndex = nums.length - 1;
+  let currentIndex = targetIndex - 1;
+  for (;;) {
+    if (targetIndex === 0) {
+      return true;
+    }
+    const canReachTarget = nums[currentIndex] >= targetIndex - currentIndex;
+    if (currentIndex === 0) {
+      return canReachTarget;
+    }
+    if (!canReachTarget) {
+      currentIndex--;
+    } else {
+      targetIndex--;
+      currentIndex = targetIndex - 1;
+    }
+  }
 };
--- tests/leetcode/jumpGame_55.test.js
+++ tests/leetcode/jumpGame_55.test.js
@@ -5,6 +5,9 @@ describe('55. Jump Game', () => {
   [
     [[2, 3, 1, 1, 4], true],
     [[3, 2, 1, 0, 4], false],
+    [[2, 0, 0], true],
+    [[1, 2, 0, 1], true],
+    [[0, 2, 3], false],
   ].forEach(([array, expected]) => {
     test(`${arrToStr(array)} -> ${expected}`, () => {
       const result = canJump(array);
