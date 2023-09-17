9e5e74fa9cf9d02457cb85717f899b1f1e93f38d
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-17 15:48:48 -0700

    simple iterative solution

--- tests/leetcode/candy_135.test.js
+++ tests/leetcode/candy_135.test.js
@@ -3,10 +3,10 @@ import { arrToStr } from '../util.js';
 
 describe('135. Candy', () => {
   [
-    // [[1, 0, 2], 5],
-    // [[1, 2, 2], 4],
-    // [[6, 3, 2], 6],
-    // [[10], 1],
+    [[1, 0, 2], 5],
+    [[1, 2, 2], 4],
+    [[6, 3, 2], 6],
+    [[10], 1],
     [[1, 6, 10, 8, 7, 3, 2], 18],
   ].forEach(([ratings, expected]) => {
     test(`${arrToStr(ratings)} -> ${expected}`, () => {
