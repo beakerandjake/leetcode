e591d04be9ae57087c12c258e4cd049e217355de
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-17 12:16:42 -0700

    simple iterative

--- src/leetcode/gasStation_134.js
+++ src/leetcode/gasStation_134.js
@@ -11,4 +11,30 @@
  * @param {number[]} cost
  * @return {number}
  */
-export const canCompleteCircuit = (gas, cost) => {};
+export const canCompleteCircuit = (gas, cost) => {
+  const test = (start) => {
+    let current = start;
+    let tank = 0;
+    for (let i = 0; i < gas.length; i++) {
+      const newTank = tank + gas[current];
+      const travelCost = cost[current];
+      if (travelCost > newTank) {
+        return -1;
+      }
+      current = (current + 1) % gas.length;
+      tank = newTank - travelCost;
+    }
+    return start;
+  };
+
+  for (let i = 0; i < gas.length; i++) {
+    // can't start at this gas station if travel cost is more than initial fill up
+    if (gas[i] < cost[i] || (gas[i] === 0 && cost[i] === 0)) {
+      continue;
+    }
+    if (test(i) === i) {
+      return i;
+    }
+  }
+  return -1;
+};
