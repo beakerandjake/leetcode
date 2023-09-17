ce3a32c5b56d87b35f7ddb456e80a02851244fad
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-16 17:32:42 -0700

    first pass leetcode 274

--- src/leetcode/hIndex_274.js
+++ src/leetcode/hIndex_274.js
@@ -4,10 +4,25 @@
  * The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
  */
 
+const simple = (citations) => {
+  if (citations.length === 1) {
+    return citations[0] ? 1 : 0;
+  }
+  const countAtLeast = (num) => citations.filter((x) => x >= num).length;
+  let toReturn = 0;
+  for (let h = 1; h <= citations.length; h++) {
+    const count = countAtLeast(h);
+    if (h > count) {
+      break;
+    }
+    toReturn = h;
+  }
+  return toReturn;
+};
+
+
 /**
  * @param {number[]} citations
  * @return {number}
  */
-export const hIndex = (citations) => {
-  
-};
+export const hIndex = simple;
--- tests/leetcode/hIndex_274.test.js
+++ tests/leetcode/hIndex_274.test.js
@@ -5,6 +5,8 @@ describe('55. Jump Game', () => {
   [
     [[3, 0, 6, 1, 5], 3],
     [[1, 3, 1], 1],
+    [[100], 1],
+    [[11, 15], 2],
   ].forEach(([array, expected]) => {
     test(`${arrToStr(array)} -> ${expected}`, () => {
       const result = hIndex(array);
