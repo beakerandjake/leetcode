d1e230dfc3fef36a2cbc500e1bf5b1dc97122f07
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-18 11:46:37 -0700

    first pass simple solution

--- src/leetcode/indexOfFirstOccurenceInString_28.js
+++ src/leetcode/indexOfFirstOccurenceInString_28.js
@@ -1,8 +1,26 @@
 /**
+ * Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ */
+
+/**
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
 export const strStr = (haystack, needle) => {
+  if (needle.length > haystack.length) {
+    return -1;
+  }
+  if (needle.length === haystack.length) {
+    return needle === haystack ? 0 : -1;
+  }
 
+  let remaining = haystack;
+  while (remaining.length >= needle.length) {
+    if (remaining.startsWith(needle)) {
+      return haystack.length - remaining.length;
+    }
+    remaining = remaining.slice(1);
+  }
+  return -1;
 };
--- tests/leetcode/indexOfFirstOccurenceInString_28.test.js
+++ tests/leetcode/indexOfFirstOccurenceInString_28.test.js
@@ -4,6 +4,11 @@ describe('28. Find the Index of the First Occurrence in a String', () => {
   [
     ['sadbutsad', 'sad', 0],
     ['leetcode', 'leeto', -1],
+    ['cat', 'catty', -1],
+    ['cat', 'cat', 0],
+    ['hello', 'll', 2],
+    ['abc', 'c', 2],
+    ['mississippi', 'issi', 1],
   ].forEach(([needle, haystack, expected]) => {
     test(`needle: ${needle}, haystack: ${haystack} -> ${expected}`, () => {
       const result = strStr(needle, haystack);
