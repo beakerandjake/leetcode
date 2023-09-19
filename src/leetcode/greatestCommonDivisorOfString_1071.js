bce78a1e23f7288f9d80c64842486738be32b021
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-19 13:25:52 -0700

    extract to function

--- src/leetcode/greatestCommonDivisorOfString_1071.js
+++ src/leetcode/greatestCommonDivisorOfString_1071.js
@@ -3,36 +3,31 @@
  * Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
  */
 
-const getLongestCommonPrefix = (a, b, i) => {
-  if (i >= a.length || i >= b.length) {
-    return i;
-  }
-  return a[i] === b[i] ? getLongestCommonPrefix(a, b, i + 1) : -1;
-};
+const firstPass = (a, b) => {
+  const getLongestCommonPrefix = (i = 0) => {
+    if (i >= a.length || i >= b.length) {
+      return i;
+    }
+    return a[i] === b[i] ? getLongestCommonPrefix(i + 1) : -1;
+  };
 
-const prefixRepeats = (string, prefixEndIndex) => {
-  if (string.length % prefixEndIndex !== 0) {
-    return false;
-  }
+  const prefixRepeats = (string, prefixEndIndex) => {
+    if (string.length % prefixEndIndex !== 0) {
+      return false;
+    }
 
-  for (let p = 0; p < prefixEndIndex; p++) {
-    const prefixChar = string[p];
-    for (let s = prefixEndIndex + p; s < string.length; s += prefixEndIndex) {
-      if (string[s] !== prefixChar) {
-        return false;
+    for (let p = 0; p < prefixEndIndex; p++) {
+      const prefixChar = string[p];
+      for (let s = prefixEndIndex + p; s < string.length; s += prefixEndIndex) {
+        if (string[s] !== prefixChar) {
+          return false;
+        }
       }
     }
-  }
-  return true;
-};
+    return true;
+  };
 
-/**
- * @param {string} a
- * @param {string} b
- * @return {string}
- */
-export const gcdOfStrings = (a, b) => {
-  let prefixEnd = getLongestCommonPrefix(a, b, 0);
+  let prefixEnd = getLongestCommonPrefix();
 
   // don't share a common prefix, can't be divisible.
   if (prefixEnd === -1) {
@@ -48,3 +43,10 @@ export const gcdOfStrings = (a, b) => {
 
   return '';
 };
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+export const gcdOfStrings = firstPass;
