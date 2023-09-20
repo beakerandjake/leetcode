c6a0b4b33ea196ecfadeb77fe8ca7dcbf6c9eddc
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-20 10:35:15 -0700

    scaffold leetcode 334

---
+++ src/leetcode/increasingTripletSubsequence_334.js
@@ -0,0 +1,11 @@
+/**
+ * Given an integer array nums, return true if
+ * there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k].
+ * If no such indices exists, return false.
+ */
+
+/**
+ * @param {number[]} nums
+* @return {boolean}
+ */
+export const increasingTriplet = (nums) => {};
---
+++ tests/leetcode/increasingTripletSubsequence_334.test.js
@@ -0,0 +1,18 @@
+import { increasingTriplet } from '../../src/leetcode/increasingTripletSubsequence_334.js';
+import { arrToStr } from '../util.js';
+
+describe('334. Increasing Triplet Subsequence', () => {
+  [
+    [[1, 2, 3, 4, 5], true],
+    [[5, 4, 3, 2, 1], false],
+    [[2, 1, 5, 0, 4, 6], true],
+    [[10, 9, 5, 4, 3, 2, 6, 3, 2, 1, 7], true],
+    [[9, 3, 20, 14, 9, 3], false],
+    [[2, 3, 20, 14, 9, 3], true],
+  ].forEach(([array, expected]) => {
+    test(`${arrToStr(array)} -> ${expected}`, () => {
+      const result = increasingTriplet(array);
+      expect(result).toEqual(expected);
+    });
+  });
+});
