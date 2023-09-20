131e99e8c123933fd990812448de0fa4f34cf721
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-20 15:37:32 -0700

    better solution

--- src/leetcode/increasingTripletSubsequence_334.js
+++ src/leetcode/increasingTripletSubsequence_334.js
@@ -4,21 +4,37 @@
  * If no such indices exists, return false.
  */
 
-const smallestSeen = (nums) => {
-  const toReturn = [nums[0]];
-  for (let i = 1; i < nums.length; i++) {
-    toReturn[i] = nums[i] < toReturn[i - 1] ? nums[i] : toReturn[i - 1];
-  }
-  return toReturn;
-};
+const simple = (() => {
+  const smallestSeen = (nums) => {
+    const toReturn = [nums[0]];
+    for (let i = 1; i < nums.length; i++) {
+      toReturn[i] = nums[i] < toReturn[i - 1] ? nums[i] : toReturn[i - 1];
+    }
+    return toReturn;
+  };
 
-const largestSeen = (nums) => {
-  const toReturn = [...nums];
-  for (let i = nums.length - 2; i >= 0; i--) {
-    toReturn[i] = nums[i] > toReturn[i + 1] ? nums[i] : toReturn[i + 1];
-  }
-  return toReturn;
-};
+  const largestSeen = (nums) => {
+    const toReturn = [...nums];
+    for (let i = nums.length - 2; i >= 0; i--) {
+      toReturn[i] = nums[i] > toReturn[i + 1] ? nums[i] : toReturn[i + 1];
+    }
+    return toReturn;
+  };
+
+  return (nums) => {
+    if (nums.length < 3) {
+      return false;
+    }
+    const sm = smallestSeen(nums);
+    const lg = largestSeen(nums);
+    for (let i = 1; i < nums.length - 1; i++) {
+      if (sm[i] < nums[i] && lg[i] > nums[i]) {
+        return true;
+      }
+    }
+    return false;
+  };
+})();
 
 /**
  * @param {number[]} nums
@@ -28,12 +44,19 @@ export const increasingTriplet = (nums) => {
   if (nums.length < 3) {
     return false;
   }
-  const sm = smallestSeen(nums);
-  const lg = largestSeen(nums);
-  for (let i = 1; i < nums.length - 1; i++) {
-    if (sm[i] < nums[i] && lg[i] > nums[i]) {
+
+  let i = Number.MAX_SAFE_INTEGER;
+  let j = Number.MAX_SAFE_INTEGER;
+
+  for (let k = 0; k < nums.length; k++) {
+    if (nums[k] <= i) {
+      i = nums[k];
+    } else if (nums[k] <= j) {
+      j = nums[k];
+    } else {
       return true;
     }
   }
+
   return false;
 };
