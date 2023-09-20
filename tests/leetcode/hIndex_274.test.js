d3802c32cedea876e378bf38fe66e7b6a4a0523d
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-20 10:36:28 -0700

    fix test name

--- tests/leetcode/hIndex_274.test.js
+++ tests/leetcode/hIndex_274.test.js
@@ -1,7 +1,7 @@
 import { hIndex } from '../../src/leetcode/hIndex_274.js';
 import { arrToStr } from '../util.js';
 
-describe('55. Jump Game', () => {
+describe('274. H-Index', () => {
   [
     [[3, 0, 6, 1, 5], 3],
     [[1, 3, 1], 1],
