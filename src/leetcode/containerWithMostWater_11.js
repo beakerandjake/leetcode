0791691afc1b44d9758e3f12562b89cd23a6a391
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-21 12:56:17 -0700

    dynamic programming solution

---
+++ src/leetcode/containerWithMostWater_11.js
@@ -0,0 +1,29 @@
+/**
+ * You are given an integer array height of length n.
+ * There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ * Return the maximum amount of water a container can store.
+ * Notice that you may not slant the container.
+ */
+
+const area = (heights, from, to) =>
+  to > from ? (to - from) * Math.min(heights[to], heights[from]) : 0;
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+export const maxArea = (heights) => {
+  const count = heights.length + 1;
+  const history = [...Array(count)].map(() => Array(count).fill(0));
+  for (let i = 1; i < count; i++) {
+    for (let j = 1; j < count; j++) {
+      history[i][j] = Math.max(
+        area(heights, i - 1, j - 1),
+        history[i - 1][j],
+        history[i][j - 1]
+      );
+    }
+  }
+  return history[count - 1][count - 1];
+};
---
+++ tests/leetcode/containerWithMostWater_11.test.js
@@ -0,0 +1,16 @@
+import { maxArea } from '../../src/leetcode/containerWithMostWater_11.js';
+import { arrToStr } from '../util.js';
+
+describe('11. Container With Most Water', () => {
+  [
+    // [[1, 8, 6, 2, 5, 4, 8, 3, 7], 49],
+    // [[1, 2], 1],
+    // [[3, 1, 3, 4], 9],
+    [[2, 3, 4, 5, 18, 17, 6], 17],
+  ].forEach(([input, expected]) => {
+    test(`${arrToStr(input)} -> ${expected}`, () => {
+      const result = maxArea(input);
+      expect(result).toEqual(expected);
+    });
+  });
+});
