ff8af124630ee49a5db7a9514b11d0e036656622
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-21 15:44:34 -0700

    sorted solution

--- src/leetcode/maxNumberOfKSumPairs.js
+++ src/leetcode/maxNumberOfKSumPairs.js
@@ -24,11 +24,11 @@
 //   return toReturn;
 // };
 
-// const frequencyMap = (nums) =>
-//   [...nums].reduce((acc, num) => {
-//     acc.set(num, acc.has(num) ? acc.get(num) + 1 : 1);
-//     return acc;
-//   }, new Map());
+const frequencyMap = (nums) =>
+  nums.reduce((acc, num) => {
+    acc.set(num, acc.has(num) ? acc.get(num) + 1 : 1);
+    return acc;
+  }, new Map());
 
 /**
  * @param {number[]} nums
@@ -36,21 +36,25 @@
  * @return {number}
  */
 export const maxOperations = (nums, k) => {
-  const sorted = [...nums].sort((a, b) => a - b);
-  let left = 0;
-  let right = nums.length - 1;
   let toReturn = 0;
-  while (left < right) {
-    const desired = k - sorted[left];
-    if (sorted[right] === desired) {
-      right--;
-      left++;
-      toReturn++;
-    } else if (sorted[right] < desired) {
-      left++;
-    } else {
-      right--;
+  const remainingNums = frequencyMap(nums);
+  for (let i = 0; i < nums.length; i++) {
+    // skip if num has been consumed.
+    if (!remainingNums.get(nums[i])) {
+      continue;
     }
+    const desired = k - nums[i];
+    const remainingCount = remainingNums.get(desired);
+
+    // not possible to make k with this number.
+    if (!remainingCount || (remainingCount === 1 && desired === nums[i])) {
+      continue;
+    }
+
+    // make k with this number and remove it and desired from set.
+    remainingNums.set(nums[i], remainingNums.get(nums[i]) - 1);
+    remainingNums.set(desired, remainingNums.get(desired) - 1);
+    toReturn++;
   }
   return toReturn;
 };
