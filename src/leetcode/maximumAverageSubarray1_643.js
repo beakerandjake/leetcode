593870730fa10c373a04ec2f3550806515b860e2
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-21 16:38:54 -0700

    simple window average

--- src/leetcode/maximumAverageSubarray1_643.js
+++ src/leetcode/maximumAverageSubarray1_643.js
@@ -4,11 +4,30 @@
  * and return this value. Any answer with a calculation error less than 10-5 will be accepted.
  */
 
+const sum = (nums, start, end) => {
+  let total = 0;
+  for (let i = start; i < end; i++) {
+    total += nums[i];
+  }
+  return total;
+};
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
 export const findMaxAverage = (nums, k) => {
-  
+  let runningSum = sum(nums, 0, k);
+  let toReturn = runningSum / k;
+  const end = nums.length - k;
+  for (let i = 1; i <= end; i++) {
+    runningSum -= nums[i - 1];
+    runningSum += nums[i + k - 1];
+    const windowAverage = runningSum / k;
+    if (windowAverage > toReturn) {
+      toReturn = windowAverage;
+    }
+  }
+  return toReturn;
 };
--- tests/leetcode/maximumAverageSubarray1_643.test.js
+++ tests/leetcode/maximumAverageSubarray1_643.test.js
@@ -5,10 +5,13 @@ describe('643. Maximum Average Subarray I', () => {
   [
     [[1, 12, -5, -6, 50, 3], 4, 12.75],
     [[5], 1, 5.0],
+    [[7, 4, 5, 8, 8, 3, 9, 8, 7, 6], 7, 7.0],
+    [[0, 0, 0, 0, 0], 5, 0],
+    [[-1], 1, -1],
   ].forEach(([arr, k, expected]) => {
     test(`${arrToStr(arr)}, ${k}-> ${expected}`, () => {
       const result = findMaxAverage(arr, k);
-      expect(result).toBe(expected);
+      expect(result).toBeCloseTo(expected);
     });
   });
 });
