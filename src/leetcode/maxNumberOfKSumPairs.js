b2ed1295b84f86553540cd25d4b4963329a0bfbe
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-21 15:24:22 -0700

    sorting solution

--- src/leetcode/maxNumberOfKSumPairs.js
+++ src/leetcode/maxNumberOfKSumPairs.js
@@ -4,9 +4,53 @@
  * Return the maximum number of operations you can perform on the array.
  */
 
+// const sorting = (nums, k) => {
+//   const sorted = [...nums].sort((a, b) => a - b);
+//   let left = 0;
+//   let right = nums.length - 1;
+//   let toReturn = 0;
+//   while (left < right) {
+//     const desired = k - sorted[left];
+//     if (sorted[right] === desired) {
+//       right--;
+//       left++;
+//       toReturn++;
+//     } else if (sorted[right] < desired) {
+//       left++;
+//     } else {
+//       right--;
+//     }
+//   }
+//   return toReturn;
+// };
+
+// const frequencyMap = (nums) =>
+//   [...nums].reduce((acc, num) => {
+//     acc.set(num, acc.has(num) ? acc.get(num) + 1 : 1);
+//     return acc;
+//   }, new Map());
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
-export const maxOperations = (nums, k) => {};
+export const maxOperations = (nums, k) => {
+  const sorted = [...nums].sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length - 1;
+  let toReturn = 0;
+  while (left < right) {
+    const desired = k - sorted[left];
+    if (sorted[right] === desired) {
+      right--;
+      left++;
+      toReturn++;
+    } else if (sorted[right] < desired) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return toReturn;
+};
