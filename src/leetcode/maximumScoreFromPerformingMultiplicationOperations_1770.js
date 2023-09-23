de2bfff63a5c21ee0aead44ad2ce3b80073140b2
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-22 19:36:48 -0700

    scaffold leetcode 1770

---
+++ src/leetcode/maximumScoreFromPerformingMultiplicationOperations_1770.js
@@ -0,0 +1,19 @@
+/**
+ * You are given two 0-indexed integer arrays nums and multipliers of size n and m respectively, where n >= m.
+ * You begin with a score of 0. You want to perform exactly m operations.
+ * On the ith operation (0-indexed) you will:
+ *
+ *     Choose one integer x from either the start or the end of the array nums.
+ *     Add multipliers[i] * x to your score.
+ *         Note that multipliers[0] corresponds to the first operation, multipliers[1] to the second operation, and so on.
+ *     Remove x from nums.
+ *
+ * Return the maximum score after performing m operations.
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number[]} multipliers
+ * @return {number}
+ */
+export const maximumScore = (nums, multipliers) => {};
