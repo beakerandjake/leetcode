413bbbe06341408e46ca1cb0d08bf04d18d27c4f
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-23 22:01:50 -0700

    first pass top down

--- src/leetcode/minimumDifficultyOfAJobSchedule_1335.js
+++ src/leetcode/minimumDifficultyOfAJobSchedule_1335.js
@@ -13,9 +13,55 @@
  * If you cannot find a schedule for the jobs return -1.
  */
 
+const getMaxLeft = (jobs) => {
+  const toReturn = [jobs[0]];
+  for (let i = 1; i < jobs.length; i++) {
+    toReturn.push(jobs[i] > toReturn[i - 1] ? jobs[i] : toReturn[i - 1]);
+  }
+  return toReturn;
+};
+
+const getMaxRight = (jobs) => {
+  const toReturn = [...jobs];
+  for (let i = jobs.length - 2; i >= 0; i--) {
+    toReturn[i] = jobs[i] > toReturn[i + 1] ? jobs[i] : toReturn[i + 1];
+  }
+  return toReturn;
+};
+
+const topDown = (jobs, days) => {
+  if (days > jobs.length) {
+    return -1;
+  }
+  const memo = new Map();
+  const maxLeft = getMaxLeft(jobs);
+  const maxRight = getMaxRight(jobs);
+  const recursive = (day, jobIndex) => {
+    const hash = `${day}_${jobIndex}`;
+    if (memo.has(hash)) {
+      return memo.get(hash);
+    }
+    if (day === days) {
+      memo.set(hash, Math.max(...jobs.slice(jobIndex)));
+    } else {
+      const jobLimit = jobs.length - (days - day);
+      let best = Number.MAX_SAFE_INTEGER;
+      for (let i = jobIndex; i < jobLimit; i++) {
+        const today = Math.max(...jobs.slice(jobIndex, i + 1));
+        best = Math.min(best, today + recursive(day + 1, i + 1));
+      }
+      memo.set(hash, best);
+    }
+
+    return memo.get(hash);
+  };
+
+  return recursive(1, 0);
+};
+
 /**
  * @param {number[]} jobDifficulty
  * @param {number} d
  * @return {number}
  */
-export const minDifficulty = (jobDifficulty, d) => {};
+export const minDifficulty = topDown;
