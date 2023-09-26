57cf40145ad6df09e2b8c5f62330290df442ac38
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-26 14:55:04 -0700

    simple iterative

--- tests/leetcode/724.test.js
+++ tests/leetcode/724.test.js
@@ -6,6 +6,7 @@ describe('724. Find Pivot Index', () => {
     [[1, 7, 3, 6, 5, 6], 3],
     [[1, 2, 3], -1],
     [[2, 1, -1], 0],
+    [[0, 0, 0], 0],
   ].forEach(([input, expected]) => {
     test(`${arrToStr(input)} -> ${expected}`, () => {
       const result = pivotIndex(input);
