6e0c77690b12217193d0ac94cbba99ee4748132c
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-25 18:00:35 -0700

    scaffold leetcode 1004

---
+++ src/leetcode/1004.js
@@ -0,0 +1,11 @@
+/**
+ * Given a binary array nums and an integer k,
+ * return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+export const longestOnes = (nums, k) => {};
