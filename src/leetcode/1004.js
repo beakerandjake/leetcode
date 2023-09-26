c82b01e3c56cae594c54b7e568d440330d0dfcc4
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-26 14:08:41 -0700

    comment out simple solution

--- src/leetcode/1004.js
+++ src/leetcode/1004.js
@@ -3,31 +3,33 @@
  * return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
  */
 
-const firstPass = (nums, k) => {
-  const consume = (start) => {
-    let streak = 0;
-    let flips = k;
-    let i = start;
+// const firstPass = (nums, k) => {
+//   const consume = (start) => {
+//     let streak = 0;
+//     let flips = k;
+//     let i = start;
 
-    while (i < nums.length) {
-      if (nums[i] === 0 && flips > 0) {
-        flips--;
-      } else if (nums[i] === 0 && flips === 0) {
-        break;
-      }
-      streak++;
-      i++;
-    }
+//     while (i < nums.length) {
+//       if (nums[i] === 0 && flips > 0) {
+//         flips--;
+//       } else if (nums[i] === 0 && flips === 0) {
+//         break;
+//       }
+//       streak++;
+//       i++;
+//     }
 
-    return streak;
-  };
+//     return streak;
+//   };
 
-  return nums.reduce((best, _, i) => Math.max(consume(i), best), 0);
-};
+//   return nums.reduce((best, _, i) => Math.max(consume(i), best), 0);
+// };
 
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
-export const longestOnes = firstPass;
+export const longestOnes = (nums, k) => {
+
+};
