c84117ac9b21f29ce709d62e6ac6b4fb2e0002ff
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-25 17:12:49 -0700

    simple first pass

--- src/leetcode/1456.js
+++ src/leetcode/1456.js
@@ -3,9 +3,33 @@
  * Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
  */
 
+const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+
+const vowelCount = (str, start, end) => {
+  let toReturn = 0;
+  for (let i = start; i < end; i++) {
+    if (vowels.has(str[i])) {
+      toReturn++;
+    }
+  }
+  return toReturn;
+};
+
+const getVowelCounts = (str, k) => {
+  const toReturn = [];
+  const maxStart = str.length - k;
+  for (let i = 0; i <= maxStart; i++) {
+    toReturn.push(vowelCount(str, i, i + k));
+  }
+  return toReturn;
+};
+
 /**
  * @param {string} str
  * @param {number} k
  * @return {number}
  */
-export const maxVowels = (str, k) => {};
+export const maxVowels = (str, k) => {
+  const vowelCounts = getVowelCounts(str, k);
+  return Math.max(...vowelCounts, 0);
+};
