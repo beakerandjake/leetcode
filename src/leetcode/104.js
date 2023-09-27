e70548a26f79d48f62c6cb9064186a951592d3aa
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-27 14:16:38 -0700

    add tail recursive and iterative solutions

--- src/leetcode/104.js
+++ src/leetcode/104.js
@@ -12,15 +12,46 @@
  * }
  */
 
-const depth = (node) => {
+const depthRecursive = (node) => {
   if (!node) {
     return 0;
   }
-  return Math.max(depth(node.left), depth(node.right)) + 1;
+  return Math.max(depthRecursive(node.left), depthRecursive(node.right)) + 1;
+};
+
+const depthTailRecursive = (node, depth) => {
+  if (!node) {
+    return depth;
+  }
+  return Math.max(
+    depthTailRecursive(node.left, depth + 1),
+    depthTailRecursive(node.right, depth + 1)
+  );
+};
+
+const depthIterative = (root) => {
+  if (!root) {
+    return 0;
+  }
+
+  let deepest = 0;
+  const queue = [{ node: root, depth: 1 }];
+  while (queue.length) {
+    const { node, depth } = queue.pop();
+    deepest = Math.max(deepest, depth);
+    if (node.left) {
+      queue.push({ node: node.left, depth: depth + 1 });
+    }
+    if (node.right) {
+      queue.push({ node: node.right, depth: depth + 1 });
+    }
+  }
+
+  return deepest;
 };
 
 /**
  * @param {TreeNode} root
  * @return {number}
  */
-export const maxDepth = (root) => depth(root);
+export const maxDepth = depthRecursive;
