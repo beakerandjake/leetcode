aef967b3b78f0eda311d01eb66ff2c288ce10016
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-27 15:09:10 -0700

    scaffold 700

---
+++ tests/leetcode/700.test.js
@@ -0,0 +1,15 @@
+import { searchBST } from '../../src/leetcode/700.js';
+import { arrToStr } from '../util.js';
+import { arrayToBinaryTree } from './util.js';
+
+describe('700. Search in a Binary Tree', () => {
+  [
+    [[4, 2, 7, 1, 3], 2, 2],
+    [[4, 2, 7, 1, 3], 5, null],
+  ].forEach(([nodes, search, expected]) => {
+    test(`${arrToStr(nodes)},${search} -> ${expected}`, () => {
+      const result = searchBST(arrayToBinaryTree(nodes), search);
+      expect(result).toBe(expected);
+    });
+  });
+});
