ba25579f17eb927a711b3c8a4b79dd2b95a79066
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-27 12:35:44 -0700

    add some solutions to 206

--- src/leetcode/206.js
+++ src/leetcode/206.js
@@ -9,8 +9,44 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
+const insertAfterTail = (head) => {
+  const getTail = (node) => {
+    let toReturn = node;
+    while (toReturn.next) {
+      toReturn = toReturn.next;
+    }
+    return toReturn;
+  };
+
+  if (!head?.next) {
+    return head;
+  }
+  const tail = getTail(head);
+  let currentHead = head;
+  while (currentHead && currentHead !== tail) {
+    const nextHead = currentHead.next;
+    currentHead.next = tail.next;
+    tail.next = currentHead;
+    currentHead = nextHead;
+  }
+  return tail;
+};
+
+const setToPreviousIterative = (head) => {
+  let previous = null;
+  let current = head;
+  while (current) {
+    const tempNext = current.next;
+    current.next = previous;
+    previous = current;
+    current = tempNext;
+  }
+  return previous;
+};
+
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-export const reverseList = (head) => {};
+export const reverseList = setToPreviousIterative;
--- tests/leetcode/206.test.js
+++ tests/leetcode/206.test.js
@@ -8,6 +8,11 @@ describe('206. Reverse Linked List', () => {
       [1, 2, 3, 4, 5],
       [5, 4, 3, 2, 1],
     ],
+    [
+      [1, 2],
+      [2, 1],
+    ],
+    [[], []],
   ].forEach(([input, expected]) => {
     test(`${arrToStr(input)} -> ${arrToStr(expected)}`, () => {
       const result = reverseList(arrayToLinkedList(input));
