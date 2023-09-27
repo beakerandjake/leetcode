4e7771346a3036cec2774d24584a63ce782ad79b
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-27 14:28:13 -0700

    scaffold 872

---
+++ src/leetcode/872.js
@@ -0,0 +1,21 @@
+/**
+ * Consider all the leaves of a binary tree, from left to right order,
+ * the values of those leaves form a leaf value sequence.
+ * Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+ * Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+export const leafSimilar = (root1, root2) => {};
---
+++ tests/leetcode/872.test.js
@@ -0,0 +1,19 @@
+import { leafSimilar } from '../../src/leetcode/872.js';
+import { arrToStr } from '../util.js';
+import { arrayToBinaryTree } from './util.js';
+
+describe('872. Leaf-Similar Trees', () => {
+  [
+    [
+      [3, 5, 1, 6, 2, 9, 8, null, null, 7, 4],
+      [3, 5, 1, 6, 7, 4, 2, null, null, null, null, null, null, 9, 8],
+      true,
+    ],
+    [[1, 2, 3], [1, 3, 2], false],
+  ].forEach(([a, b, expected]) => {
+    test(`${arrToStr(a)},${arrToStr(b)} -> ${expected}`, () => {
+      const result = leafSimilar(a, b);
+      expect(result).toBe(expected);
+    });
+  });
+});
