7cb75eac6703c268072b73acf8ca5547432badb8
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-27 11:50:57 -0700

    scaffold leetcode 206

---
+++ src/leetcode/206.js
@@ -0,0 +1,16 @@
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+export const reverseList = (head) => {};
