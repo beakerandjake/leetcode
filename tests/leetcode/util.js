c8223988a34e101afeb729d2d26773acf398a6ce
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-27 11:50:52 -0700

    add leetcode specific util

---
+++ tests/leetcode/util.js
@@ -0,0 +1,26 @@
+/**
+ * Converts an array to a singly linked list and returns the head node.
+ */
+export const arrayToLinkedList = (arr) => {
+  if (arr.length <= 0) {
+    return undefined;
+  }
+  const nodes = arr.map((x) => ({ val: x, next: null }));
+  for (let i = 0; i < arr.length - 1; i++) {
+    nodes[i].next = nodes[i + 1];
+  }
+  return nodes[0];
+};
+
+export const linkedListToArray = (list) => {
+  if (!list) {
+    return [];
+  }
+  const toReturn = [];
+  let current = list;
+  while (current.next) {
+    toReturn.push(current.val);
+    current = current.next;
+  }
+  return toReturn;
+};
