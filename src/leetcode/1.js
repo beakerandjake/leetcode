de2164af1a68cee4f772b4dca5d69c710dee6753
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-28 14:36:25 -0700

    first pass leetcode 1

---
+++ src/leetcode/1.js
@@ -0,0 +1,34 @@
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ */
+
+const indexMap = (nums) => {
+  const toReturn = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (!toReturn.has(nums[i])) {
+      toReturn.set(nums[i], [i]);
+    } else {
+      toReturn.get(nums[i]).push(i);
+    }
+  }
+  return toReturn;
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+export const twoSum = (nums, target) => {
+  const frequencies = indexMap(nums);
+  for (let i = 0; i < nums.length; i++) {
+    const needed = target - nums[i];
+    const neededIndexes = frequencies.get(needed);
+    if (!neededIndexes?.length || (needed === nums[i] && neededIndexes.length === 1)) {
+      continue;
+    }
+    return [i, neededIndexes.find((x) => x > i)];
+  }
+};
