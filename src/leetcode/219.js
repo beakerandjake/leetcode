77462a3424d9863580cd377e064c19df691365f4
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-28 16:46:05 -0700

    add better solution

--- src/leetcode/219.js
+++ src/leetcode/219.js
@@ -3,19 +3,18 @@
  * such that nums[i] == nums[j] and abs(i - j) <= k.
  */
 
-const getIndexMap = (nums) => {
-  const toReturn = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    if (!toReturn.has(nums[i])) {
-      toReturn.set(nums[i], [i]);
-    } else {
-      toReturn.get(nums[i]).push(i);
-    }
-  }
-  return toReturn;
-};
-
 const simple = (nums, k) => {
+  const getIndexMap = () => {
+    const toReturn = new Map();
+    for (let i = 0; i < nums.length; i++) {
+      if (!toReturn.has(nums[i])) {
+        toReturn.set(nums[i], [i]);
+      } else {
+        toReturn.get(nums[i]).push(i);
+      }
+    }
+    return toReturn;
+  };
   const getPartner = (indexes, index) =>
     indexes.find((i) => i !== index && Math.abs(i - index) <= k);
   const indexMap = getIndexMap(nums);
@@ -27,4 +26,14 @@ const simple = (nums, k) => {
  * @param {number} k
  * @return {boolean}
  */
-export const containsNearbyDuplicate = simple;
+export const containsNearbyDuplicate = (nums, k) => {
+  const indexMap = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (indexMap.has(num) && Math.abs(indexMap.get(num) - i) <= k) {
+      return true;
+    }
+    indexMap.set(num, i);
+  }
+  return false;
+};
