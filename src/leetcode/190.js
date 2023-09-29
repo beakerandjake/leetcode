6a09f4e63d7bcee91f7577f7cb3c3b6493dd18d9
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-29 16:59:23 -0700

    add additional solution

--- src/leetcode/190.js
+++ src/leetcode/190.js
@@ -2,7 +2,15 @@
  * Reverse bits of a given 32 bits unsigned integer.
  */
 
-// import { binToStr } from '../../tests/util';
+const xor = (n) => {
+  let result = 0;
+  for (let i = 0; i < 32; i++) {
+    if (n & (1 << (31 - i))) {
+      result ^= 1 << i;
+    }
+  }
+  return result >>> 0;
+};
 
 /**
  * @param {number} n - a positive integer
@@ -10,10 +18,11 @@
  */
 export const reverseBits = (n) => {
   let result = 0;
+  let remaining = n;
   for (let i = 0; i < 32; i++) {
-    if (n & (1 << (31 - i))) {
-      result ^= 1 << i;
-    }
+    result <<= 1;
+    result |= remaining & 1;
+    remaining >>= 1;
   }
   return result >>> 0;
 };
