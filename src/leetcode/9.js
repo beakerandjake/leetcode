23bb1c1e351e62963f742dd6ff90a5e2ffb3b8bb
beakerandjake <beakerandjake@users.noreply.github.com>
2023-09-29 13:27:08 -0700

    simple implementation

--- src/leetcode/9.js
+++ src/leetcode/9.js
@@ -2,16 +2,46 @@
  * Given an integer x, return true if x is a palindrome, and false otherwise.
  */
 
-const functional = (x) => {
+const arraysEqual = (x) => {
   const digits = (num) => [...`${num}`];
   const reverse = (arr) => [...arr].reverse();
   const equals = (a, b) => a.every((digit, i) => digit === b[i]);
   return equals(digits(x), reverse(digits(x)));
 };
 
+const arraySymmetrical = (x) => {
+  const digits = (num) => {
+    const toReturn = [];
+    let current = num;
+    while (current > 0) {
+      toReturn.push(current % 10);
+      current = Math.floor(current / 10);
+    }
+    return toReturn;
+  };
+
+  const symmetrical = (arr) => {
+    const m = Math.floor(arr.length / 2);
+    for (let i = 0; i < m; i++) {
+      if (arr[i] !== arr[arr.length - i - 1]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  if (x < 0) {
+    return false;
+  }
+  if (x < 10) {
+    return true;
+  }
+
+  return symmetrical(digits(x));
+};
 
 /**
  * @param {number} x
  * @return {boolean}
  */
-export const isPalindrome = functional;
+export const isPalindrome = arraySymmetrical;
