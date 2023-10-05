e1b5285d504fc4d5452858311d2b2ee79d6e22f8
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-05 15:48:41 -0700

    add additional solve method

--- src/leetcode/57.js
+++ src/leetcode/57.js
@@ -4,39 +4,84 @@
  * Return intervals after the insertion.
  */
 
-const isBefore = (a, b) => a[1] < b[0];
+const firstPass = (() => {
+  const isBefore = (a, b) => a[1] < b[0];
 
-const isAfter = (a, b) => a[0] > b[1];
+  const isAfter = (a, b) => a[0] > b[1];
+
+  return (intervals, newInterval) => {
+    if (intervals.length <= 0) {
+      return [newInterval];
+    }
+    const toReturn = [];
+    const toInsert = newInterval;
+    let inserted = false;
+    for (const current of intervals) {
+      if (isAfter(toInsert, current)) {
+        toReturn.push(current);
+      } else if (isBefore(toInsert, current)) {
+        if (!inserted) {
+          toReturn.push(toInsert);
+          inserted = true;
+        }
+        toReturn.push(current);
+      } else {
+        toInsert[0] = Math.min(toInsert[0], current[0]);
+        toInsert[1] = Math.max(toInsert[1], current[1]);
+      }
+    }
+    if (!inserted) {
+      toReturn.push(toInsert);
+    }
+
+    return toReturn;
+  };
+})();
+
+const slicing = (() => {
+  const isBefore = (a, b) => a[1] < b[0];
+
+  const overlaps = ([s1, e1], [s2, e2]) => s1 <= e2 && e1 >= s2;
+
+  const findInsertIndex = (intervals, newInterval) => {
+    for (let i = 0; i < intervals.length; i++) {
+      if (!isBefore(intervals[i], newInterval)) {
+        return i;
+      }
+    }
+    return intervals.length;
+  };
+
+  return (intervals, newInterval) => {
+    if (intervals.length <= 0) {
+      return [newInterval];
+    }
+
+    const insertAt = findInsertIndex(intervals, newInterval);
+
+    if (insertAt === intervals.length) {
+      return [...intervals, newInterval];
+    }
+
+    let i = insertAt;
+    const merged = newInterval;
+    while (i < intervals.length) {
+      const current = intervals[i];
+      if (!overlaps(merged, current)) {
+        break;
+      }
+      merged[0] = Math.min(merged[0], current[0]);
+      merged[1] = Math.max(merged[1], current[1]);
+      i++;
+    }
+
+    return [...intervals.slice(0, insertAt), merged, ...intervals.slice(i)];
+  };
+})();
 
 /**
  * @param {number[][]} intervals
  * @param {number[]} newInterval
  * @return {number[][]}
  */
-export const insert = (intervals, newInterval) => {
-  if (intervals.length <= 0) {
-    return [newInterval];
-  }
-  const toReturn = [];
-  const toInsert = newInterval;
-  let inserted = false;
-  for (const current of intervals) {
-    if (isAfter(toInsert, current)) {
-      toReturn.push(current);
-    } else if (isBefore(toInsert, current)) {
-      if (!inserted) {
-        toReturn.push(toInsert);
-        inserted = true;
-      }
-      toReturn.push(current);
-    } else {
-      toInsert[0] = Math.min(toInsert[0], current[0]);
-      toInsert[1] = Math.max(toInsert[1], current[1]);
-    }
-  }
-  if (!inserted) {
-    toReturn.push(toInsert);
-  }
-
-  return toReturn;
-};
+export const insert = slicing;
