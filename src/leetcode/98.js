1b2f9e95c8db5590d2fa95e8eda97577b39cdc4e
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-09 12:14:22 -0700

    first pass leetcode 98

--- src/leetcode/98.js
+++ src/leetcode/98.js
@@ -17,8 +17,38 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
+const nodes = (root) => {
+  const valuesRecursive = (node, arr) => {
+    if (!node) {
+      return arr;
+    }
+    return [
+      ...arr,
+      node.val,
+      ...valuesRecursive(node.left, arr),
+      ...valuesRecursive(node.right, arr),
+    ];
+  };
+  return valuesRecursive(root, []);
+};
+
+const valid = (node) => {
+  if (!node) {
+    return true;
+  }
+  return (
+    nodes(node.left).every((x) => x < node.val) &&
+    nodes(node.right).every((x) => x > node.val) &&
+    valid(node.left) &&
+    valid(node.right)
+  );
+};
+
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
-export const isValidBST = (root) => {};
+export const isValidBST = (root) => {
+  return valid(root);
+};
