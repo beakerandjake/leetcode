4c00de6b197531522a11f30597907b44d9c9546a
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-10 11:21:52 -0700

    leetcode 155

---
+++ src/leetcode/155.js
@@ -0,0 +1,50 @@
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+ class MinStack {
+  constructor() {
+    this.stack = [];
+    this.min = [];
+  }
+
+  /**
+   * @param {number} val
+   * @return {void}
+   */
+  push(val) {
+    this.stack.push(val);
+    if (!this.min.length || val <= this.min.at(-1)) {
+      this.min.push(val);
+    }
+  }
+
+  /**
+   * @return {void}
+   */
+  pop() {
+    const popped = this.stack.pop();
+    if (this.min.length && this.min.at(-1) === popped) {
+      this.min.pop();
+    }
+  }
+
+  /**
+   * @return {number}
+   */
+  top() {
+    return this.stack.at(-1);
+  }
+
+  /**
+   * @return {number}
+   */
+  getMin() {
+    return this.min.at(-1);
+  }
+}
