649d5ed277f80125b4d14bd386c1cba0a65cbe31
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-18 13:37:18 -0700

    add recursive solution

--- src/leetcode/230.js
+++ src/leetcode/230.js
@@ -2,15 +2,47 @@
  * Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
  */
 
-const inOrder = (root, visitFn) => {
-  if (!root) {
-    return;
-  }
-  inOrder(root.left, visitFn);
-  if (visitFn(root) === false) {
-    return;
-  }
-  inOrder(root.right, visitFn);
+const iterative = (() => {
+  const inOrder = (root, visitFn) => {
+    if (!root) {
+      return;
+    }
+    inOrder(root.left, visitFn);
+    if (visitFn(root) === false) {
+      return;
+    }
+    inOrder(root.right, visitFn);
+  };
+
+  return (root, k) => {
+    let index = 1;
+    let toReturn;
+    inOrder(root, (node) => {
+      if (index++ === k) {
+        toReturn = node;
+        return false;
+      }
+    });
+    return toReturn?.val;
+  };
+})();
+
+const recursive = (root, k) => {
+  let index = 1;
+  const kTh = (node) => {
+    if (!node) {
+      return -1;
+    }
+    const left = kTh(node.left);
+    if (left !== -1) {
+      return left;
+    }
+    if (index++ === k) {
+      return node.val;
+    }
+    return kTh(node.right);
+  };
+  return kTh(root);
 };
 
 /**
@@ -18,14 +50,4 @@ const inOrder = (root, visitFn) => {
  * @param {number} k
  * @return {number}
  */
-export const kthSmallest = (root, k) => {
-  let index = 1;
-  let toReturn;
-  inOrder(root, (node) => {
-    if (index++ === k) {
-      toReturn = node;
-      return false;
-    }
-  });
-  return toReturn?.val;
-};
+export const kthSmallest = iterative;
