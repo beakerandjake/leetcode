c099636d79ca835b5a87b97376d13d132f03e7d4
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-18 15:36:14 -0700

    lc 162

---
+++ src/leetcode/162.js
@@ -0,0 +1,40 @@
+/**
+ * A peak element is an element that is strictly greater than its neighbors.
+ * Given a 0-indexed integer array nums, find a peak element, and return its index.
+ * If the array contains multiple peaks, return the index to any of the peaks.
+ * You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+ * You must write an algorithm that runs in O(log n) time.
+ */
+
+const elementAt = (nums, index) =>
+  index >= 0 && index < nums.length ? nums[index] : Number.MIN_SAFE_INTEGER;
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+export const findPeakElement = (nums) => {
+  if (nums.length === 1) {
+    return 0;
+  }
+
+  let l = 0;
+  let u = nums.length - 1;
+  while (l <= u) {
+    const m = Math.floor(l + (u - l) / 2);
+    const previous = elementAt(nums, m - 1);
+    const current = elementAt(nums, m);
+    const next = elementAt(nums, m + 1);
+    if (previous < current && current > next) {
+      return m;
+    }
+    if (current < previous) {
+      // go left
+      u = m - 1;
+    } else {
+      // go right
+      l = m + 1;
+    }
+  }
+  return -1;
+};
