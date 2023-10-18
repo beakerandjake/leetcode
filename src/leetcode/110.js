5bedcf93c0f41660e63269e82702f90a7f1c1d98
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-18 14:12:57 -0700

    lc 110

---
+++ src/leetcode/110.js
@@ -0,0 +1,31 @@
+/**
+ * Given a binary tree, determine if it is height-balanced.
+ */
+
+const height = (root) => {
+  if (!root) {
+    return 0;
+  }
+  if (!root.left && !root.right) {
+    return 1;
+  }
+
+  const toReturn = Math.max(height(root.left), height(root.right));
+  return toReturn === 0 ? 0 : 1 + toReturn;
+};
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+export const isBalanced = (root) => {
+  if (!root) {
+    return true;
+  }
+
+  if(Math.abs(height(root.left) - height(root.right)) > 1){
+    return false;
+  }
+
+  return isBalanced(root.left) && isBalanced(root.right);
+};
