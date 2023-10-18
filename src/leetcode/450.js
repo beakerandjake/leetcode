366c5ea4692b98749eb7d46c7d099cc278d4b9be
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-18 12:50:33 -0700

    add recursive solution

--- src/leetcode/450.js
+++ src/leetcode/450.js
@@ -8,84 +8,126 @@
  *     If the node is found, delete the node.
  */
 
-const findNode = (root, value) => {
-  let parent = null;
-  let current = root;
-  while (current) {
-    if (current.val === value) {
-      return { parent, node: current };
+const iterative = (() => {
+  const findNode = (root, value) => {
+    let parent = null;
+    let current = root;
+    while (current) {
+      if (current.val === value) {
+        return { parent, node: current };
+      }
+      parent = current;
+      if (value < current.val) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
     }
-    parent = current;
-    if (value < current.val) {
-      current = current.left;
-    } else {
-      current = current.right;
-    }
-  }
-  return null;
-};
+    return null;
+  };
 
-const findSuccessor = (root) => {
-  let parent = root;
-  let next = root.right;
-  while (next.left) {
-    parent = next;
-    next = next.left;
-  }
-  return { parent, node: next };
-};
+  const findSuccessor = (root) => {
+    let parent = root;
+    let next = root.right;
+    while (next.left) {
+      parent = next;
+      next = next.left;
+    }
+    return { parent, node: next };
+  };
+
+  (root, key) => {
+    const toDelete = findNode(root, key);
+    // bail if key isn't in tree.
+    if (!toDelete) {
+      return root;
+    }
+
+    // node is leaf.
+    if (!toDelete.node.left && !toDelete.node.right) {
+      // deleting the root node
+      if (!toDelete.parent) {
+        return null;
+      }
+
+      if (toDelete.parent.left === toDelete.node) {
+        toDelete.parent.left = null;
+      } else {
+        toDelete.parent.right = null;
+      }
+
+      return root;
+    }
+
+    // node has one child.
+    if (!toDelete.node.left || !toDelete.node.right) {
+      // deleting root node.
+      if (!toDelete.parent) {
+        return toDelete.node.left || toDelete.node.right;
+      }
+
+      if (toDelete.node === toDelete.parent.left) {
+        toDelete.parent.left = toDelete.node.left || toDelete.node.right;
+      } else {
+        toDelete.parent.right = toDelete.node.left || toDelete.node.right;
+      }
+
+      return root;
+    }
+
+    // node has two children
+    const successor = findSuccessor(toDelete.node);
+    if (successor.node === successor.parent.left) {
+      successor.parent.left = successor.node.right;
+    } else {
+      successor.parent.right = successor.node.right;
+    }
+    toDelete.node.val = successor.node.val;
+    return root;
+  };
+})();
+
+const recursive = (() => {
+  const findSuccessor = (root) => {
+    let current = root.right;
+    while (current?.left) {
+      current = current.left;
+    }
+    return current;
+  };
+
+  const deleteRecursive = (node, value) => {
+    if (!node) {
+      return null;
+    }
+    if (node.val === value) {
+      if (!node.left) {
+        return node.right;
+      }
+      if (!node.right) {
+        return node.left;
+      }
+      const successor = findSuccessor(node);
+      node.val = successor.val;
+      node.right = deleteRecursive(node.right, successor.val);
+    } else if (value > node.val) {
+      node.right = deleteRecursive(node.right, value);
+    } else {
+      node.left = deleteRecursive(node.left, value);
+    }
+    return node;
+  };
+
+  return deleteRecursive;
+})();
+
+
+
+
 
 /**
  * @param {TreeNode} root
  * @param {number} key
  * @return {TreeNode}
  */
-export const deleteNode = (root, key) => {
-  const toDelete = findNode(root, key);
-  // bail if key isn't in tree.
-  if (!toDelete) {
-    return root;
-  }
-
-  // node is leaf.
-  if (!toDelete.node.left && !toDelete.node.right) {
-    // deleting the root node
-    if (!toDelete.parent) {
-      return null;
-    }
-
-    if (toDelete.parent.left === toDelete.node) {
-      toDelete.parent.left = null;
-    } else {
-      toDelete.parent.right = null;
-    }
-
-    return root;
-  }
-
-  // node has one child.
-  if (!toDelete.node.left || !toDelete.node.right) {
-    // deleting root node.
-    if (!toDelete.parent) {
-      return toDelete.node.left || toDelete.node.right;
-    }
-
-    if (toDelete.node === toDelete.parent.left) {
-      toDelete.parent.left = toDelete.node.left || toDelete.node.right;
-    } else {
-      toDelete.parent.right = toDelete.node.left || toDelete.node.right;
-    }
-
-    return root;
-  }
-
-  // node has two children
-  const successor = findSuccessor(toDelete.node);
-  if (successor.node === successor.parent.left) {
-    successor.parent.left = successor.node.right;
-  } else {
-    successor.parent.right = successor.node.right;
-  }
-  toDelete.node.val = successor.node.val;
-  return root;
-};
+export const deleteNode = recursive;
