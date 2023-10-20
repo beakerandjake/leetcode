ff1bc3fe669b25b045c6a0ae9dfa0e6d472ee2c3
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-20 14:05:08 -0700

    lc 4

--- src/leetcode/4.js
+++ src/leetcode/4.js
@@ -3,30 +3,34 @@
  * The overall run time complexity should be O(log (m+n)).
  */
 
-const merge = (a, b) => {
-  const toReturn = [];
-  let aIndex = 0;
-  let bIndex = 0;
-  while (aIndex < a.length && bIndex < b.length) {
-    toReturn.push(a[aIndex] < b[bIndex] ? a[aIndex++] : b[bIndex++]);
-  }
-  if (aIndex !== a.length) {
-    toReturn.push(...a.slice(aIndex));
-  }
-  if (bIndex !== b.length) {
-    toReturn.push(...b.slice(bIndex));
-  }
-  return toReturn;
-};
+const simple = (() => {
+  const merge = (a, b) => {
+    const toReturn = [];
+    let aIndex = 0;
+    let bIndex = 0;
+    while (aIndex < a.length && bIndex < b.length) {
+      toReturn.push(a[aIndex] < b[bIndex] ? a[aIndex++] : b[bIndex++]);
+    }
+    while (aIndex !== a.length) {
+      toReturn.push(a[aIndex++]);
+    }
+    while (bIndex !== b.length) {
+      toReturn.push(b[bIndex++]);
+    }
+    return toReturn;
+  };
 
-const median = (arr) =>
-  arr.length % 2 === 0
-    ? (arr[Math.floor(arr.length / 2)] + arr[Math.floor(arr.length / 2 - 1)]) / 2
-    : arr[Math.floor(arr.length / 2)];
+  const median = (arr) =>
+    arr.length % 2 === 0
+      ? (arr[Math.floor(arr.length / 2)] + arr[Math.floor(arr.length / 2 - 1)]) / 2
+      : arr[Math.floor(arr.length / 2)];
+
+  return (a, b) => median(merge(a, b));
+})();
 
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
-export const findMedianSortedArrays = (nums1, nums2) => median(merge(nums1, nums2));
+export const findMedianSortedArrays = simple;
