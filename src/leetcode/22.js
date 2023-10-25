4d596774a83bb0d4e48ef9d1740dac34658800b6
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-25 15:01:47 -0700

    add faster solution

--- src/leetcode/22.js
+++ src/leetcode/22.js
@@ -2,24 +2,48 @@
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
  */
 
-const isWellFormed = (parens) => {
-  if (!parens.length || parens.length % 2 !== 0) {
-    return false;
-  }
-
-  const stack = [];
-  for (let i = 0; i < parens.length; i++) {
-    if (parens[i] === '(') {
-      stack.push(1);
-    } else {
-      if (!stack.length) {
-        return false;
-      }
-      stack.pop();
+const simple = (() => {
+  const isWellFormed = (parens) => {
+    if (!parens.length || parens.length % 2 !== 0) {
+      return false;
     }
-  }
-  return stack.length === 0;
-};
+
+    const stack = [];
+    for (let i = 0; i < parens.length; i++) {
+      if (parens[i] === '(') {
+        stack.push(1);
+      } else {
+        if (!stack.length) {
+          return false;
+        }
+        stack.pop();
+      }
+    }
+    return stack.length === 0;
+  };
+
+  return (n) => {
+    const combinations = [];
+    const maxLength = n * 2;
+    const helper = (combination) => {
+      if (combination.length === maxLength) {
+        if (isWellFormed(combination)) {
+          combinations.push(combination.join(''));
+        }
+        return;
+      }
+
+      if (!combination.length) {
+        helper([...combination, '(']);
+      } else {
+        helper([...combination, '(']);
+        helper([...combination, ')']);
+      }
+    };
+    helper([]);
+    return combinations;
+  };
+})();
 
 /**
  * @param {number} n
@@ -28,21 +52,22 @@ const isWellFormed = (parens) => {
 export const generateParenthesis = (n) => {
   const combinations = [];
   const maxLength = n * 2;
-  const helper = (combination) => {
+  const helper = (combination, openCount, closedCount) => {
     if (combination.length === maxLength) {
-      if (isWellFormed(combination)) {
-        combinations.push(combination.join(''));
-      }
+      combinations.push(combination.join(''));
       return;
     }
-
-    if (!combination.length) {
-      helper([...combination, '(']);
-    } else {
-      helper([...combination, '(']);
-      helper([...combination, ')']);
+    if (openCount < n) {
+      combination.push('(');
+      helper(combination, openCount + 1, closedCount);
+      combination.pop();
+    }
+    if (closedCount < openCount) {
+      combination.push(')');
+      helper(combination, openCount, closedCount + 1);
+      combination.pop();
     }
   };
-  helper([]);
+  helper([], 0, 0);
   return combinations;
 };
