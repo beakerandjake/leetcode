ab5afbeefddcfd22e79f03029acbd8125ec5ab92
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-25 10:18:55 -0700

    lc 47

--- src/leetcode/47.js
+++ src/leetcode/47.js
@@ -2,11 +2,7 @@
  * Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
  */
 
-const splice = (arr, i) => {
-  const copy = [...arr];
-  copy.splice(i, 1);
-  return copy;
-};
+const splice = (arr, index) => arr.filter((_, i) => index !== i);
 
 /**
  * @param {number[]} nums
@@ -21,7 +17,7 @@ export const permuteUnique = (nums) => {
       if (!encountered.has(hash)) {
         encountered.add(hash);
         permutations.push([...permutation]);
-      } 
+      }
       return;
     }
     for (let i = 0; i < remaining.length; i++) {
