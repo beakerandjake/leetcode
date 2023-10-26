ddd8b9d0e48d6f73800bf50151a05f35f09ac45f
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-26 10:48:15 -0700

    speed up lc 79

--- src/leetcode/79.js
+++ src/leetcode/79.js
@@ -1,19 +1,13 @@
+/* eslint-disable no-param-reassign */
 /**
  * Given an m x n grid of characters board and a string word, return true if word exists in the grid.
  * The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
  */
 
-const hashPosition = ({ x, y }) => `${x} ${y}`;
-
 const getShape = (board) => ({
   height: board.length,
   width: board.length ? board[0].length : 0,
 });
-
-const add = (lhs, rhs) => ({ x: lhs.x + rhs.x, y: lhs.y + rhs.y });
-
-const inBounds = ({ width, height }, { x, y }) =>
-  x >= 0 && x < width && y >= 0 && y < height;
 
 const findLetter = (board, { width, height }, letter) => {
   const matches = [];
@@ -27,6 +21,8 @@ const findLetter = (board, { width, height }, letter) => {
   return matches;
 };
 
+const zeros = ({ width, height }) => [...Array(height)].map(() => Array(width).fill(0));
+
 const cardinalDirections = [
   // up
   { x: 0, y: -1 },
@@ -38,27 +34,32 @@ const cardinalDirections = [
   { x: -1, y: 0 },
 ];
 
+const add = (lhs, rhs) => ({ x: lhs.x + rhs.x, y: lhs.y + rhs.y });
+
+const inBounds = ({ width, height }, { x, y }) =>
+  x >= 0 && x < width && y >= 0 && y < height;
+
 const getNeighbors = (shape, position) =>
-  cardinalDirections
-    .map((direction) => add(position, direction))
-    .filter((neighbor) => inBounds(shape, neighbor));
+  cardinalDirections.reduce((acc, direction) => {
+    const neighbor = add(position, direction);
+    if (inBounds(shape, neighbor)) {
+      acc.push(neighbor);
+    }
+    return acc;
+  }, []);
 
 const formsWord = (board, shape, word, position, wordIndex, visited) => {
-  if (word[wordIndex] !== board[position.y][position.x]) {
-    return false;
-  }
   if (wordIndex === word.length - 1) {
     return true;
   }
-
   for (const neighbor of getNeighbors(shape, position)) {
-    const hash = hashPosition(neighbor);
-    if (!visited.has(hash)) {
-      visited.add(hash);
+    const { x, y } = neighbor;
+    if (board[y][x] === word[wordIndex + 1] && !visited[y][x]) {
+      visited[y][x] = 1;
       if (formsWord(board, shape, word, neighbor, wordIndex + 1, visited)) {
         return true;
       }
-      visited.delete(hash);
+      visited[y][x] = 0;
     }
   }
   return false;
@@ -72,15 +73,14 @@ const formsWord = (board, shape, word, position, wordIndex, visited) => {
 export const exist = (board, word) => {
   const shape = getShape(board);
   const matches = findLetter(board, shape, word[0]);
-
-  if (word.length === 1) {
-    return !!matches.length;
-  }
+  const visited = zeros(shape);
 
   for (const match of matches) {
-    if (formsWord(board, shape, word, match, 0, new Set([hashPosition(match)]))) {
+    visited[match.y][match.x] = 1;
+    if (formsWord(board, shape, word, match, 0, visited)) {
       return true;
     }
+    visited[match.y][match.x] = 0;
   }
   return false;
 };
