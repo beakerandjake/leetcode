8868f08f6b0e997df0d2244f2f7919c542b69cb4
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-26 10:53:44 -0700

    don't find letters first

--- src/leetcode/79.js
+++ src/leetcode/79.js
@@ -9,18 +9,6 @@ const getShape = (board) => ({
   width: board.length ? board[0].length : 0,
 });
 
-const findLetter = (board, { width, height }, letter) => {
-  const matches = [];
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      if (board[y][x] === letter) {
-        matches.push({ y, x });
-      }
-    }
-  }
-  return matches;
-};
-
 const zeros = ({ width, height }) => [...Array(height)].map(() => Array(width).fill(0));
 
 const cardinalDirections = [
@@ -72,15 +60,23 @@ const formsWord = (board, shape, word, position, wordIndex, visited) => {
  */
 export const exist = (board, word) => {
   const shape = getShape(board);
-  const matches = findLetter(board, shape, word[0]);
-  const visited = zeros(shape);
 
-  for (const match of matches) {
-    visited[match.y][match.x] = 1;
-    if (formsWord(board, shape, word, match, 0, visited)) {
-      return true;
-    }
-    visited[match.y][match.x] = 0;
+  if (word.length > shape.width * shape.height) {
+    return false;
   }
+
+  const visited = zeros(shape);
+  for (let y = 0; y < shape.height; y++) {
+    for (let x = 0; x < shape.width; x++) {
+      if (board[y][x] === word[0]) {
+        visited[y][x] = 1;
+        if (formsWord(board, shape, word, { y, x }, 0, visited)) {
+          return true;
+        }
+        visited[y][x] = 0;
+      }
+    }
+  }
+
   return false;
 };
