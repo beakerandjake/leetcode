ba6184118447f66eb15e79122d592edb523d21a9
beakerandjake <beakerandjake@users.noreply.github.com>
2023-10-28 15:50:58 -0700

    better solution

--- src/leetcode/286.js
+++ src/leetcode/286.js
@@ -7,11 +7,9 @@
  *
  * Fill each empty room with the distance to its nearest gate. If it is impossible to reach a gate, it should be filled with INF.
  */
-const tiles = {
-  gate: 0,
-  wall: -1,
-  empty: 2147483647,
-};
+
+const GATE = 0;
+const EMPTY = 2147483647;
 
 const getShape = (matrix) => ({ height: matrix.length, width: matrix[0].length });
 
@@ -19,8 +17,8 @@ const gates = (matrix, { width, height }) => {
   const found = [];
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
-      if (matrix[y][x] === tiles.gate) {
-        found.push({ y, x });
+      if (matrix[y][x] === GATE) {
+        found.push([x, y]);
       }
     }
   }
@@ -28,21 +26,13 @@ const gates = (matrix, { width, height }) => {
 };
 
 const directions = [
-  { x: 0, y: -1 },
-  { x: 1, y: 0 },
-  { x: 0, y: 1 },
-  { x: -1, y: 0 },
+  [0, -1],
+  [1, 0],
+  [0, 1],
+  [-1, 0],
 ];
 
-const add = (a, b) => ({ x: a.x + b.x, y: a.y + b.y });
-
-const inBounds = ({ x, y }, { width, height }) =>
-  x >= 0 && x < width && y >= 0 && y < height;
-
-const neighbors = (point, shape) =>
-  directions
-    .map((direction) => add(direction, point))
-    .filter((neighbor) => inBounds(neighbor, shape));
+const inBounds = (x, y, { width, height }) => x >= 0 && x < width && y >= 0 && y < height;
 
 /**
  * @param {number[][]} matrix
@@ -50,17 +40,16 @@ const neighbors = (point, shape) =>
  */
 export const wallsAndGates = (matrix) => {
   const shape = getShape(matrix);
-  const queue = gates(matrix, shape)
-    .flatMap((gate) => neighbors(gate, shape))
-    .map((point) => ({ point, distance: 1 }));
-
+  const queue = gates(matrix, shape);
   while (queue.length) {
-    const { point, distance } = queue.shift();
-    if (matrix[point.y][point.x] === tiles.empty) {
-      matrix[point.y][point.x] = distance;
-      queue.push(
-        ...neighbors(point, shape).map((n) => ({ point: n, distance: distance + 1 }))
-      );
+    const [x, y] = queue.shift();
+    for (const [dx, dy] of directions) {
+      const nx = x + dx;
+      const ny = y + dy;
+      if (inBounds(nx, ny, shape) && matrix[ny][nx] === EMPTY) {
+        matrix[ny][nx] = matrix[y][x] + 1;
+        queue.push([nx, ny]);
+      }
     }
   }
 };
