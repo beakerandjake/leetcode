1171bb56b62708d6e81a0d8f715797dc75352d9d
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-04 13:40:23 -0700

    uglify remove export and copy to clipboard

--- bin/lcUglify.sh
+++ bin/lcUglify.sh
@@ -15,13 +15,26 @@ if [ ! -f $srcFile ]
         exit 1
 fi
 
-# uglify with or without without mangling props.
+# uglify with or without extra options.
+ugly=0
 if [[ -z $2 ]]
     then
-        # mangle the props.
-        uglifyjs -m -c --mangle-props keep_quoted $srcFile 
+        # extra options
+        ugly=$(uglifyjs $srcFile -c drop_console=true,passes=2 -m properties=keep_quoted)
     else 
-        # keep the props
-        uglifyjs $srcFile -m -c
+        # simple options
+        ugly=$(uglifyjs $srcFile -c drop_console=true,passes=2 -m)
 fi
 
+# remove the export portion of the uglified output.
+ugly=$(echo $ugly | sed -E 's/;export\{\w+\};/;/g')
+
+
+# attempt to copy uglify output to clipboard
+if command -v xclip &> /dev/null
+    then
+        echo $ugly | xclip -selection clipboard
+fi
+
+# output result
+echo $ugly
