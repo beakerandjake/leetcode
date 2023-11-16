3a478ab631ffcc48fb2d7acdffc88f267e338430
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-16 15:52:12 -0700

    lc 54

--- src/leetcode/54.js
+++ src/leetcode/54.js
@@ -35,8 +35,71 @@
  * https://leetcode.com/problems/spiral-matrix
  */
 
+const outputRow = (matrix, row, colStart, colEnd) => {
+  const output = [];
+  for (let col = colStart; col <= colEnd; col++) {
+    output.push(matrix[row][col]);
+  }
+  return output;
+};
+
+const outputRowReverse = (matrix, row, colStart, colEnd) => {
+  const output = [];
+  for (let col = colStart; col >= colEnd; col--) {
+    output.push(matrix[row][col]);
+  }
+  return output;
+};
+
+const outputCol = (matrix, col, rowStart, rowEnd) => {
+  const output = [];
+  for (let row = rowStart; row <= rowEnd; row++) {
+    output.push(matrix[row][col]);
+  }
+  return output;
+};
+
+const outputColReverse = (matrix, col, rowStart, rowEnd) => {
+  const output = [];
+  for (let row = rowStart; row >= rowEnd; row--) {
+    output.push(matrix[row][col]);
+  }
+  return output;
+};
+
 /**
  * @param {number[][]} matrix
  * @return {number[]}
  */
-export const spiralOrder = (matrix) => {};
+export const spiralOrder = (matrix) => {
+  const output = [];
+  const height = matrix.length;
+  const width = matrix[0].length;
+  const elementCount = width * height;
+  let left = 0;
+  let right = width - 1;
+  let top = 0;
+  let bottom = height - 1;
+
+  while (output.length !== elementCount) {
+    output.push(...outputRow(matrix, top, left, right));
+    top++;
+    if (output.length >= elementCount) {
+      break;
+    }
+    output.push(...outputCol(matrix, right, top, bottom));
+    right--;
+    if (output.length >= elementCount) {
+      break;
+    }
+    output.push(...outputRowReverse(matrix, bottom, right, left));
+    bottom--;
+    if (output.length >= elementCount) {
+      break;
+    }
+    output.push(...outputColReverse(matrix, left, bottom, top));
+    left++;
+  }
+
+  return output;
+};
--- tests/leetcode/54.test.js
+++ tests/leetcode/54.test.js
@@ -1,14 +1,37 @@
-import { spiralOrder } from "../../src/leetcode/54.js";
-import { arrToStr } from "../util.js";
+import { spiralOrder } from '../../src/leetcode/54.js';
+import { arrToStr } from '../util.js';
 
-describe("54. Spiral Matrix", () => {
+describe('54. Spiral Matrix', () => {
   [
-    // replace with real test data
-    [true, false],
+    [
+      [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+      ],
+      [1, 2, 3, 6, 9, 8, 7, 4, 5],
+    ],
+    [
+      [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+      ],
+      [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7],
+    ],
+    [
+      [
+        [1, 2, 3, 4, 5],
+        [14, 15, 16, 17, 6],
+        [13, 20, 19, 18, 7],
+        [12, 11, 10, 9, 8],
+      ],
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    ],
   ].forEach(([input, expected]) => {
-    test(`${input} -> ${expected}`, () => {
+    test(`${arrToStr(input)} -> ${arrToStr(expected)}`, () => {
       const result = spiralOrder(input);
-      expect(result).toBe(expected);
+      expect(result).toEqual(expected);
     });
   });
 });
