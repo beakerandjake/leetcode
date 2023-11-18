6585a3ec961be1698112b6d110603ff664689487
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-18 14:47:00 -0700

    lc 649

--- src/leetcode/649.js
+++ src/leetcode/649.js
@@ -62,47 +62,72 @@
  * https://leetcode.com/problems/dota2-senate
  */
 
-const victory = (senate) => new Set(senate).size === 1;
+const simple = (() => {
+  const victory = (senate) => new Set(senate).size === 1;
 
-const findBan = (senator, senate, banned) => {
-  const party = senate[senator];
-  // try to ban those who haven't yet voted.
-  for (let i = senator + 1; i < senate.length; i++) {
-    if (senate[i] !== party && !banned.has(i)) {
-      return i;
-    }
-  }
-  // try to ban previous voters if no one ahead of us
-  for (let i = 0; i < senator; i++) {
-    if (senate[i] !== party && !banned.has(i)) {
-      return i;
-    }
-  }
-  return -1;
-};
-
-const round = (senate) => {
-  const banned = new Set();
-  for (let i = 0; i < senate.length; i++) {
-    if (!banned.has(i)) {
-      const toBan = findBan(i, senate, banned);
-      if (toBan === -1) {
-        return [senate[i]];
+  const findBan = (senator, senate, banned) => {
+    const party = senate[senator];
+    // try to ban those who haven't yet voted.
+    for (let i = senator + 1; i < senate.length; i++) {
+      if (senate[i] !== party && !banned.has(i)) {
+        return i;
       }
-      banned.add(toBan);
     }
-  }
-  return senate.filter((_, i) => !banned.has(i));
-};
+    // try to ban previous voters if no one ahead of us
+    for (let i = 0; i < senator; i++) {
+      if (senate[i] !== party && !banned.has(i)) {
+        return i;
+      }
+    }
+    return -1;
+  };
+
+  const round = (senate) => {
+    const banned = new Set();
+    for (let i = 0; i < senate.length; i++) {
+      if (!banned.has(i)) {
+        const toBan = findBan(i, senate, banned);
+        if (toBan === -1) {
+          return [senate[i]];
+        }
+        banned.add(toBan);
+      }
+    }
+    return senate.filter((_, i) => !banned.has(i));
+  };
+
+  return (senate) => {
+    let remaining = [...senate];
+    while (!victory(remaining)) {
+      remaining = round(remaining);
+    }
+    return remaining[0] === 'R' ? 'Radiant' : 'Dire';
+  };
+})();
 
 /**
  * @param {string} senate
  * @return {string}
  */
 export const predictPartyVictory = (senate) => {
-  let remaining = [...senate];
-  while (!victory(remaining)) {
-    remaining = round(remaining);
+  const queue = [...senate];
+  const headCount = {
+    R: queue.filter((x) => x === 'R').length,
+    D: queue.filter((x) => x === 'D').length,
+  };
+  const pendingBans = {
+    R: 0,
+    D: 0,
+  };
+  while (headCount.R && headCount.D) {
+    const current = queue.shift();
+    if (!pendingBans[current]) {
+      queue.push(current);
+      pendingBans[current === 'R' ? 'D' : 'R']++;
+    } else {
+      headCount[current]--;
+      pendingBans[current]--;
+    }
   }
-  return remaining[0] === 'R' ? 'Radiant' : 'Dire';
+  return headCount.R ? 'Radiant' : 'Dire';
 };
