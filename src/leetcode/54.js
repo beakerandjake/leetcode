007aa4db6134bbf585745caf8d604f332a4720a7
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-18 10:52:56 -0700

    add state machine solution

--- src/leetcode/54.js
+++ src/leetcode/54.js
@@ -35,43 +35,7 @@
  * https://leetcode.com/problems/spiral-matrix
  */
 
-const outputRow = (matrix, row, colStart, colEnd) => {
-  const output = [];
-  for (let col = colStart; col <= colEnd; col++) {
-    output.push(matrix[row][col]);
-  }
-  return output;
-};
-
-const outputRowReverse = (matrix, row, colStart, colEnd) => {
-  const output = [];
-  for (let col = colStart; col >= colEnd; col--) {
-    output.push(matrix[row][col]);
-  }
-  return output;
-};
-
-const outputCol = (matrix, col, rowStart, rowEnd) => {
-  const output = [];
-  for (let row = rowStart; row <= rowEnd; row++) {
-    output.push(matrix[row][col]);
-  }
-  return output;
-};
-
-const outputColReverse = (matrix, col, rowStart, rowEnd) => {
-  const output = [];
-  for (let row = rowStart; row >= rowEnd; row--) {
-    output.push(matrix[row][col]);
-  }
-  return output;
-};
-
-/**
- * @param {number[][]} matrix
- * @return {number[]}
- */
-export const spiralOrder = (matrix) => {
+const simple = (matrix) => {
   const output = [];
   const height = matrix.length;
   const width = matrix[0].length;
@@ -82,24 +46,119 @@ export const spiralOrder = (matrix) => {
   let bottom = height - 1;
 
   while (output.length !== elementCount) {
-    output.push(...outputRow(matrix, top, left, right));
+    // left to right (top row)
+    for (let col = left; col <= right; col++) {
+      output.push(matrix[top][col]);
+    }
     top++;
+
     if (output.length >= elementCount) {
       break;
     }
-    output.push(...outputCol(matrix, right, top, bottom));
+
+    // top to bottom (right col)
+    for (let row = top; row <= bottom; row++) {
+      output.push(matrix[row][right]);
+    }
     right--;
+
     if (output.length >= elementCount) {
       break;
     }
-    output.push(...outputRowReverse(matrix, bottom, right, left));
+
+    // left to right (bottom row)
+    for (let col = right; col >= left; col--) {
+      output.push(matrix[bottom][col]);
+    }
     bottom--;
+
     if (output.length >= elementCount) {
       break;
     }
-    output.push(...outputColReverse(matrix, left, bottom, top));
+
+    // bottom to top (left col)
+    for (let row = bottom; row >= top; row--) {
+      output.push(matrix[row][left]);
+    }
     left++;
   }
 
   return output;
 };
+
+const stateMachine = (() => {
+  /**
+   * Travels right on the top row.
+   */
+  const travelRight = (bounds, visitFn) => {
+    for (let col = bounds.left; col <= bounds.right; col++) {
+      visitFn(bounds.top, col);
+    }
+    return { ...bounds, top: bounds.top + 1 };
+  };
+
+  /**
+   * Travels down on the right col
+   */
+  const travelDown = (bounds, visitFn) => {
+    for (let row = bounds.top; row <= bounds.bottom; row++) {
+      visitFn(row, bounds.right);
+    }
+    return { ...bounds, right: bounds.right - 1 };
+  };
+
+  /**
+   * Travels left on the bottom row.
+   */
+  const travelLeft = (bounds, visitFn) => {
+    for (let col = bounds.right; col >= bounds.left; col--) {
+      visitFn(bounds.bottom, col);
+    }
+    return { ...bounds, bottom: bounds.bottom - 1 };
+  };
+
+  /**
+   * Travel up on the left row.
+   */
+  const travelUp = (bounds, visitFn) => {
+    for (let row = bounds.bottom; row >= bounds.top; row--) {
+      visitFn(row, bounds.left);
+    }
+    return { ...bounds, left: bounds.left + 1 };
+  };
+
+  const transitions = new Map([
+    [travelRight, travelDown],
+    [travelDown, travelLeft],
+    [travelLeft, travelUp],
+    [travelUp, travelRight],
+  ]);
+
+  return (matrix) => {
+    const output = [];
+    const height = matrix.length;
+    const width = matrix[0].length;
+    const items = width * height;
+    let currentState = travelRight;
+    let bounds = {
+      left: 0,
+      right: width - 1,
+      top: 0,
+      bottom: height - 1,
+    };
+    const visit = (row, col) => {
+      output.push(matrix[row][col]);
+    };
+    while (output.length < items) {
+      bounds = currentState(bounds, visit);
+      currentState = transitions.get(currentState);
+    }
+    return output;
+  };
+})();
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+export const spiralOrder = stateMachine;
