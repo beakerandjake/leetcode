64eb017832b154005f933f4d06af4d681e273f8f
beakerandjake <beakerandjake@users.noreply.github.com>
2023-11-25 13:36:37 -0700

    add top down solution

--- src/leetcode/72.js
+++ src/leetcode/72.js
@@ -46,12 +46,7 @@
  * https://leetcode.com/problems/edit-distance
  */
 
-/**
- * @param {string} word1
- * @param {string} word2
- * @return {number}
- */
-export const minDistance = (word1, word2) => {
+const bottomUp = (word1, word2) => {
   const m = word2.length + 1;
   const n = word1.length + 1;
   const dp = [...Array(m)].map(() => Array(n).fill(0));
@@ -69,3 +64,37 @@ export const minDistance = (word1, word2) => {
   }
   return dp[word2.length][word1.length];
 };
+
+const topDown = (word1, word2) => {
+  const memo = [...Array(word1.length + 1)].map(() => Array(word2.length + 1).fill(-1));
+  const dp = (word1Index, word2Index) => {
+    if (!word1Index) {
+      return word2Index;
+    }
+    if (!word2Index) {
+      return word1Index;
+    }
+    if (memo[word1Index][word2Index] === -1) {
+      memo[word1Index][word2Index] =
+        word1[word1Index - 1] === word2[word2Index - 1]
+          ? dp(word1Index - 1, word2Index - 1)
+          : Math.min(
+              // insert
+              dp(word1Index, word2Index - 1),
+              // delete
+              dp(word1Index - 1, word2Index),
+              // replace
+              dp(word1Index - 1, word2Index - 1)
+            ) + 1;
+    }
+    return memo[word1Index][word2Index];
+  };
+  return dp(word1.length, word2.length);
+};
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+export const minDistance = topDown;
