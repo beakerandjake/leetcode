510e23e0f6de11b88da72dec34776536c9149845
beakerandjake <beakerandjake@users.noreply.github.com>
2024-02-06 13:14:22 -0700

    add in place quicksort

--- src/912.js
+++ src/912.js
@@ -35,58 +35,96 @@
  * https://leetcode.com/problems/sort-an-array
  */
 
-const mergeSort = (() => {
-  const merge = (lhs, rhs) => {
-    const merged = [];
-    let lhsIndex = 0;
-    let rhsIndex = 0;
-    while (lhsIndex < lhs.length && rhsIndex < rhs.length) {
-      merged.push(lhs[lhsIndex] < rhs[rhsIndex] ? lhs[lhsIndex++] : rhs[rhsIndex++]);
-    }
-    while (lhsIndex < lhs.length) {
-      merged.push(lhs[lhsIndex++]);
-    }
-    while (rhsIndex < rhs.length) {
-      merged.push(rhs[rhsIndex++]);
-    }
-    return merged;
+// const mergeSort = (() => {
+//   const merge = (lhs, rhs) => {
+//     const merged = [];
+//     let lhsIndex = 0;
+//     let rhsIndex = 0;
+//     while (lhsIndex < lhs.length && rhsIndex < rhs.length) {
+//       merged.push(lhs[lhsIndex] < rhs[rhsIndex] ? lhs[lhsIndex++] : rhs[rhsIndex++]);
+//     }
+//     while (lhsIndex < lhs.length) {
+//       merged.push(lhs[lhsIndex++]);
+//     }
+//     while (rhsIndex < rhs.length) {
+//       merged.push(rhs[rhsIndex++]);
+//     }
+//     return merged;
+//   };
+
+//   const doMergeSort = (arr) => {
+//     if (arr.length <= 1) {
+//       return arr;
+//     }
+//     const mid = Math.floor(arr.length / 2);
+//     const lhs = doMergeSort(arr.slice(0, mid));
+//     const rhs = doMergeSort(arr.slice(mid));
+//     return merge(lhs, rhs);
+//   };
+
+//   return doMergeSort;
+// })();
+
+// const quicksortSimple = (arr) => {
+//   if (arr.length < 2) {
+//     return arr;
+//   }
+//   const pivotIndex = Math.floor(Math.random() * arr.length);
+//   const lt = [];
+//   const gte = [];
+//   arr.forEach((x, i) => {
+//     if (i === pivotIndex) {
+//       return;
+//     }
+//     if (x < arr[pivotIndex]) {
+//       lt.push(x);
+//     } else {
+//       gte.push(x);
+//     }
+//   });
+//   return [...quicksortSimple(lt), arr[pivotIndex], ...quicksortSimple(gte)];
+// };
+
+const quicksortInPlace = (arr) => {
+  const swap = (a, b) => {
+    const temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
   };
 
-  const doMergeSort = (arr) => {
-    if (arr.length <= 1) {
-      return arr;
+  const partition = (lo, hi) => {
+    const pivotIndex = hi;
+    let firstHi = lo;
+    for (let i = lo; i < hi; i++) {
+      if (arr[i] < arr[pivotIndex]) {
+        swap(i, firstHi);
+        firstHi++;
+      }
     }
-    const mid = Math.floor(arr.length / 2);
-    const lhs = doMergeSort(arr.slice(0, mid));
-    const rhs = doMergeSort(arr.slice(mid));
-    return merge(lhs, rhs);
+    swap(pivotIndex, firstHi);
+    return firstHi;
   };
 
-  return doMergeSort;
-})();
+  const partitionRandom = (lo, hi) => {
+    const pivot = Math.floor(Math.random() * (hi - lo + 1) + lo);
+    swap(pivot, hi);
+    return partition(lo, hi);
+  };
 
-const quicksort = (arr) => {
-  if (arr.length < 2) {
-    return arr;
-  }
-  const pivotIndex = Math.floor(Math.random() * arr.length);
-  const lt = [];
-  const gte = [];
-  arr.forEach((x, i) => {
-    if (i === pivotIndex) {
-      return;
+  const quicksort = (lo, hi) => {
+    if (lo < hi) {
+      const p = partitionRandom(lo, hi);
+      quicksort(lo, p - 1);
+      quicksort(p + 1, hi);
     }
-    if (x < arr[pivotIndex]) {
-      lt.push(x);
-    } else {
-      gte.push(x);
-    }
-  });
-  return [...quicksort(lt), arr[pivotIndex], ...quicksort(gte)];
+  };
+
+  quicksort(0, arr.length - 1);
+  return arr;
 };
 
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
-export const sortArray = quicksort;
+export const sortArray = quicksortInPlace;
