3bdd45b18ed6cab3aa2dc77ba1ebc53f7c835f1d
beakerandjake <beakerandjake@users.noreply.github.com>
2024-02-06 15:02:07 -0700

    lc 315

--- src/315.js
+++ src/315.js
@@ -42,30 +42,73 @@
  * https://leetcode.com/problems/count-of-smaller-numbers-after-self
  */
 
-const toSorted = (arr) => [...arr].sort((a, b) => a - b);
+const bruteForce = (() => {
+  const toSorted = (arr) => [...arr].sort((a, b) => a - b);
+  const toIndexLookup = (arr) => arr.reduce((acc, x, i) => acc.set(x, i), new Map());
+  return (nums) => {
+    const sorted = toSorted([...new Set(nums)]);
+    const indexLookup = toIndexLookup(nums);
+    const counts = Array(nums.length).fill(0);
+    for (const current of sorted) {
+      let encountered = 1;
+      for (let i = indexLookup.get(current) - 1; i >= 0; i--) {
+        if (nums[i] === current) {
+          encountered++;
+        } else if (nums[i] > current) {
+          counts[i] += encountered;
+        }
+      }
+    }
+    return counts;
+  };
+})();
 
-// map each element to the index of the last occurrence in the array.
-const toIndexLookup = (arr) => arr.reduce((acc, x, i) => acc.set(x, i), new Map());
+const usingMergeSort = (nums) => {
+  if (!nums?.length) {
+    return nums;
+  }
+  const counts = Array(nums.length).fill(0);
+
+  const mergeAndCount = (left, right) => {
+    const merged = [];
+    let lIndex = 0;
+    let rIndex = 0;
+    let numSmaller = 0;
+    while (lIndex < left.length && rIndex < right.length) {
+      if (left[lIndex].value > right[rIndex].value) {
+        numSmaller++;
+        merged.push(right[rIndex++]);
+      } else {
+        counts[left[lIndex].index] += numSmaller;
+        merged.push(left[lIndex++]);
+      }
+    }
+    while (lIndex < left.length) {
+      counts[left[lIndex].index] += numSmaller;
+      merged.push(left[lIndex++]);
+    }
+    while (rIndex < right.length) {
+      merged.push(right[rIndex++]);
+    }
+    return merged;
+  };
+
+  const mergeSort = (arr) => {
+    if (arr.length < 2) {
+      return arr;
+    }
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+    return mergeAndCount(left, right);
+  };
+
+  mergeSort(nums.map((value, index) => ({ value, index })));
+  return counts;
+};
 
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
-export const countSmaller = (nums) => {
-  const sorted = toSorted([...new Set(nums)]);
-  const indexLookup = toIndexLookup(nums);
-  const counts = Array(nums.length).fill(0);
-
-  for (const current of sorted) {
-    let encountered = 1;
-    for (let i = indexLookup.get(current) - 1; i >= 0; i--) {
-      if (nums[i] === current) {
-        encountered++;
-      } else if (nums[i] > current) {
-        counts[i] += encountered;
-      }
-    }
-  }
-
-  return counts;
-};
+export const countSmaller = usingMergeSort;
