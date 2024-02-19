0ce179a2a8935c71557dd58a759024d00ccf4f0d
beakerandjake <beakerandjake@users.noreply.github.com>
2024-02-19 16:55:51 -0700

    102 - add iterative and recursive

--- src/102.js
+++ src/102.js
@@ -1,12 +1,77 @@
 /**
- * Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+ * Given the root of a binary tree, return the level order traversal of its nodes'
+ * values. (i.e., from left to right, level by level).
+ *
+ *
+ *
+ * Example 1:
+ *
+ * [https://assets.leetcode.com/uploads/2021/02/19/tree1.jpg]
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[9,20],[15,7]]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1]
+ * Output: [[1]]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *  * The number of nodes in the tree is in the range [0, 2000].
+ *  * -1000 <= Node.val <= 1000
+ *
+ *
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal
  */
 
 /**
- * @param {TreeNode} root
- * @return {number[][]}
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-export const levelOrder = (root) => {
+
+// level order using recursion
+const recursiveVersion = (root) => {
+  if (!root) {
+    return [];
+  }
+  const toReturn = [];
+  const recursive = (node, level) => {
+    if (!node) {
+      return;
+    }
+    if (!toReturn[level]) {
+      toReturn[level] = [node.val];
+    } else {
+      toReturn[level].push(node.val);
+    }
+    recursive(node.left, level + 1);
+    recursive(node.right, level + 1);
+  };
+  recursive(root, 0);
+  return toReturn;
+};
+
+// level order using queue
+const iterativeVersion = (root) => {
   if (!root) {
     return [];
   }
@@ -14,11 +79,10 @@ export const levelOrder = (root) => {
   const queue = [{ node: root, level: 0 }];
   while (queue.length) {
     const { node, level } = queue.shift();
-
-    if (toReturn[level]) {
-      toReturn[level].push(node.val);
-    } else {
+    if (!toReturn[level]) {
       toReturn[level] = [node.val];
+    } else {
+      toReturn[level].push(node.val);
     }
 
     if (node.left) {
@@ -30,3 +94,9 @@ export const levelOrder = (root) => {
   }
   return toReturn;
 };
+
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+export const levelOrder = recursiveVersion;
