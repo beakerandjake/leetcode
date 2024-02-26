3c7a3583efc3cfe83b45b9b7e7b9a6aaef0286b0
beakerandjake <beakerandjake@users.noreply.github.com>
2024-02-26 09:28:32 -0700

    practice 206

--- src/206.js
+++ src/206.js
@@ -1,7 +1,49 @@
-/* eslint-disable no-param-reassign */
-
 /**
- * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * Given the head of a singly linked list, reverse the list, and return the
+ * reversed list.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * [https://assets.leetcode.com/uploads/2021/02/19/rev1ex1.jpg]
+ *
+ *
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ *
+ *
+ * Example 2:
+ *
+ * [https://assets.leetcode.com/uploads/2021/02/19/rev1ex2.jpg]
+ *
+ *
+ * Input: head = [1,2]
+ * Output: [2,1]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = []
+ * Output: []
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *  * The number of nodes in the list is the range [0, 5000].
+ *  * -5000 <= Node.val <= 5000
+ *
+ *
+ *
+ * Follow up: A linked list can be reversed either iteratively or recursively.
+ * Could you implement both?
+ *
+ *
+ *
+ * https://leetcode.com/problems/reverse-linked-list
  */
 
 /**
@@ -12,56 +54,18 @@
  * }
  */
 
-const insertAfterTail = (head) => {
-  const getTail = (node) => {
-    let toReturn = node;
-    while (toReturn.next) {
-      toReturn = toReturn.next;
-    }
-    return toReturn;
-  };
-
-  if (!head?.next) {
-    return head;
-  }
-  const tail = getTail(head);
-  let currentHead = head;
-  while (currentHead && currentHead !== tail) {
-    const nextHead = currentHead.next;
-    currentHead.next = tail.next;
-    tail.next = currentHead;
-    currentHead = nextHead;
-  }
-  return tail;
-};
-
-const setToPreviousIterative = (head) => {
-  let previous = null;
-  let current = head;
-  while (current) {
-    const tempNext = current.next;
-    current.next = previous;
-    previous = current;
-    current = tempNext;
-  }
-  return previous;
-};
-
-const setToPreviousRecursive = (head) => {
-  const recursive = (current) => {
-    if (!current?.next) {
-      return current;
-    }
-    const p = recursive(current.next);
-    current.next.next = current;
-    current.next = null;
-    return p;
-  };
-  return recursive(head);
-};
-
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-export const reverseList = setToPreviousRecursive;
+export const reverseList = (head) => {
+  let previous = null;
+  let current = head;
+  while (current) {
+    const temp = current.next;
+    current.next = previous;
+    previous = current;
+    current = temp;
+  }
+  return previous;
+};
