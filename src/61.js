0102a3411d65ab5bf28370cc7f256413f9d3539c
beakerandjake <beakerandjake@users.noreply.github.com>
2024-02-26 10:55:14 -0700

    practice 61

--- src/61.js
+++ src/61.js
@@ -1,8 +1,40 @@
 /**
  * Given the head of a linked list, rotate the list to the right by k places.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * [https://assets.leetcode.com/uploads/2020/11/13/rotate1.jpg]
+ *
+ *
+ * Input: head = [1,2,3,4,5], k = 2
+ * Output: [4,5,1,2,3]
+ *
+ *
+ * Example 2:
+ *
+ * [https://assets.leetcode.com/uploads/2020/11/13/roate2.jpg]
+ *
+ *
+ * Input: head = [0,1,2], k = 4
+ * Output: [2,0,1]
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *  * The number of nodes in the list is in the range [0, 500].
+ *  * -100 <= Node.val <= 100
+ *  * 0 <= k <= 2 * 109
+ *
+ *
+ *
+ * https://leetcode.com/problems/rotate-list
  */
 
-/**
+/*
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
@@ -10,67 +42,40 @@
  * }
  */
 
-const simple = (() => {
-  const size = (head) => {
-    let current = head;
-    let count = 0;
-    while (current) {
-      count++;
-      current = current.next;
-    }
-    return count;
-  };
+const findTail = (head) => {
+  let count = 1;
+  let previous = head;
+  let current = head.next;
+  while (current) {
+    previous = current;
+    current = current.next;
+    count++;
+  }
+  return { tail: previous, length: count };
+};
 
-  const nodeAtIndex = (head, index) => {
-    let currentIndex = 0;
-    let currentNode = head;
-    while (currentIndex !== index) {
-      currentIndex++;
-      currentNode = currentNode.next;
-    }
-    return currentNode;
-  };
-
-  const tail = (head) => {
-    let current = head;
-    while (current.next) {
-      current = current.next;
-    }
-    return current;
-  };
-
-  const predecessor = (head, node) => {
-    if (node === head) {
-      return null;
-    }
-    let current = head;
-    while (current.next !== node) {
-      current = current.next;
-    }
-    return current;
-  };
-
-  return (head, k) => {
-    if (!head || k === 0) {
-      return head;
-    }
-
-    const listSize = size(head);
-    const shiftCount = k % listSize;
-    if (shiftCount === 0) {
-      return head;
-    }
-
-    const newHead = nodeAtIndex(head, listSize - shiftCount);
-    predecessor(head, newHead).next = null;
-    tail(newHead).next = head;
-    return newHead;
-  };
-})();
+const nodeAtIndex = (list, index) =>
+  index <= 0 ? list : nodeAtIndex(list.next, index - 1);
 
 /**
  * @param {ListNode} head
  * @param {number} k
  * @return {ListNode}
  */
-export const rotateRight = simple;
+export const rotateRight = (head, k) => {
+  if (!head) {
+    return null;
+  }
+
+  const { length, tail } = findTail(head);
+  const kActual = k % length;
+  // bail if list is not modified by rotations.
+  if (kActual === 0) {
+    return head;
+  }
+  tail.next = head;
+  const newTail = nodeAtIndex(head, length - kActual - 1);
+  const newHead = newTail.next;
+  newTail.next = null;
+  return newHead;
+};
--- tests/61.test.js
+++ tests/61.test.js
@@ -5,6 +5,7 @@ describe('61. Rotate List', () => {
   [
     [[1, 2, 3, 4, 5], 2, [4, 5, 1, 2, 3]],
     [[0, 1, 2], 4, [2, 0, 1]],
+    [[1, 2], 1, [2, 1]],
   ].forEach(([input, k, expected]) => {
     test(`${arrToStr(input)},${k} -> ${expected}`, () => {
       const result = rotateRight(arrayToLinkedList(input), k);
