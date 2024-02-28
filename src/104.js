068525159d52be6f71cbc105501c8ba2ceda8102
beakerandjake <beakerandjake@users.noreply.github.com>
2024-02-28 08:48:26 -0700

    reset 104

--- src/104.js
+++ src/104.js
@@ -1,6 +1,37 @@
 /**
  * Given the root of a binary tree, return its maximum depth.
- * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ *
+ * A binary tree's maximum depth is the number of nodes along the longest path from
+ * the root node down to the farthest leaf node.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * [https://assets.leetcode.com/uploads/2020/11/26/tmp-tree.jpg]
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1,null,2]
+ * Output: 2
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *  * The number of nodes in the tree is in the range [0, 104].
+ *  * -100 <= Node.val <= 100
+ *
+ *
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree
  */
 
 /**
@@ -11,47 +42,13 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
-
-const depthRecursive = (node) => {
-  if (!node) {
-    return 0;
-  }
-  return Math.max(depthRecursive(node.left), depthRecursive(node.right)) + 1;
-};
-
-const depthTailRecursive = (node, depth) => {
-  if (!node) {
-    return depth;
-  }
-  return Math.max(
-    depthTailRecursive(node.left, depth + 1),
-    depthTailRecursive(node.right, depth + 1)
-  );
-};
-
-const depthIterative = (root) => {
-  if (!root) {
-    return 0;
-  }
-
-  let deepest = 0;
-  const queue = [{ node: root, depth: 1 }];
-  while (queue.length) {
-    const { node, depth } = queue.pop();
-    deepest = Math.max(deepest, depth);
-    if (node.left) {
-      queue.push({ node: node.left, depth: depth + 1 });
-    }
-    if (node.right) {
-      queue.push({ node: node.right, depth: depth + 1 });
-    }
-  }
-
-  return deepest;
-};
-
 /**
  * @param {TreeNode} root
  * @return {number}
  */
-export const maxDepth = depthRecursive;
+export const maxDepth = (root) => {
+  if (!root) {
+    return 0;
+  }
+  return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));
+};
