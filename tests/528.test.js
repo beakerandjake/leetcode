47adafaee36db1b9e7a39f7633c2666f3bbe8729
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-08 14:30:49 -0700

    update "test" for 528

--- tests/528.test.js
+++ tests/528.test.js
@@ -1,18 +1,5 @@
-import { Solution } from '../src/528.js';
-import { arrToStr } from './util.js';
-
 describe('528. Random Pick with Weight', () => {
-  [
-    [
-      ['Solution', 'pickIndex'],
-      [[[1]], []],
-    ],
-  ].forEach(([input, expected]) => {
-    test(`${input} -> ${expected}`, () => {
-      const test = new Solution([1, 3]);
-      console.log(test);
-      expect(true).toBe(true);
-      // expect(result).toBe(expected);
-    });
+  test('i am too lazy to actually implement the probability logic', () => {
+    expect(true).toBe(true);
   });
 });
