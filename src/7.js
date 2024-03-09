c4e0e291f4de1dd51b4449eed0ec9bd6856df065
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-08 18:07:27 -0700

    solve 7

--- src/7.js
+++ src/7.js
@@ -40,8 +40,62 @@
  * https://leetcode.com/problems/reverse-integer
  */
 
+// returns an array of digits which make up the number.
+const toDigits = (number) => {
+  const results = [];
+  let remaining = Math.abs(number);
+  while (remaining) {
+    results.push(`${remaining % 10}`);
+    remaining = Math.floor(remaining / 10);
+  }
+  return results.reverse();
+};
+
+// the digits of the max positive number.
+const MAX_POSITIVE = toDigits(2 ** 31 - 1);
+
+// the digits of the smallest negative number.
+const MAX_NEGATIVE = toDigits(2 ** 31);
+
+// checks if the number formed by the digits is less than or equal to the number formed by the max digits
+const isSafeInteger = (digits, maxDigits) => {
+  if (digits.length > maxDigits.length) {
+    return false;
+  }
+  if (digits.length < maxDigits.length) {
+    return true;
+  }
+  for (let i = 0; i < digits.length; i++) {
+    if (digits[i] > maxDigits[i]) {
+      return false;
+    }
+    if (digits[i] < maxDigits[i]) {
+      return true;
+    }
+  }
+  return true;
+};
+
+// converts the digits into a positive or negative number.
+const toNumber = (digits, sign) => {
+  let number = 0;
+  for (let i = 0; i < digits.length; i++) {
+    number += 10 ** (digits.length - i - 1) * digits[i];
+  }
+  return number * sign;
+};
+
 /**
  * @param {number} x
  * @return {number}
  */
-export const reverse = (x) => {};
+export const reverse = (x) => {
+  if (!x) {
+    return x;
+  }
+  const sign = Math.sign(x);
+  const reversed = toDigits(x).reverse();
+  return isSafeInteger(reversed, sign === 1 ? MAX_POSITIVE : MAX_NEGATIVE)
+    ? toNumber(reversed, sign)
+    : 0;
+};
--- tests/7.test.js
+++ tests/7.test.js
@@ -1,10 +1,15 @@
-import { reverse } from "../src/7.js";
-import { arrToStr } from "./util.js";
+import { reverse } from '../src/7.js';
+import { arrToStr } from './util.js';
 
-describe("7. Reverse Integer", () => {
+describe('7. Reverse Integer', () => {
   [
-    // replace with real test data
-    [true, false],
+    [123, 321],
+    [-123, -321],
+    [120, 21],
+    [1111111119, 0],
+    [0, 0],
+    [-2147483412, -2143847412],
+    [1534236469, 0],
   ].forEach(([input, expected]) => {
     test(`${input} -> ${expected}`, () => {
       const result = reverse(input);
