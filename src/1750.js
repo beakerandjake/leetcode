af587142c2f4418ccbf5c29f41584648c5d045f2
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-15 10:10:21 -0700

    better solution 1750

--- src/1750.js
+++ src/1750.js
@@ -56,30 +56,71 @@
  * https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends
  */
 
-const trimEnd = (s, char) => {
-  if (!s?.length) {
-    return '';
-  }
-  return s.at(-1) === char ? trimEnd(s.slice(0, -1), char) : s;
-};
+const simple = (() => {
+  const trimEnd = (s, char) => {
+    if (!s?.length) {
+      return '';
+    }
+    return s.at(-1) === char ? trimEnd(s.slice(0, -1), char) : s;
+  };
 
-const trimStart = (s, char) => {
-  if (!s?.length) {
-    return '';
-  }
-  return s.at(0) === char ? trimStart(s.slice(1), char) : s;
-};
+  const trimStart = (s, char) => {
+    if (!s?.length) {
+      return '';
+    }
+    return s.at(0) === char ? trimStart(s.slice(1), char) : s;
+  };
+
+  const minimumLength = (s) => {
+    if (!s || s.length <= 1) {
+      return s?.length || 0;
+    }
+    if (s.at(0) !== s.at(-1)) {
+      return s.length;
+    }
+    return minimumLength(trimEnd(trimStart(s, s.at(0)), s.at(0)));
+  };
+
+  return minimumLength;
+})();
+
+const inPlace = (() => {
+  const trimLeft = (str, target, index) => {
+    if (index >= str.length) {
+      return index;
+    }
+    return str[index] === target ? trimLeft(str, target, index + 1) : index;
+  };
+
+  const trimRight = (str, target, index) => {
+    if (index < 0) {
+      return index;
+    }
+    return str[index] === target ? trimRight(str, target, index - 1) : index;
+  };
+
+  const minimum = (str, left, right) => {
+    if (left > right) {
+      return 0;
+    }
+    if (left === right) {
+      return 1;
+    }
+    if (str.at(left) !== str.at(right)) {
+      return right - left + 1;
+    }
+    return minimum(
+      str,
+      trimLeft(str, str.at(left), left),
+      trimRight(str, str.at(right), right)
+    );
+  };
+
+  return (str) => minimum(str, 0, str.length - 1);
+})();
 
 /**
  * @param {string} s
  * @return {number}
  */
-export const minimumLength = (s) => {
-  if (!s || s.length <= 1) {
-    return s?.length || 0;
-  }
-  if (s.at(0) !== s.at(-1)) {
-    return s.length;
-  }
-  return minimumLength(trimEnd(trimStart(s, s.at(0)), s.at(0)));
-};
+export const minimumLength = inPlace;
