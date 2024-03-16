a142ec87aa328548cc81a79ce21cd61343808fe9
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-15 17:32:52 -0700

    solve 53

--- tests/53.test.js
+++ tests/53.test.js
@@ -1,12 +1,13 @@
-import { maxSubArray } from "../src/53.js";
-import { arrToStr } from "./util.js";
+import { maxSubArray } from '../src/53.js';
+import { arrToStr } from './util.js';
 
-describe("53. Maximum Subarray", () => {
+describe('53. Maximum Subarray', () => {
   [
-    // replace with real test data
-    [true, false],
+    [[-2, 1, -3, 4, -1, 2, 1, -5, 4], 6],
+    [[1], 1],
+    [[5, 4, -1, 7, 8], 23],
   ].forEach(([input, expected]) => {
-    test(`${input} -> ${expected}`, () => {
+    test(`${arrToStr(input)} -> ${expected}`, () => {
       const result = maxSubArray(input);
       expect(result).toBe(expected);
     });
