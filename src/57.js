a235d58604571d29ae3493afdd76f09ac0fff641
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-17 12:20:32 -0700

    re-solve 57

--- src/57.js
+++ src/57.js
@@ -1,81 +1,122 @@
 /**
- * You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
- * Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+ * You are given an array of non-overlapping intervals intervals where intervals[i]
+ * = [starti, endi] represent the start and the end of the ith interval and
+ * intervals is sorted in ascending order by starti. You are also given an interval
+ * newInterval = [start, end] that represents the start and end of another
+ * interval.
+ *
+ * Insert newInterval into intervals such that intervals is still sorted in
+ * ascending order by starti and intervals still does not have any overlapping
+ * intervals (merge overlapping intervals if necessary).
+ *
  * Return intervals after the insertion.
+ *
+ * Note that you don't need to modify intervals in-place. You can make a new array
+ * and return it.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+ * Output: [[1,5],[6,9]]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+ * Output: [[1,2],[3,10],[12,16]]
+ * Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *  * 0 <= intervals.length <= 104
+ *  * intervals[i].length == 2
+ *  * 0 <= starti <= endi <= 105
+ *  * intervals is sorted by starti in ascending order.
+ *  * newInterval.length == 2
+ *  * 0 <= start <= end <= 105
+ *
+ *
+ *
+ * https://leetcode.com/problems/insert-interval
  */
 
-const firstPass = (() => {
-  const isBefore = (a, b) => a[1] < b[0];
+// returns the start of the interval
+const start = (interval) => interval[0];
 
-  const isAfter = (a, b) => a[0] > b[1];
+// returns the end of the interval
+const end = (interval) => interval[1];
 
-  return (intervals, newInterval) => {
-    if (intervals.length <= 0) {
-      return [newInterval];
-    }
-    const toReturn = [];
-    const toInsert = newInterval;
-    let inserted = false;
-    for (const current of intervals) {
-      if (isAfter(toInsert, current)) {
-        toReturn.push(current);
-      } else if (isBefore(toInsert, current)) {
-        if (!inserted) {
-          toReturn.push(toInsert);
-          inserted = true;
+// returns true if interval a comes before interval b
+const comesBefore = (a, b) => end(a) < start(b);
+
+// returns true if interval a comes after interval
+const comesAfter = (a, b) => start(a) > end(b);
+
+// returns true if a and b overlap
+const overlaps = (a, b) => !comesBefore(a, b) && !comesAfter(a, b);
+
+// merges the overlapping intervals into one interval.
+const merge = (a, b) =>
+  a && b ? [Math.min(start(a), start(b)), Math.max(end(a), end(b))] : a || b;
+
+// merge an array of sorted overlapping intervals into a single interval.
+const compress = (overlapping) => merge(overlapping.at(0), overlapping.at(-1));
+
+const recursiveSolution = (intervals, newInterval) => {
+  if (!intervals?.length) {
+    return [newInterval];
+  }
+  if (!newInterval) {
+    return intervals;
+  }
+  if (comesBefore(newInterval, intervals[0])) {
+    return [newInterval, ...intervals];
+  }
+  if (comesAfter(newInterval, intervals[0])) {
+    return [intervals[0], ...recursiveSolution(intervals.slice(1), newInterval)];
+  }
+  return recursiveSolution(intervals.slice(1), merge(newInterval, intervals[0]));
+};
+
+const simpleSolution = (intervals, newInterval) => [
+  // intervals that come before
+  ...intervals.filter((x) => comesBefore(x, newInterval)),
+  // invervals that overlap (merged together into one interval)
+  merge(compress(intervals.filter((x) => overlaps(x, newInterval))), newInterval),
+  // intervals that come after
+  ...intervals.filter((x) => comesAfter(x, newInterval)),
+];
+
+const partitionSolution = (() => {
+  const partition = (intervals, newInterval) =>
+    intervals.reduce(
+      (acc, x) => {
+        if (comesBefore(x, newInterval)) {
+          acc[0].push(x);
+        } else if (comesAfter(x, newInterval)) {
+          acc[2].push(x);
+        } else {
+          acc[1].push(x);
         }
-        toReturn.push(current);
-      } else {
-        toInsert[0] = Math.min(toInsert[0], current[0]);
-        toInsert[1] = Math.max(toInsert[1], current[1]);
-      }
-    }
-    if (!inserted) {
-      toReturn.push(toInsert);
-    }
-
-    return toReturn;
-  };
-})();
-
-const slicing = (() => {
-  const isBefore = (a, b) => a[1] < b[0];
-
-  const overlaps = ([s1, e1], [s2, e2]) => s1 <= e2 && e1 >= s2;
-
-  const findInsertIndex = (intervals, newInterval) => {
-    for (let i = 0; i < intervals.length; i++) {
-      if (!isBefore(intervals[i], newInterval)) {
-        return i;
-      }
-    }
-    return intervals.length;
-  };
+        return acc;
+      },
+      [[], [], []]
+    );
 
   return (intervals, newInterval) => {
-    if (intervals.length <= 0) {
-      return [newInterval];
-    }
-
-    const insertAt = findInsertIndex(intervals, newInterval);
-
-    if (insertAt === intervals.length) {
-      return [...intervals, newInterval];
-    }
-
-    let i = insertAt;
-    const merged = newInterval;
-    while (i < intervals.length) {
-      const current = intervals[i];
-      if (!overlaps(merged, current)) {
-        break;
-      }
-      merged[0] = Math.min(merged[0], current[0]);
-      merged[1] = Math.max(merged[1], current[1]);
-      i++;
-    }
-
-    return [...intervals.slice(0, insertAt), merged, ...intervals.slice(i)];
+    const [before, overlapping, after] = partition(intervals, newInterval);
+    return [
+      ...before,
+      overlapping.length ? merge(compress(overlapping), newInterval) : newInterval,
+      ...after,
+    ];
   };
 })();
 
@@ -84,4 +125,4 @@ const slicing = (() => {
  * @param {number[]} newInterval
  * @return {number[][]}
  */
-export const insert = slicing;
+export const insert = simpleSolution;
