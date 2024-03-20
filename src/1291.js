196f4d85e1a9e50c653ff945d7c684b3af5a1279
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-20 16:51:55 -0700

    solve 1291

--- src/1291.js
+++ src/1291.js
@@ -30,9 +30,81 @@
  * https://leetcode.com/problems/sequential-digits
  */
 
+// invokes the visitFn for every substring of the str.
+const substrings = (str) => {
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i; j < str.length; j++) {
+      result.push(str.slice(i, j + 1));
+    }
+  }
+  return result;
+};
+
+const bruteForce = (() => {
+  // contains all possible sequential numbers for the input range.
+  const lookup = new Set(substrings('123456789').map((x) => Number.parseInt(x, 10)));
+
+  // invokes the visitFn on every number in range (from-to) inclusive.
+  const iterateRange = (from, to, visitFn) => {
+    for (let i = from; i <= to; i++) {
+      visitFn(i);
+    }
+  };
+
+  return (low, high) => {
+    const nums = [];
+    iterateRange(low, high, (num) => {
+      if (lookup.has(num)) {
+        nums.push(num);
+      }
+    });
+    return nums;
+  };
+})();
+
+const usingBinarySearch = (() => {
+  const lookup = substrings('123456789')
+    .map((x) => Number.parseInt(x, 10))
+    .sort((a, b) => a - b);
+
+  // returns the index of the first value higher than target
+  const findLowIndex = (arr, target) => {
+    let low = 0;
+    let high = arr.length - 1;
+    while (low <= high) {
+      const m = low + Math.floor((high - low) / 2);
+      if (arr[m] < target) {
+        low = m + 1;
+      } else {
+        high = m - 1;
+      }
+    }
+    return low;
+  };
+
+  // returns the index of the first value larger than target.
+  const findHighIndex = (arr, target) => {
+    let low = 0;
+    let high = arr.length - 1;
+    while (low <= high) {
+      const m = low + Math.floor((high - low) / 2);
+      if (target < arr[m]) {
+        high = m - 1;
+      } else {
+        low = m + 1;
+      }
+    }
+    return low;
+  };
+
+  return (low, high) =>
+    lookup.slice(findLowIndex(lookup, low), findHighIndex(lookup, high));
+})();
+
 /**
  * @param {number} low
  * @param {number} high
  * @return {number[]}
  */
-export const sequentialDigits = (low, high) => {};
+export const sequentialDigits = usingBinarySearch;
--- tests/1291.test.js
+++ tests/1291.test.js
@@ -1,14 +1,15 @@
-import { sequentialDigits } from "../src/1291.js";
-import { arrToStr } from "./util.js";
+import { sequentialDigits } from '../src/1291.js';
+import { arrToStr } from './util.js';
 
-describe("1291. Sequential Digits", () => {
+describe('1291. Sequential Digits', () => {
   [
-    // replace with real test data
-    [true, false],
-  ].forEach(([input, expected]) => {
-    test(`${input} -> ${expected}`, () => {
-      const result = sequentialDigits(input);
-      expect(result).toBe(expected);
+    [100, 300, [123, 234]],
+    [1000, 13000, [1234, 2345, 3456, 4567, 5678, 6789, 12345]],
+    [89, 234, [89, 123, 234]],
+  ].forEach(([a, b, expected]) => {
+    test(`${a},${b} -> ${arrToStr(expected)}`, () => {
+      const result = sequentialDigits(a, b);
+      expect(result).toEqual(expected);
     });
   });
 });
