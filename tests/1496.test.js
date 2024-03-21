7187d7329fe455ff8840d28be61f97a31a6570b7
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-21 09:55:04 -0700

    touch 1496

---
+++ tests/1496.test.js
@@ -0,0 +1,12 @@
+import { isPathCrossing } from "../src/1496.js";
+import { generateTestName } from "./util.js";
+
+describe("1496. Path Crossing", () => {
+  [].forEach((args) => {
+    const [input, expected] = args;
+    test(generateTestName(isPathCrossing, ...args), () => {
+      const result = isPathCrossing(input);
+      expect(result).toBe(expected);
+    });
+  });
+});
