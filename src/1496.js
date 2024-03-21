c013cd5863c373adfa36de206be070b01ec9d829
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-21 10:31:46 -0700

    solve 1496

--- src/1496.js
+++ src/1496.js
@@ -3,7 +3,8 @@
  * moving one unit north, south, east, or west, respectively. You start at the
  * origin (0, 0) on a 2D plane and walk on the path specified by path.
  *
- * Return true if the path crosses itself at any point, that is, if at any time you
+ * Return true if the path crosses itself at any vector
+ * , that is, if at any time you
  * are on a location you have previously visited. Return false otherwise.
  *
  *
@@ -15,7 +16,8 @@
  *
  * Input: path = "NES"
  * Output: false
- * Explanation: Notice that the path doesn't cross any point more than once.
+ * Explanation: Notice that the path doesn't cross any vector
+ *  more than once.
  *
  *
  * Example 2:
@@ -39,8 +41,48 @@
  * https://leetcode.com/problems/path-crossing
  */
 
+// returns a new vector.
+const create = (x, y) => [x, y];
+
+// returns the x component of the vector.
+const x = (vector) => vector[0];
+
+// returns the y component of the vector.
+const y = (vector) => vector[1];
+
+// returns a hash representation of the vector.
+const hash = (vector) => `<${x(vector)},${y(vector)}>`;
+
+// adds vectors a and b
+const add = (a, b) => create(x(a) + x(b), y(a) + y(b));
+
+// maps an input direction to a vector.
+const directions = new Map([
+  ['N', create(0, 1)],
+  ['S', create(0, -1)],
+  ['E', create(1, 0)],
+  ['W', create(-1, 0)],
+]);
+
+// returns a function which returns true if the position has been previously visited.
+const historyChecker = (origin) => {
+  const visited = new Set([hash(origin)]);
+  return (position) =>
+    visited.has(hash(position)) ? true : !visited.add(hash(position));
+};
+
 /**
  * @param {string} path
  * @return {boolean}
  */
-export const isPathCrossing = (path) => {};
+export const isPathCrossing = (path) => {
+  let position = create(0, 0);
+  const hasVisited = historyChecker(position);
+  for (const instruction of path) {
+    position = add(position, directions.get(instruction));
+    if (hasVisited(position)) {
+      return true;
+    }
+  }
+  return false;
+};
--- tests/1496.test.js
+++ tests/1496.test.js
@@ -1,8 +1,12 @@
-import { isPathCrossing } from "../src/1496.js";
-import { generateTestName } from "./util.js";
+import { isPathCrossing } from '../src/1496.js';
+import { generateTestName } from './util.js';
 
-describe("1496. Path Crossing", () => {
-  [].forEach((args) => {
+describe('1496. Path Crossing', () => {
+  [
+    ['NES', false],
+    ['NESWW', true],
+    ['NNSWWEWSSESSWENNW', true],
+  ].forEach((args) => {
     const [input, expected] = args;
     test(generateTestName(isPathCrossing, ...args), () => {
       const result = isPathCrossing(input);
