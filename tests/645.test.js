c4640dd1077f7008add3c7dddbd7abc49baea253
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-21 09:07:12 -0700

    touch 645

--- tests/645.test.js
+++ tests/645.test.js
@@ -2,7 +2,7 @@ import { findErrorNums } from "../src/645.js";
 import { generateTestName } from "./util.js";
 
 describe("645. Set Mismatch", () => {
-  [[true, false]].forEach((args) => {
+  [].forEach((args) => {
     const [input, expected] = args;
     test(generateTestName(findErrorNums, ...args), () => {
       const result = findErrorNums(input);
