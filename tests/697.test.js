bdf1c6b369259dc1ae0a15268305bb2fa39a9a37
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-22 16:42:37 -0700

    touch 697

---
+++ tests/697.test.js
@@ -0,0 +1,15 @@
+import { findShortestSubArray } from "../src/697.js";
+import { generateTestName } from "./util.js";
+
+describe("697. Degree of an Array", () => {
+  [
+    [[1, 2, 2, 3, 1], 2],
+    [[1, 2, 2, 3, 1, 4, 2], 6],
+  ].forEach((args) => {
+    const [nums, expected] = args;
+    test(generateTestName(findShortestSubArray, ...args), () => {
+      const result = findShortestSubArray(nums);
+      expect(result).toBe(expected);
+    });
+  });
+});
