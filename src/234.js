a5fe27c1f471ca9499692fa7db9145047b1805f9
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-21 19:12:29 -0700

    re-solve 234

--- src/234.js
+++ src/234.js
@@ -46,26 +46,105 @@
  * }
  */
 
-const iterateReverse = (head, visitFn) => {
-  if (!head) {
-    return;
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
   }
-  iterateReverse(head.next, visitFn);
-  visitFn(head);
-};
+}
+
+const reverseAndCompare = (() => {
+  const copy = (list) => (list ? new ListNode(list.val, copy(list.next)) : null);
+
+  const reverse = (list) => {
+    if (!list?.next) {
+      return list;
+    }
+    const predecessor = reverse(list.next);
+    list.next.next = list;
+    list.next = null;
+    return predecessor;
+  };
+
+  const equals = (a, b) => {
+    if (!a && !b) {
+      return true;
+    }
+    if (!a || !b) {
+      return false;
+    }
+    return a.val === b.val && equals(a.next, b.next);
+  };
+
+  return (head) => equals(reverse(copy(head)), head);
+})();
+
+const reverseMiddleHalfAndCompare = (() => {
+  const middle = (list) => {
+    if (!list) {
+      return null;
+    }
+    let slow = list;
+    let fast = list;
+    while (fast?.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  };
+
+  const reverse = (list) => {
+    if (!list?.next) {
+      return list;
+    }
+    const predecessor = reverse(list.next);
+    list.next.next = list;
+    list.next = null;
+    return predecessor;
+  };
+
+  return (head) => {
+    let a = head;
+    let b = reverse(middle(head));
+    while (b) {
+      if (a.val !== b.val) {
+        return false;
+      }
+      a = a.next;
+      b = b.next;
+    }
+    return true;
+  };
+})();
+
+const reverseIterationAndCompare = (() => {
+  const iterateReverse = (list, visitFn) => {
+    if (!list) {
+      return;
+    }
+    iterateReverse(list.next, visitFn);
+    visitFn(list.val);
+  };
+
+  return (list) => {
+    if (!list) {
+      return false;
+    }
+
+    let current = list;
+    let isValid = true;
+    iterateReverse(list, (value) => {
+      if (current.val !== value) {
+        isValid = false;
+      }
+      current = current.next;
+    });
+    return isValid;
+  };
+})();
 
 /**
  * @param {ListNode} head
  * @return {boolean}
  */
-export const isPalindrome = (head) => {
-  let toReturn = true;
-  let current = head;
-  iterateReverse(head, (node) => {
-    if (current.val !== node.val) {
-      toReturn = false;
-    }
-    current = current.next;
-  });
-  return toReturn;
-};
+export const isPalindrome = reverseAndCompare;
--- tests/234.test.js
+++ tests/234.test.js
@@ -1,12 +1,13 @@
 import { isPalindrome } from '../src/234.js';
-import { arrToStr, arrToList } from './util.js';
+import { arrToList, generateTestName } from './util.js';
 
 describe('234. Palindrome Linked List', () => {
   [
     [[1, 2, 2, 1], true],
-    [[1, 2, 3, 4], false],
-  ].forEach(([input, expected]) => {
-    test(`${arrToStr(input)} -> ${expected}`, () => {
+    [[1, 2], false],
+  ].forEach((args) => {
+    const [input, expected] = args;
+    test(generateTestName(isPalindrome, ...args), () => {
       const result = isPalindrome(arrToList(input));
       expect(result).toBe(expected);
     });
