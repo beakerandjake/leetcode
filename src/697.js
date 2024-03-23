073e2930beb60960bf031d12b2e56ac20718ea58
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-22 17:08:39 -0700

    solve 697

--- src/697.js
+++ src/697.js
@@ -41,8 +41,62 @@
  * https://leetcode.com/problems/degree-of-an-array
  */
 
+const bruteForce = (() => {
+  // returns the numbers which occur the most frequently in the array.
+  const findMostFrequent = (nums) => {
+    const toReturn = [];
+    const counts = nums.reduce((acc, x) => acc.set(x, (acc.get(x) || 0) + 1), new Map());
+    const max = Math.max(...counts.values());
+    for (const [num, count] of counts.entries()) {
+      if (count === max) {
+        toReturn.push(num);
+      }
+    }
+    return toReturn;
+  };
+
+  // returns the length of the smallest subarray which contains all instances of the target number
+  const smallestSubarray = (nums, target) => {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < nums.length && nums[left] !== target) {
+      left++;
+    }
+    while (right >= 0 && nums[right] !== target) {
+      right--;
+    }
+    return right - left + 1;
+  };
+
+  return (nums) =>
+    nums.length
+      ? Math.min(...findMostFrequent(nums).map((num) => smallestSubarray(nums, num)))
+      : 0;
+})();
+
+const usingFirstAndLastLookup = (nums) => {
+  const first = Array(nums.length).fill(null);
+  const last = Array(nums.length).fill(null);
+  const counts = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (first[nums[i]] == null) {
+      first[nums[i]] = i;
+    }
+    last[nums[i]] = i;
+    counts.set(nums[i], (counts.get(nums[i]) || 0) + 1);
+  }
+  let result = Number.MAX_SAFE_INTEGER;
+  const maxCount = Math.max(...counts.values());
+  for (const [num, count] of counts) {
+    if (count === maxCount) {
+      result = Math.min(result, last[num] - first[num] + 1);
+    }
+  }
+  return result;
+};
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
-export const findShortestSubArray = (nums) => {};
+export const findShortestSubArray = usingFirstAndLastLookup;
