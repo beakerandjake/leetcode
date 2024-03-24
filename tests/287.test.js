286242ca14d58693c0cc78a508d0363d31982a47
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-24 08:18:03 -0700

    touch 287

---
+++ tests/287.test.js
@@ -0,0 +1,16 @@
+import { findDuplicate } from "../src/287.js";
+import { generateTestName } from "./util.js";
+
+describe("287. Find the Duplicate Number", () => {
+  [
+    [[1, 3, 4, 2, 2], 2],
+    [[3, 1, 3, 4, 2], 3],
+    [[3, 3, 3, 3, 3], 3],
+  ].forEach((args) => {
+    const [nums, expected] = args;
+    test(generateTestName(findDuplicate, ...args), () => {
+      const result = findDuplicate(nums);
+      expect(result).toBe(expected);
+    });
+  });
+});
