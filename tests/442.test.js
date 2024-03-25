d44a284cadbf3aea0d46960cdd50626bdea421af
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-25 10:35:26 -0700

    touch 442

---
+++ tests/442.test.js
@@ -0,0 +1,19 @@
+import { findDuplicates } from "../src/442.js";
+import { generateTestName } from "./util.js";
+
+describe("442. Find All Duplicates in an Array", () => {
+  [
+    [
+      [4, 3, 2, 7, 8, 2, 3, 1],
+      [2, 3],
+    ],
+    [[1, 1, 2], [1]],
+    [[1], []],
+  ].forEach((args) => {
+    const [nums, expected] = args;
+    test(generateTestName(findDuplicates, ...args), () => {
+      const result = findDuplicates(nums);
+      expect(result).toEqual(expected);
+    });
+  });
+});
