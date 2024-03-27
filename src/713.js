ae07a8de9ef52506003eb62f54481708ee1f866f
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-27 11:16:46 -0700

    solve 713

--- src/713.js
+++ src/713.js
@@ -35,9 +35,63 @@
  * https://leetcode.com/problems/subarray-product-less-than-k
  */
 
+const bruteForce = (() => {
+  const subarrays = (arr, visitFn) => {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i; j < arr.length; j++) {
+        visitFn(i, j);
+      }
+    }
+  };
+
+  const product = (arr, from, to) => {
+    let total = 1;
+    for (let i = from; i <= to; i++) {
+      total *= arr[i];
+    }
+    return total;
+  };
+
+  return (nums, k) => {
+    let count = 0;
+    subarrays(nums, (from, to) => {
+      if (product(nums, from, to) < k) {
+        count++;
+      }
+    });
+    return count;
+  };
+})();
+
+const usingTwoPointers = (() => {
+  // returns the number of subarrays between the left and right index (inclusive)
+  const numSubArrays = (left, right) => right - left + 1;
+
+  return (nums, k) => {
+    let subarrayCount = 0;
+    let left = 0;
+    let right = 0;
+    let product = 1;
+    while (right < nums.length) {
+      product *= nums[right];
+      // attempt to hit target by contracting window from left.
+      while (left < right && product >= k) {
+        product /= nums[left];
+        left++;
+      }
+      if (product < k) {
+        subarrayCount += numSubArrays(left, right);
+      }
+      // expand window right.
+      right++;
+    }
+    return subarrayCount;
+  };
+})();
+
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
-export const numSubarrayProductLessThanK = (nums, k) => {};
+export const numSubarrayProductLessThanK = usingTwoPointers;
