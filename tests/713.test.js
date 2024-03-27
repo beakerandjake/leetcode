a203265589309f6674fe8622d083616db3dba7fb
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-27 10:43:02 -0700

    touch 713

---
+++ tests/713.test.js
@@ -0,0 +1,15 @@
+import { numSubarrayProductLessThanK } from "../src/713.js";
+import { generateTestName } from "./util.js";
+
+describe("713. Subarray Product Less Than K", () => {
+  [
+    [[10, 5, 2, 6], 100, 8],
+    [[1, 2, 3], 0, 0],
+  ].forEach((args) => {
+    const [nums, k, expected] = args;
+    test(generateTestName(numSubarrayProductLessThanK, ...args), () => {
+      const result = numSubarrayProductLessThanK(nums, k);
+      expect(result).toBe(expected);
+    });
+  });
+});
