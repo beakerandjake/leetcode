097328dc41e519dcf2db8da5efe8f06948623934
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-28 07:44:34 -0700

    touch 2958

---
+++ tests/2958.test.js
@@ -0,0 +1,16 @@
+import { maxSubarrayLength } from "../src/2958.js";
+import { generateTestName } from "./util.js";
+
+describe("2958. Length of Longest Subarray With at Most K Frequency", () => {
+  [
+    [[1, 2, 3, 1, 2, 3, 1, 2], 2, 6],
+    [[1, 2, 1, 2, 1, 2, 1, 2], 1, 2],
+    [[5, 5, 5, 5, 5, 5, 5], 4, 4],
+  ].forEach((args) => {
+    const [nums, k, expected] = args;
+    test(generateTestName(maxSubarrayLength, ...args), () => {
+      const result = maxSubarrayLength(nums, k);
+      expect(result).toBe(expected);
+    });
+  });
+});
