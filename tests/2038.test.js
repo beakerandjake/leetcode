87364b7dc6f63e31d938307ce5a5996a430fb8a2
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-28 14:12:52 -0700

    touch 2038

---
+++ tests/2038.test.js
@@ -0,0 +1,16 @@
+import { winnerOfGame } from "../src/2038.js";
+import { generateTestName } from "./util.js";
+
+describe("2038. Remove Colored Pieces if Both Neighbors are the Same Color", () => {
+  [
+    ["AAABABB", true],
+    ["AA", false],
+    ["ABBBBBBBAAA", false],
+  ].forEach((args) => {
+    const [colors, expected] = args;
+    test(generateTestName(winnerOfGame, ...args), () => {
+      const result = winnerOfGame(colors);
+      expect(result).toBe(expected);
+    });
+  });
+});
