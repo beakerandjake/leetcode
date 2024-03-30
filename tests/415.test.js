2ac707f5696640685b04639f14280fd40c8899c3
beakerandjake <beakerandjake@users.noreply.github.com>
2024-03-30 08:24:04 -0700

    touch 415

---
+++ tests/415.test.js
@@ -0,0 +1,16 @@
+import { addStrings } from "../src/415.js";
+import { generateTestName } from "./util.js";
+
+describe("415. Add Strings", () => {
+  [
+    ["11", "123", "134"],
+    ["456", "77", "533"],
+    ["0", "0", "0"],
+  ].forEach((args) => {
+    const [num1, num2, expected] = args;
+    test(generateTestName(addStrings, ...args), () => {
+      const result = addStrings(num1, num2);
+      expect(result).toBe(expected);
+    });
+  });
+});
