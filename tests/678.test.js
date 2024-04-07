04ab2845075da5af9de28c033435e48a8d6df688
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-07 08:26:12 -0700

    touch 678

---
+++ tests/678.test.js
@@ -0,0 +1,16 @@
+import { checkValidString } from "../src/678.js";
+import { generateTestName } from "./util.js";
+
+describe("678. Valid Parenthesis String", () => {
+  [
+    ["()", true],
+    ["(*)", true],
+    ["(*))", true],
+  ].forEach((args) => {
+    const [s, expected] = args;
+    test(generateTestName(checkValidString, ...args), () => {
+      const result = checkValidString(s);
+      expect(result).toBe(expected);
+    });
+  });
+});
