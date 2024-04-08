7a7921a4052e803fca681e48beba3b4f45502864
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-08 09:27:23 -0700

    touch 1700

---
+++ tests/1700.test.js
@@ -0,0 +1,15 @@
+import { countStudents } from "../src/1700.js";
+import { generateTestName } from "./util.js";
+
+describe("1700. Number of Students Unable to Eat Lunch", () => {
+  [
+    [[1, 1, 0, 0], [0, 1, 0, 1], 0],
+    [[1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1], 3],
+  ].forEach((args) => {
+    const [students, sandwiches, expected] = args;
+    test(generateTestName(countStudents, ...args), () => {
+      const result = countStudents(students, sandwiches);
+      expect(result).toBe(expected);
+    });
+  });
+});
