e94fe67297ef78c53f8b815170b36af0431a1d0c
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-09 13:14:08 -0700

    solve 149

--- src/149.js
+++ src/149.js
@@ -36,9 +36,48 @@
  *
  * https://leetcode.com/problems/max-points-on-a-line
  */
+/**
+ *    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) <= 2:
+            return len(points)
+        
+        def find_slope(p1, p2):
+            x1, y1 = p1
+            x2, y2 = p2
+            if x1-x2 == 0:
+                return inf
+            return (y1-y2)/(x1-x2)
+        
+        ans = 1
+        for i, p1 in enumerate(points):
+            slopes = defaultdict(int)
+            for j, p2 in enumerate(points[i+1:]):
+                slope = find_slope(p1, p2)
+                slopes[slope] += 1
+                ans = max(slopes[slope], ans)
+        return ans+1
+ */
+
+const x = (point) => point[0];
+const y = (point) => point[1];
+const slope = (a, b) => (x(a) === x(b) ? Infinity : (y(a) - y(b)) / (x(a) - x(b)));
 
 /**
  * @param {number[][]} points
  * @return {number}
  */
-export const maxPoints = (points) => {};
+export const maxPoints = (points) => {
+  if (points.length < 3) {
+    return points.length;
+  }
+  let result = 1;
+  for (let i = 0; i < points.length; i++) {
+    const slopes = new Map();
+    for (let j = i + 1; j < points.length; j++) {
+      const slp = slope(points[i], points[j]);
+      slopes.set(slp, (slopes.get(slp) || 0) + 1);
+    }
+    result = Math.max(...slopes.values(), result);
+  }
+  return result + 1;
+};
