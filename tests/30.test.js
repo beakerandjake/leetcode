1b62bdc8f670061d3073d94c83cc6cda2698c527
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-09 13:57:49 -0700

    touch 30

---
+++ tests/30.test.js
@@ -0,0 +1,16 @@
+import { findSubstring } from '../src/30.js';
+import { generateTestName } from './util.js';
+
+describe('30. Substring with Concatenation of All Words', () => {
+  [
+    ['barfoothefoobarman', ['foo', 'bar'], [0, 9]],
+    ['wordgoodgoodgoodbestword', ['word', 'good', 'best', 'word'], []],
+    ['barfoofoobarthefoobarman', ['bar', 'foo', 'the'], [6, 9, 12]],
+  ].forEach((args) => {
+    const [s, words, expected] = args;
+    test(generateTestName(findSubstring, ...args), () => {
+      const result = findSubstring(s, words);
+      expect(result).toEqual(expected);
+    });
+  });
+});
