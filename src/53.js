966f4990178ddebec6c4b072d6c91629837d76ac
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-12 10:29:57 -0700

    reset 53

--- src/53.js
+++ src/53.js
@@ -45,8 +45,64 @@
  * https://leetcode.com/problems/maximum-subarray
  */
 
+const bruteForce = (() => {
+  // returns the sum of the array.
+  const sum = (arr) => arr.reduce((acc, x) => acc + x, 0);
+
+  // invokes the visit fn on every sub array of the array.
+  const subarrays = (arr, visitFn) => {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i; j < arr.length; j++) {
+        visitFn(arr.slice(i, j + 1));
+      }
+    }
+  };
+
+  return (nums) => {
+    let max = Number.MIN_SAFE_INTEGER;
+    subarrays(nums, (arr) => {
+      max = Math.max(max, sum(arr));
+    });
+    return max;
+  };
+})();
+
+const kadane = (nums) => {
+  let best = Number.MIN_SAFE_INTEGER;
+  let current = 0;
+  for (const num of nums) {
+    current = Math.max(num, num + current);
+    best = Math.max(best, current);
+  }
+  return best;
+};
+
+const dynamicProgramming = (nums) => {
+  const memo = new Map();
+  const dp = (index, isSubarray) => {
+    if (index >= nums.length) {
+      // if reached end without starting a subarray, then not valid.
+      return isSubarray ? 0 : Number.MIN_SAFE_INTEGER;
+    }
+    const stateHash = `${index}_${isSubarray}`;
+    if (!memo.has(stateHash)) {
+      // if currently iterating a subarray, keep iterating it.
+      // otherwise best choise is either use the element to start a subarray or skip it.
+      memo.set(
+        stateHash,
+        isSubarray
+          ? Math.max(0, nums[index] + dp(index + 1, true))
+          : Math.max(dp(index + 1, false), nums[index] + dp(index + 1, true)),
+      );
+    }
+    return memo.get(stateHash);
+  };
+
+  return dp(0, false);
+};
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
-export const maxSubArray = (nums) => {};
+export const maxSubArray = dynamicProgramming;
