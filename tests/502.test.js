98b5b95669a17c337e6073ef2c753c669aa08873
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-12 11:28:16 -0700

    touch 502

---
+++ tests/502.test.js
@@ -0,0 +1,15 @@
+import { findMaximizedCapital } from '../src/502.js';
+import { generateTestName } from './util.js';
+
+describe('502. IPO', () => {
+  [
+    [2, 0, [1, 2, 3], [0, 1, 1], 4],
+    [3, 0, [1, 2, 3], [0, 1, 2], 6],
+  ].forEach((args) => {
+    const [k, w, profits, capital, expected] = args;
+    test(generateTestName(findMaximizedCapital, ...args), () => {
+      const result = findMaximizedCapital(k, w, profits, capital);
+      expect(result).toBe(expected);
+    });
+  });
+});
