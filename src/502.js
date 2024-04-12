6c35aead93385daabd6efd71f56a982cf0284c5b
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-12 13:07:15 -0700

    solve 502

--- src/502.js
+++ src/502.js
@@ -54,11 +54,162 @@
  * https://leetcode.com/problems/ipo
  */
 
+const toProject = (profit, capital) => [profit, capital];
+
+const zip = (profits, requiredCapital) => {
+  const projects = [];
+  for (let i = 0; i < profits.length; i++) {
+    projects.push(toProject(profits[i], requiredCapital[i]));
+  }
+  return projects;
+};
+
+const profit = (project) => project[0];
+
+const capital = (project) => project[1];
+
+// class Heap {
+//   #items = [];
+//   #maxIndex = -1;
+//   #priorityExtractorFn;
+
+//   constructor(priorityExtractorFn = (x) => x) {
+//     this.#priorityExtractorFn = priorityExtractorFn;
+//   }
+
+//   push(...item) {
+//     this.#items.push(...item);
+//     this.#maxIndex = this.#findMax();
+//   }
+
+//   peek() {
+//     if (this.#maxIndex === -1) {
+//       return null;
+//     }
+//     return this.#items[this.#maxIndex];
+//   }
+
+//   pop() {
+//     if (this.#maxIndex === -1) {
+//       return null;
+//     }
+//     const toReturn = this.#items[this.#maxIndex];
+//     this.#items.splice(this.#maxIndex, 1);
+//     this.#maxIndex = this.#findMax();
+//     return toReturn;
+//   }
+
+//   size() {
+//     return this.#items.length;
+//   }
+
+//   #findMax() {
+//     if (!this.#items.length) {
+//       return -1;
+//     }
+//     let maxPriority = this.#priorityExtractorFn(this.#items[0]);
+//     let maxIndex = 0;
+//     for (let i = 1; i < this.#items.length; i++) {
+//       if (this.#priorityExtractorFn(this.#items[i]) > maxPriority) {
+//         maxPriority = this.#priorityExtractorFn(this.#items[i]);
+//         maxIndex = i;
+//       }
+//     }
+//     return maxIndex;
+//   }
+// }
+
+class MaxHeap {
+  #items = [];
+  #size = 0;
+
+  get size() {
+    return this.#size;
+  }
+
+  push(key, value) {
+    this.#size++;
+    this.#items[this.#size] = { key, value };
+    this.#bubbleUp();
+  }
+
+  peek() {
+    return this.#items[1].value;
+  }
+
+  #bubbleUp() {
+    const parent = (i) => Math.floor(i / 2);
+
+    let i = this.#size;
+    while (i > 1 && this.#items[i].key > this.#items[parent(i)].key) {
+      this.#swap(i, parent(i));
+      i = parent(i);
+    }
+  }
+
+  pop() {
+    if (!this.#size) {
+      return undefined;
+    }
+    this.#swap(1, this.#size);
+    const toReturn = this.#items.pop();
+    this.#size--;
+    this.#bubbleDown();
+    return toReturn.value;
+  }
+
+  pushPop(key, value) {
+    this.#items[1] = { key, value };
+    this.#bubbleDown();
+  }
+
+  #bubbleDown() {
+    const left = (i) => i * 2;
+    const right = (i) => i * 2 + 1;
+    let i = 1;
+    while (left(i) <= this.#size) {
+      // choose larger child
+      const child =
+        right(i) <= this.#size && this.#items[right(i)].key > this.#items[left(i)].key
+          ? right(i)
+          : left(i);
+      // stop if child is in right place.
+      if (this.#items[i].key > this.#items[child].key) {
+        break;
+      }
+      this.#swap(i, child);
+      i = child;
+    }
+  }
+
+  #swap(a, b) {
+    [this.#items[a], this.#items[b]] = [this.#items[b], this.#items[a]];
+  }
+}
+
 /**
  * @param {number} k
  * @param {number} w
  * @param {number[]} profits
- * @param {number[]} capital
+ * @param {number[]} requiredCapital
  * @return {number}
  */
-export const findMaximizedCapital = (k, w, profits, capital) => {};
+export const findMaximizedCapital = (k, w, profits, requiredCapital) => {
+  let result = w;
+  const heap = new MaxHeap();
+  const projects = zip(profits, requiredCapital).sort((a, b) => capital(a) - capital(b));
+  let projectIndex = 0;
+  let remaining = k;
+  while (remaining && (projects.length || heap.size())) {
+    while (projectIndex < projects.length && capital(projects[projectIndex]) <= result) {
+      heap.push(profit(projects[projectIndex]), projects[projectIndex]);
+      projectIndex++;
+    }
+    if (!heap.size) {
+      break;
+    }
+    result += profit(heap.pop());
+    remaining--;
+  }
+  return result;
+};
--- tests/502.test.js
+++ tests/502.test.js
@@ -5,6 +5,7 @@ describe('502. IPO', () => {
   [
     [2, 0, [1, 2, 3], [0, 1, 1], 4],
     [3, 0, [1, 2, 3], [0, 1, 2], 6],
+    [1, 0, [1, 2, 3], [1, 1, 2], 0],
   ].forEach((args) => {
     const [k, w, profits, capital, expected] = args;
     test(generateTestName(findMaximizedCapital, ...args), () => {
