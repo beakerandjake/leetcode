6efa4718aa3ebe8831b7caa48b0965e960451343
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-12 08:05:33 -0700

    touch 42

--- src/42.js
+++ src/42.js
@@ -1,114 +1,41 @@
 /**
- * Given n non-negative integers representing an elevation map where the width of each bar is 1,
- * compute how much water it can trap after raining.
+ * Given n non-negative integers representing an elevation map where the width of
+ * each bar is 1, compute how much water it can trap after raining.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * [https://assets.leetcode.com/uploads/2018/10/22/rainwatertrap.png]
+ *
+ *
+ * Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * Output: 6
+ * Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: height = [4,2,0,3,2,5]
+ * Output: 9
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *  * n == height.length
+ *  * 1 <= n <= 2 * 104
+ *  * 0 <= height[i] <= 105
+ *
+ *
+ *
+ * https://leetcode.com/problems/trapping-rain-water
  */
-
-const countVerticallyRegex = (heights) => {
-  if (heights.length === 1) {
-    return 0;
-  }
-  const rows = [];
-  const maxHeight = Math.max(...heights);
-  for (let y = 1; y <= maxHeight; y++) {
-    const row = [];
-    for (let x = 0; x < heights.length; x++) {
-      row.push(heights[x] === 0 || heights[x] < y ? 0 : 1);
-    }
-    rows.push(row.join(''));
-  }
-  let total = 0;
-  for (const row of rows) {
-    const matches = row.match(/(?<=1)0+(?=1)/g);
-    if (!matches) {
-      continue;
-    }
-    total += matches.reduce((acc, x) => acc + x.length, 0);
-  }
-  return total;
-};
-
-const usingBuckets = (heights) => {
-  if (heights.length <= 2) {
-    return 0;
-  }
-
-  const findBucketEnd = (start) => {
-    let bestHeight = 0;
-    let bestHeightIndex = -1;
-    for (let i = start + 1; i < heights.length; i++) {
-      if (heights[i] >= heights[start]) {
-        return i - start > 1 ? i : -1;
-      }
-      if (heights[i] > bestHeight) {
-        bestHeight = heights[i];
-        bestHeightIndex = i;
-      }
-    }
-    return bestHeight > 0 && bestHeightIndex - start > 1 ? bestHeightIndex : -1;
-  };
-
-  const findBuckets = () => {
-    const buckets = [];
-    let i = 0;
-    while (i < heights.length - 1) {
-      const end = findBucketEnd(i);
-      if (end === -1) {
-        i++;
-        continue;
-      }
-      buckets.push([i, end]);
-      i = end;
-    }
-    return buckets;
-  };
-
-  const sumBuckets = (buckets) =>
-    buckets.reduce((acc, [start, end]) => {
-      const height = Math.min(heights[start], heights[end]);
-      let newTotal = acc;
-      for (let i = start + 1; i < end; i++) {
-        newTotal += height - heights[i];
-      }
-      return newTotal;
-    }, 0);
-
-  return sumBuckets(findBuckets());
-};
 
 /**
- * @param {number[]} heights
+ * @param {number[]} height
  * @return {number}
  */
-export const trap = (heights) => {
-  if (heights.length <= 2) {
-    return 0;
-  }
-
-  const leftMemo = new Map();
-  const leftMax = (index) => {
-    if (index === 0) {
-      return heights[index];
-    }
-    if (leftMemo.has(index)) {
-      return leftMemo.get(index);
-    }
-    const result = Math.max(heights[index], leftMax(index - 1));
-    leftMemo.set(index, result);
-    return result;
-  };
-
-  const rightMemo = new Map();
-  const rightMax = (index) => {
-    if (index === heights.length - 1) {
-      return heights[index];
-    }
-    if (rightMemo.has(index)) {
-      return rightMemo.get(index);
-    }
-    const result = Math.max(heights[index], rightMax(index + 1));
-    rightMemo.set(index, result);
-    return result;
-  };
-
-  return heights.reduce((acc, x, i) => acc + Math.min(leftMax(i), rightMax(i)) - x, 0);
-};
+export const trap = (height) => {};
--- tests/42.test.js
+++ tests/42.test.js
@@ -1,27 +1,14 @@
 import { trap } from '../src/42.js';
-import { arrToStr } from './util.js';
+import { generateTestName } from './util.js';
 
 describe('42. Trapping Rain Water', () => {
   [
-    [[0, 1, 0], 0],
-    [[0, 0, 0, 0], 0],
-    [[1, 1, 1, 1], 0],
     [[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6],
     [[4, 2, 0, 3, 2, 5], 9],
-    [[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6],
-    [[2, 0, 2], 2],
-    [[1, 2, 3, 4], 0],
-    [[4, 2, 3], 1],
-    [[4, 1, 2, 1, 3], 5],
-    [[3, 1, 1, 2], 2],
-    [[5, 4, 1, 2], 1],
-    [[5, 2, 2, 2, 2, 2], 0],
-    [[1, 0, 1], 1],
-    [[3, 1, 2, 0, 5, 0, 4, 1, 3], 12],
-    [[7, 1, 1, 1, 3, 1, 4], 13],
-  ].forEach(([heights, expected]) => {
-    test(`${arrToStr(heights)} -> ${expected}`, () => {
-      const result = trap(heights);
+  ].forEach((args) => {
+    const [height, expected] = args;
+    test(generateTestName(trap, ...args), () => {
+      const result = trap(height);
       expect(result).toBe(expected);
     });
   });
