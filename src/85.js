6d14c2a42d35ef787e870b9d304c3f286f36cb66
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-13 12:50:09 -0700

    first pass 85

--- src/85.js
+++ src/85.js
@@ -42,8 +42,74 @@
  * https://leetcode.com/problems/maximal-rectangle
  */
 
+const height = (matrix) => matrix.length;
+
+const width = (matrix) => matrix[0].length;
+
+const empty = (m, n) => [...Array(m)].map(() => Array(n).fill(0));
+
+const consecutiveY = (matrix) => {
+  const toReturn = empty(height(matrix), width(matrix));
+  const maxY = height(matrix) - 1;
+  for (let y = maxY; y >= 0; y--) {
+    for (let x = 0; x < width(matrix); x++) {
+      if (matrix[y][x] === '1') {
+        toReturn[y][x] = y < maxY ? toReturn[y + 1][x] + 1 : 1;
+      }
+    }
+  }
+  return toReturn;
+};
+
+const consecutiveX = (matrix) => {
+  const toReturn = empty(height(matrix), width(matrix));
+  const maxX = width(matrix) - 1;
+  for (let y = 0; y < height(matrix); y++) {
+    for (let x = maxX; x >= 0; x--) {
+      if (matrix[y][x] === '1') {
+        toReturn[y][x] = x < maxX ? toReturn[y][x + 1] + 1 : 1;
+      }
+    }
+  }
+  return toReturn;
+};
+
+const largestRect = (heights) => {
+  let maxArea = 0;
+  let minHeight = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < heights.length; i++) {
+    const w = i + 1;
+    minHeight = Math.min(minHeight, heights[i]);
+    if (minHeight === 0) {
+      break;
+    }
+    maxArea = Math.max(maxArea, w * minHeight);
+  }
+
+  return maxArea;
+};
+
 /**
  * @param {character[][]} matrix
  * @return {number}
  */
-export const maximalRectangle = (matrix) => {};
+export const maximalRectangle = (matrix) => {
+  let max = 0;
+  const yLookup = consecutiveY(matrix);
+  const xLookup = consecutiveX(matrix);
+  for (let y = 0; y < height(matrix); y++) {
+    for (let x = 0; x < width(matrix); x++) {
+      if (matrix[y][x] !== '1') {
+        continue;
+      }
+      max = Math.max(
+        max,
+        yLookup[y][x],
+        xLookup[y][x],
+        largestRect(yLookup[y].slice(x, x + xLookup[y][x])),
+      );
+    }
+  }
+
+  return max;
+};
