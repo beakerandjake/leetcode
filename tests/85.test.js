3aaca413dc91b2f03247188f1de2307b0b5a7e44
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-13 11:57:49 -0700

    touch 85

---
+++ tests/85.test.js
@@ -0,0 +1,24 @@
+import { maximalRectangle } from '../src/85.js';
+import { generateTestName } from './util.js';
+
+describe('85. Maximal Rectangle', () => {
+  [
+    [
+      [
+        ['1', '0', '1', '0', '0'],
+        ['1', '0', '1', '1', '1'],
+        ['1', '1', '1', '1', '1'],
+        ['1', '0', '0', '1', '0'],
+      ],
+      6,
+    ],
+    [[['0']], 0],
+    [[['1']], 1],
+  ].forEach((args) => {
+    const [matrix, expected] = args;
+    test(generateTestName(maximalRectangle, ...args), () => {
+      const result = maximalRectangle(matrix);
+      expect(result).toBe(expected);
+    });
+  });
+});
