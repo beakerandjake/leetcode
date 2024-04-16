90bebddb3f5e2c98d0809cf7d16c7841bb80aeaa
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-16 11:32:24 -0700

    fix expected in test

--- tests/95.test.js
+++ tests/95.test.js
@@ -6,11 +6,11 @@ describe('95. Unique Binary Search Trees II', () => {
     [
       3,
       [
-        [1, null, 2, null, 3, null, null],
-        [1, null, 3, 2, null, null, null],
-        [2, 1, 3, null, null, null, null],
-        [3, 1, null, null, 2, null, null],
-        [3, 2, null, 1, null, null, null],
+        [1, null, 2, null, 3],
+        [1, null, 3, 2],
+        [2, 1, 3],
+        [3, 1, null, null, 2],
+        [3, 2, null, 1],
       ],
     ],
   ].forEach(([n, expected]) => {
