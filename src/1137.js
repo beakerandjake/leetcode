5dcf4fc1f415d0da80d1b93fe95721ee48ea3a0b
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-24 09:06:27 -0700

    touch 1137

--- src/1137.js
+++ src/1137.js
@@ -1,36 +1,44 @@
 /**
  * The Tribonacci sequence Tn is defined as follows:
- *   T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+ *
+ * T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+ *
  * Given n, return the value of Tn.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: n = 4
+ * Output: 4
+ * Explanation:
+ * T_3 = 0 + 1 + 1 = 2
+ * T_4 = 1 + 1 + 2 = 4
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: n = 25
+ * Output: 1389537
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *  * 0 <= n <= 37
+ *  * The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 -
+ *    1.
+ *
+ *
+ *
+ * https://leetcode.com/problems/n-th-tribonacci-number
  */
-
-const topDown = (n) => {
-  const memo = new Map();
-  const recursive = (x) => {
-    if (x === 0) {
-      return 0;
-    }
-    if (x <= 2) {
-      return 1;
-    }
-    if (!memo.has(x)) {
-      memo.set(x, recursive(x - 1) + recursive(x - 2) + recursive(x - 3));
-    }
-    return memo.get(x);
-  };
-  return recursive(n);
-};
-
-const bottomUp = (n) => {
-  const history = [0, 1, 1];
-  for (let i = 3; i <= n; i++) {
-    history[i] = history[i - 1] + history[i - 2] + history[i - 3];
-  }
-  return history[n];
-};
 
 /**
  * @param {number} n
  * @return {number}
  */
-export const tribonacci = bottomUp;
+export const tribonacci = (n) => {};
--- tests/1137.test.js
+++ tests/1137.test.js
@@ -1,12 +1,14 @@
 import { tribonacci } from '../src/1137.js';
+import { generateTestName } from './util.js';
 
 describe('1137. N-th Tribonacci Number', () => {
   [
     [4, 4],
     [25, 1389537],
-  ].forEach(([input, expected]) => {
-    test(`${input} -> ${expected}`, () => {
-      const result = tribonacci(input);
+  ].forEach((args) => {
+    const [n, expected] = args;
+    test(generateTestName(tribonacci, ...args), () => {
+      const result = tribonacci(n);
       expect(result).toBe(expected);
     });
   });
