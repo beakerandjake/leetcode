a17f2f3549c50ed967648168ad72deafed2cdb08
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-29 10:42:04 -0700

    touch 152

---
+++ tests/152.test.js
@@ -0,0 +1,15 @@
+import { maxProduct } from '../src/152.js';
+import { generateTestName } from './util.js';
+
+describe('152. Maximum Product Subarray', () => {
+  [
+    [[2, 3, -2, 4], 6],
+    [[-2, 0, -1], 0],
+  ].forEach((args) => {
+    const [nums, expected] = args;
+    test(generateTestName(maxProduct, ...args), () => {
+      const result = maxProduct(nums);
+      expect(result).toBe(expected);
+    });
+  });
+});
