9ce680189bf82d673c95c5972115432ba1faacb8
beakerandjake <beakerandjake@users.noreply.github.com>
2024-05-04 07:59:13 -0700

    touch 881

---
+++ tests/881.test.js
@@ -0,0 +1,16 @@
+import { numRescueBoats } from '../src/881.js';
+import { generateTestName } from './util.js';
+
+describe('881. Boats to Save People', () => {
+  [
+    [[1, 2], 3, 1],
+    [[3, 2, 2, 1], 3, 3],
+    [[3, 5, 3, 4], 5, 4],
+  ].forEach((args) => {
+    const [people, limit, expected] = args;
+    test(generateTestName(numRescueBoats, ...args), () => {
+      const result = numRescueBoats(people, limit);
+      expect(result).toBe(expected);
+    });
+  });
+});
