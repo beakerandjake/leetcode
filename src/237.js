7a5bdb860edc0e513466b9b41f91c406348f30e4
beakerandjake <beakerandjake@users.noreply.github.com>
2024-05-05 11:28:11 -0700

    touch 237

--- src/237.js
+++ src/237.js
@@ -70,7 +70,4 @@
  * @param {ListNode} node
  * @return {void} Do not return anything, modify node in-place instead.
  */
-export const deleteNode = (node) => {
-  node.val = node.next.val;
-  node.next = node.next.next;
-};
+export const deleteNode = (node) => {};
--- tests/237.test.js
+++ tests/237.test.js
@@ -1,25 +1,15 @@
 import { deleteNode } from '../src/237.js';
-import { arrToStr, listToArr, arrToList } from './util.js';
-
-const findNode = (head, val) => {
-  if (!head) {
-    return null;
-  }
-  if (head.val === val) {
-    return head;
-  }
-  return findNode(head.next, val);
-};
+import { generateTestName } from './util.js';
 
 describe('237. Delete Node in a Linked List', () => {
   [
     [[4, 5, 1, 9], 5, [4, 1, 9]],
     [[4, 5, 1, 9], 1, [4, 5, 9]],
-  ].forEach(([input, node, expected]) => {
-    test(`${arrToStr(input)},${node} -> ${arrToStr(expected)}`, () => {
-      const list = arrToList(input);
-      deleteNode(findNode(list, node));
-      expect(listToArr(list)).toEqual(expected);
+  ].forEach((args) => {
+    const [head, node, expected] = args;
+    test(generateTestName(deleteNode, ...args), () => {
+      const result = deleteNode(head, node);
+      expect(result).toEqual(expected);
     });
   });
 });
