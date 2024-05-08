8a33aba123efaf629eabb1c7e528bd5abb493f34
beakerandjake <beakerandjake@users.noreply.github.com>
2024-05-08 11:50:32 -0700

    solve 767

--- src/767.js
+++ src/767.js
@@ -30,8 +30,51 @@
  * https://leetcode.com/problems/reorganize-string
  */
 
+import { MaxPriorityQueue } from '@datastructures-js/priority-queue';
+
+const toPriorityQueue = (str) => {
+  const charCounts = [...str].reduce(
+    (acc, x) => acc.set(x, (acc.get(x) || 0) + 1),
+    new Map(),
+  );
+  return MaxPriorityQueue.from([...charCounts.entries()]);
+};
+
 /**
- * @param {string} s
+ * @param {string} str
  * @return {string}
  */
-export const reorganizeString = (s) => {};
+export const reorganizeString = (str) => {
+  const result = [];
+  const heap = toPriorityQueue(str);
+
+  // alternately append first and second most common characters.
+  while (heap.size() > 1) {
+    const first = heap.dequeue();
+    const second = heap.dequeue();
+
+    if (!result.length || result.at(-1) !== first.element) {
+      result.push(first.element, second.element);
+    } else {
+      // handle edge case where most frequent element is equal to the last pushed character.
+      result.push(second.element, first.element);
+    }
+
+    // only re-enqueue if there are remaining characters.
+    if (first.priority > 1) {
+      heap.enqueue(first.element, first.priority - 1);
+    }
+    if (second.priority > 1) {
+      heap.enqueue(second.element, second.priority - 1);
+    }
+  }
+
+  if (heap.isEmpty()) {
+    return result.join('');
+  }
+  // if more than one remaining character then cannot rearrange string.
+  if (heap.front().priority > 1) {
+    return '';
+  }
+  return [...result, heap.front().element].join('');
+};
