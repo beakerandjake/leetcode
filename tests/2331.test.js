d69503621a357f592a01685d5f90deb3c0e4e599
beakerandjake <beakerandjake@users.noreply.github.com>
2024-05-16 08:17:58 -0700

    touch 2331

--- tests/2331.test.js
+++ tests/2331.test.js
@@ -1,13 +1,14 @@
 import { evaluateTree } from '../src/2331.js';
-import { arrToBst, arrToStr } from './util.js';
+import { generateTestName } from './util.js';
 
 describe('2331. Evaluate Boolean Binary Tree', () => {
   [
     [[2, 1, 3, null, null, 0, 1], true],
     [[0], false],
-  ].forEach(([input, expected]) => {
-    test(`${arrToStr(input)} -> ${expected}`, () => {
-      const result = evaluateTree(arrToBst(input));
+  ].forEach((args) => {
+    const [root, expected] = args;
+    test(generateTestName(evaluateTree, ...args), () => {
+      const result = evaluateTree(root);
       expect(result).toBe(expected);
     });
   });
