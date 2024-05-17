e9f9024da625abc6fbf27201f2ca544ada8643fb
beakerandjake <beakerandjake@users.noreply.github.com>
2024-05-17 08:21:04 -0700

    touch 1325

---
+++ tests/1325.test.js
@@ -0,0 +1,16 @@
+import { removeLeafNodes } from '../src/1325.js';
+import { generateTestName } from './util.js';
+
+describe('1325. Delete Leaves With a Given Value', () => {
+  [
+    [[1, 2, 3, 2, null, 2, 4], 2, [1, null, 3, null, 4]],
+    [[1, 3, 3, 3, 2], 3, [1, 3, null, null, 2]],
+    [[1, 2, null, 2, null, 2], 2, [1]],
+  ].forEach((args) => {
+    const [root, target, expected] = args;
+    test(generateTestName(removeLeafNodes, ...args), () => {
+      const result = removeLeafNodes(root, target);
+      expect(result).toEqual(expected);
+    });
+  });
+});
