12eb0176e40809f48b579e0285a4818da0d085b0
beakerandjake <beakerandjake@users.noreply.github.com>
2024-05-20 09:24:34 -0700

    touch 1863

--- tests/1863.test.js
+++ tests/1863.test.js
@@ -1,13 +1,15 @@
 import { subsetXORSum } from '../src/1863.js';
-import { arrToStr } from './util.js';
+import { generateTestName } from './util.js';
 
-describe('1863. Sum of all Subset XOR totals', () => {
+describe('1863. Sum of All Subset XOR Totals', () => {
   [
     [[1, 3], 6],
     [[5, 1, 6], 28],
-  ].forEach(([input, expected]) => {
-    test(`${arrToStr(input)} -> ${expected}`, () => {
-      const result = subsetXORSum(input);
+    [[3, 4, 5, 6, 7, 8], 480],
+  ].forEach((args) => {
+    const [nums, expected] = args;
+    test(generateTestName(subsetXORSum, ...args), () => {
+      const result = subsetXORSum(nums);
       expect(result).toBe(expected);
     });
   });
