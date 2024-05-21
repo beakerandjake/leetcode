073dfe725fe20c79f4f2f63bf873774e90b83973
beakerandjake <beakerandjake@users.noreply.github.com>
2024-05-21 09:13:47 -0700

    touch 78

--- tests/78.test.js
+++ tests/78.test.js
@@ -1,16 +1,17 @@
 import { subsets } from '../src/78.js';
-import { arrToStr } from './util.js';
+import { generateTestName } from './util.js';
 
 describe('78. Subsets', () => {
   [
     [
       [1, 2, 3],
-      [[1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], []],
+      [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]],
     ],
-    [[0], [[0], []]],
-  ].forEach(([input, expected]) => {
-    test(`${arrToStr(input)} -> ${arrToStr(expected)}`, () => {
-      const result = subsets(input);
+    [[0], [[], [0]]],
+  ].forEach((args) => {
+    const [nums, expected] = args;
+    test(generateTestName(subsets, ...args), () => {
+      const result = subsets(nums);
       expect(result).toEqual(expected);
     });
   });
