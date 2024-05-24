a74ad0f2a4d4ebe7ce19cb66a562ea34a891e918
beakerandjake <beakerandjake@users.noreply.github.com>
2024-05-24 11:17:45 -0700

    solve 1255

--- src/1255.js
+++ src/1255.js
@@ -56,10 +56,83 @@
  * https://leetcode.com/problems/maximum-score-words-formed-by-letters
  */
 
+// maps each item in the array to the number of times it occurs.
+const frequencyCount = (arr) =>
+  arr.reduce((acc, x) => acc.set(x, (acc.get(x) || 0) + 1), new Map());
+
+// returns true if the two maps overlap (b contains every key of a, and every value of b is larger than the value of a).
+const overlaps = (a, b) => {
+  for (const [k, v] of a) {
+    if (!b.has(k) || b.get(k) < v) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// returns the frequency map of all words which can be constructed using the letter bank.
+const possibleWords = (words, letterBank) =>
+  words
+    .map((word) => frequencyCount([...word]))
+    .filter((word) => overlaps(word, letterBank));
+
+// eslint-disable-next-line func-style
+function* subsets(arr) {
+  for (let mask = 2 ** arr.length - 1; mask > 0; mask--) {
+    const subset = [];
+    for (let bit = 0; bit < arr.length; bit++) {
+      if (mask & (1 << bit)) {
+        subset.push(arr[bit]);
+      }
+    }
+    yield subset;
+  }
+}
+
+// returns the new letter bank after using all the words letters.
+const useWord = (word, letterBank) => {
+  const newBank = new Map(letterBank);
+  for (const [letter, count] of word) {
+    newBank.set(letter, newBank.get(letter) - count);
+    if (newBank.get(letter) === 0) {
+      newBank.delete(letter);
+    }
+  }
+  return newBank;
+};
+
+// returns the score of the word.
+const scoreWord = (word, letterScores) =>
+  [...word].reduce(
+    (acc, [letter, count]) => acc + letterScores[letter.charCodeAt() - 97] * count,
+    0,
+  );
+
+// returns the score of the subset (returns 0 if not possible to build subset from letter bank)
+const subsetScore = (subset, letterBank, letterScores) => {
+  let score = 0;
+  let remaining = new Map(letterBank);
+  for (const word of subset) {
+    // if not possible to build this word, not possible to build subset.
+    if (!overlaps(word, remaining)) {
+      return 0;
+    }
+    remaining = useWord(word, remaining);
+    score += scoreWord(word, letterScores);
+  }
+  return score;
+};
+
 /**
  * @param {string[]} words
  * @param {character[]} letters
- * @param {number[]} score
+ * @param {number[]} letterScores
  * @return {number}
  */
-export const maxScoreWords = (words, letters, score) => {};
+export const maxScoreWords = (words, letters, letterScores) => {
+  const bank = frequencyCount(letters);
+  const scores = [...subsets(possibleWords(words, bank))].map((subset) =>
+    subsetScore(subset, bank, letterScores),
+  );
+  return Math.max(...scores, 0);
+};
