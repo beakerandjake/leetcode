677be0af3a253ccf4ddc2edab386ee2b6ec3eeeb
beakerandjake <beakerandjake@users.noreply.github.com>
2024-05-30 09:37:40 -0700

    brute force 1442

--- src/1442.js
+++ src/1442.js
@@ -41,8 +41,32 @@
  * https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor
  */
 
+const bruteForce = (() => {
+  // iterates all triplets
+  // eslint-disable-next-line func-style
+  function* triplets(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        for (let k = j; k < arr.length; k++) {
+          yield [i, j, k];
+        }
+      }
+    }
+  }
+
+  // returns the xor of the "slice" starting at from and up to but not including to.
+  const xor = (arr, from, to) => (from < to ? arr[from] ^ xor(arr, from + 1, to) : 0);
+
+  // returns the count of all triplets who were equal.
+  return (arr) =>
+    [...triplets(arr)].reduce(
+      (acc, [i, j, k]) => acc + (xor(arr, i, j) === xor(arr, j, k + 1)),
+      0,
+    );
+})();
+
 /**
  * @param {number[]} arr
  * @return {number}
  */
-export const countTriplets = (arr) => {};
+export const countTriplets = bruteForce;
