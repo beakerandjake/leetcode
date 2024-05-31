e439af740fb9bca3ad12076ee06a652d39308ba0
beakerandjake <beakerandjake@users.noreply.github.com>
2024-05-31 09:09:11 -0700

    touch 260

---
+++ tests/260.test.js
@@ -0,0 +1,25 @@
+import { singleNumber } from '../src/260.js';
+import { generateTestName } from './util.js';
+
+describe('260. Single Number III', () => {
+  [
+    [
+      [1, 2, 1, 3, 2, 5],
+      [3, 5],
+    ],
+    [
+      [-1, 0],
+      [-1, 0],
+    ],
+    [
+      [0, 1],
+      [1, 0],
+    ],
+  ].forEach((args) => {
+    const [nums, expected] = args;
+    test(generateTestName(singleNumber, ...args), () => {
+      const result = singleNumber(nums);
+      expect(result).toEqual(expected);
+    });
+  });
+});
