e5765057398474a71cde911c4d8e70bb4742bd7d
beakerandjake <beakerandjake@users.noreply.github.com>
2024-06-03 12:08:57 -0700

    re-solve 344

--- src/344.js
+++ src/344.js
@@ -1,23 +1,48 @@
 /**
- * Write a function that reverses a string.
- * The input string is given as an array of characters s.
- * You must do this by modifying the input array in-place with O(1) extra memory.
+ * Write a function that reverses a string. The input string is given as an array
+ * of characters s.
+ *
+ * You must do this by modifying the input array in-place
+ * [https://en.wikipedia.org/wiki/In-place_algorithm] with O(1) extra memory.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = ["h","e","l","l","o"]
+ * Output: ["o","l","l","e","h"]
+ *
+ *
+ * Example 2:
+ *
+ * Input: s = ["H","a","n","n","a","h"]
+ * Output: ["h","a","n","n","a","H"]
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *  * 1 <= s.length <= 105
+ *  * s[i] is a printable ascii character
+ *    [https://en.wikipedia.org/wiki/ASCII#Printable_characters].
+ *
+ *
+ *
+ * https://leetcode.com/problems/reverse-string
  */
-
-const recursive = (chars, left, right) => {
-  if (left >= right) {
-    return;
-  }
-  const temp = chars[left];
-  chars[left] = chars[right];
-  chars[right] = temp;
-  recursive(chars, left + 1, right - 1);
-};
 
 /**
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-export const reverseString = (chars) => {
-  recursive(chars, 0, chars.length - 1);
+export const reverseString = (s) => {
+  const doReverse = (left, right) => {
+    if (left >= right) {
+      return;
+    }
+    [s[left], s[right]] = [s[right], s[left]];
+    doReverse(left + 1, right - 1);
+  };
+  doReverse(0, s.length - 1);
 };
