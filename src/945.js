68c232086c33824016090f445f7f655bfaecab52
beakerandjake <beakerandjake@users.noreply.github.com>
2024-06-14 12:54:23 -0700

    solve 945

--- src/945.js
+++ src/945.js
@@ -37,8 +37,42 @@
  * https://leetcode.com/problems/minimum-increment-to-make-array-unique
  */
 
+// returns a bit vector of the specified range.
+// each index i represents the number of times that number appeared in the source arr.
+const toBitVector = (arr, range) =>
+  arr.reduce((acc, x) => {
+    acc[x]++;
+    return acc;
+  }, Array(range).fill(0));
+
+// returns the number of increments needed to make the final element in the bit vector unique
+const pushOverflow = (remaining) => {
+  if (remaining <= 1) {
+    return 0;
+  }
+  return remaining - 1 + pushOverflow(remaining - 1);
+};
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
-export const minIncrementForUnique = (nums) => {};
+export const minIncrementForUnique = (nums) => {
+  let result = 0;
+  const max = 10 ** 5;
+  // add one to bit vector because range is inclusive, add another one to hold any carry from max.
+  const bitVector = toBitVector(nums, max + 2);
+  // iterate over all possible numbers in range (also has effect of iterating numbers in asc order)
+  for (let i = 0; i <= max; i++) {
+    // skip if number is unique (or non existent)
+    if (bitVector[i] <= 1) {
+      continue;
+    }
+    // simulate leaving only one copy of this number and increment all duplicates by one
+    const carry = bitVector[i] - 1;
+    bitVector[i + 1] += carry;
+    result += carry;
+  }
+  // ensure that any overflow is handled
+  return result + pushOverflow(bitVector.at(-1));
+};
