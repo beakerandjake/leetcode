891831a71e2e43b42f754134d8b506f1179b7028
beakerandjake <beakerandjake@users.noreply.github.com>
2024-06-25 09:20:23 -0700

    touch 1038

---
+++ tests/1038.test.js
@@ -0,0 +1,21 @@
+import { bstToGst } from '../src/1038.js';
+import { generateTestName } from './util.js';
+
+describe('1038. Binary Search Tree to Greater Sum Tree', () => {
+  [
+    [
+      [4, 1, 6, 0, 2, 5, 7, null, null, null, 3, null, null, null, 8],
+      [30, 36, 21, 36, 35, 26, 15, null, null, null, 33, null, null, null, 8],
+    ],
+    [
+      [0, null, 1],
+      [1, null, 1],
+    ],
+  ].forEach((args) => {
+    const [root, expected] = args;
+    test(generateTestName(bstToGst, ...args), () => {
+      const result = bstToGst(root);
+      expect(result).toEqual(expected);
+    });
+  });
+});
