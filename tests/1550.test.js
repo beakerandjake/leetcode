0f5317293d836f4b554d8b12142d6f5649955eec
beakerandjake <beakerandjake@users.noreply.github.com>
2024-07-01 08:54:52 -0700

    touch 1550

---
+++ tests/1550.test.js
@@ -0,0 +1,15 @@
+import { threeConsecutiveOdds } from '../src/1550.js';
+import { generateTestName } from './util.js';
+
+describe('1550. Three Consecutive Odds', () => {
+  [
+    [[2, 6, 4, 1], false],
+    [[1, 2, 34, 3, 4, 5, 7, 23, 12], true],
+  ].forEach((args) => {
+    const [arr, expected] = args;
+    test(generateTestName(threeConsecutiveOdds, ...args), () => {
+      const result = threeConsecutiveOdds(arr);
+      expect(result).toBe(expected);
+    });
+  });
+});
