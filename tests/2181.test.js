b7e70e2d59b5b3baf1ddb61374e1c45fb63ac691
beakerandjake <beakerandjake@users.noreply.github.com>
2024-07-04 08:45:29 -0700

    touch 2181

---
+++ tests/2181.test.js
@@ -0,0 +1,21 @@
+import { mergeNodes } from '../src/2181.js';
+import { generateTestName } from './util.js';
+
+describe('2181. Merge Nodes in Between Zeros', () => {
+  [
+    [
+      [0, 3, 1, 0, 4, 5, 2, 0],
+      [4, 11],
+    ],
+    [
+      [0, 1, 0, 3, 0, 2, 2, 0],
+      [1, 3, 4],
+    ],
+  ].forEach((args) => {
+    const [head, expected] = args;
+    test(generateTestName(mergeNodes, ...args), () => {
+      const result = mergeNodes(head);
+      expect(result).toEqual(expected);
+    });
+  });
+});
