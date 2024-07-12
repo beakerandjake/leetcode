17c3eb20e1fe281dbbea21192da04fd5c08d0b10
beakerandjake <beakerandjake@users.noreply.github.com>
2024-07-12 09:41:12 -0700

    solve 1717

--- src/1717.js
+++ src/1717.js
@@ -43,10 +43,83 @@
  * https://leetcode.com/problems/maximum-score-from-removing-substrings
  */
 
+const bruteForce = (() => {
+  const pop = (arr) => arr.slice(0, -1);
+
+  const push = (arr, item) => [...arr, item];
+
+  return (s, x, y) => {
+    const recurse = (index, previous) => {
+      if (index >= s.length) {
+        return 0;
+      }
+      if (s[index] === 'b' && previous.at(-1) === 'a') {
+        // either remove this 'ba' substring, or don't.
+        return Math.max(
+          x + recurse(index + 1, pop(previous)),
+          recurse(index + 1, push(previous, s[index])),
+        );
+      }
+      if (s[index] === 'a' && previous.at(-1) === 'b') {
+        // either remove this 'ab' substring, or don't.
+        return Math.max(
+          y + recurse(index + 1, pop(previous)),
+          recurse(index + 1, push(previous, s[index])),
+        );
+      }
+      // not a substring of interest, just continue.
+      return recurse(index + 1, push(previous, s[index]));
+    };
+
+    return recurse(0, []);
+  };
+})();
+
+const usingStack = (() => {
+  const cons = (a, b) => [a, b];
+
+  const car = (x) => x[0];
+
+  const cdr = (x) => x[1];
+
+  // continually removes each occurrence of the substring and returns the resulting string.
+  const remove = (str, substring) => {
+    const chars = [];
+    let index = 0;
+    while (index < str.length) {
+      if (chars.at(-1) + str[index] === substring) {
+        chars.pop();
+      } else {
+        chars.push(str[index]);
+      }
+      index++;
+    }
+    return chars.join('');
+  };
+
+  // continually replaces all of the substrings and returns the total score.
+  const replaceAll = (str, substringPairs) => {
+    let totalScore = 0;
+    let current = str;
+    for (const pair of substringPairs) {
+      const result = remove(current, cdr(pair));
+      totalScore += Math.floor((current.length - result.length) / 2) * car(pair);
+      current = result;
+    }
+    return totalScore;
+  };
+
+  return (s, x, y) => {
+    const pX = cons(x, 'ab');
+    const pY = cons(y, 'ba');
+    return replaceAll(s, x >= y ? [pX, pY] : [pY, pX]);
+  };
+})();
+
 /**
  * @param {string} s
  * @param {number} x
  * @param {number} y
  * @return {number}
  */
-export const maximumGain = (s, x, y) => {};
+export const maximumGain = usingStack;
