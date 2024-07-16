107e90b850183b2da4bfc07c1734773590277fe4
beakerandjake <beakerandjake@users.noreply.github.com>
2024-07-16 11:07:06 -0700

    touch 236

--- src/236.js
+++ src/236.js
@@ -1,60 +1,68 @@
 /**
- * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
- * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ * Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in
+ * the tree.
+ *
+ * According to the definition of LCA on Wikipedia
+ * [https://en.wikipedia.org/wiki/Lowest_common_ancestor]: “The lowest common
+ * ancestor is defined between two nodes p and q as the lowest node in T that has
+ * both p and q as descendants (where we allow a node to be a descendant of
+ * itself).”
+ *
+ *
+ *
+ * Example 1:
+ *
+ * [https://assets.leetcode.com/uploads/2018/12/14/binarytree.png]
+ *
+ *
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * Output: 3
+ * Explanation: The LCA of nodes 5 and 1 is 3.
+ *
+ *
+ * Example 2:
+ *
+ * [https://assets.leetcode.com/uploads/2018/12/14/binarytree.png]
+ *
+ *
+ * Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * Output: 5
+ * Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = [1,2], p = 1, q = 2
+ * Output: 1
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *  * The number of nodes in the tree is in the range [2, 105].
+ *  * -109 <= Node.val <= 109
+ *  * All Node.val are unique.
+ *  * p != q
+ *  * p and q will exist in the tree.
+ *
+ *
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree
  */
 
-// const slow = (() => {
-//   const contains = (root, value) => {
-//     if (!root) {
-//       return false;
-//     }
-//     if (root.val === value) {
-//       return true;
-//     }
-//     return contains(root.left, value) || contains(root.right, value);
-//   };
-
-//   const recurse = (root, p, q) => {
-//     if (!root) {
-//       return null;
-//     }
-
-//     const pInLeft = contains(root.left, p);
-//     const pInRight = contains(root.right, p);
-//     const qInLeft = contains(root.left, q);
-//     const qInRight = contains(root.right, q);
-
-//     if (
-//       (pInLeft && qInRight) ||
-//       (pInRight && qInLeft) ||
-//       (pInLeft && root.val === q) ||
-//       (qInLeft && root.val === p) ||
-//       (pInRight && root.val === q) ||
-//       (qInRight && root.val === p)
-//     ) {
-//       return root;
-//     }
-
-//     return recurse(root.left, p, q) || recurse(root.right, p, q);
-//   };
-
-//   return recurse;
-// })();
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
 /**
  * @param {TreeNode} root
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-export const lowestCommonAncestor = (root, p, q) => {
-  if (!root) {
-    return null;
-  }
-  if (root.val === p || root.val === q) {
-    return root;
-  }
-  const inLeft = lowestCommonAncestor(root.left, p, q);
-  const inRight = lowestCommonAncestor(root.right, p, q);
-  return inLeft && inRight ? root : inLeft || inRight;
-};
+export const lowestCommonAncestor = (root, p, q) => {};
