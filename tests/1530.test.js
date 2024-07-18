535cd3b519c442c9b9ad6e68e717da4e174576ec
beakerandjake <beakerandjake@users.noreply.github.com>
2024-07-18 09:48:40 -0700

    touch 1530

---
+++ tests/1530.test.js
@@ -0,0 +1,16 @@
+import { countPairs } from '../src/1530.js';
+import { generateTestName } from './util.js';
+
+describe('1530. Number of Good Leaf Nodes Pairs', () => {
+  [
+    [[1, 2, 3, null, 4], 3, 1],
+    [[1, 2, 3, 4, 5, 6, 7], 3, 2],
+    [[7, 1, 4, 6, null, 5, 3, null, null, null, null, null, 2], 3, 1],
+  ].forEach((args) => {
+    const [root, distance, expected] = args;
+    test(generateTestName(countPairs, ...args), () => {
+      const result = countPairs(root, distance);
+      expect(result).toBe(expected);
+    });
+  });
+});
