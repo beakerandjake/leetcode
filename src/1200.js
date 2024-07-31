90f76f971b55e81121810067afa38db3f953db09
beakerandjake <beakerandjake@users.noreply.github.com>
2024-07-31 10:59:12 -0700

    solve 1200

--- src/1200.js
+++ src/1200.js
@@ -44,8 +44,58 @@
  * https://leetcode.com/problems/minimum-absolute-difference
  */
 
+const bruteForce = (() => {
+  // eslint-disable-next-line func-style
+  function* pairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        yield [arr[i], arr[j]];
+      }
+    }
+  }
+
+  // returns the minimum difference between any two adjacent elements of the array.
+  const minDiff = (arr) => {
+    let min = Number.MAX_SAFE_INTEGER;
+    for (let i = 1; i < arr.length; i++) {
+      min = Math.min(min, arr[i] - arr[i - 1]);
+    }
+    return min;
+  };
+
+  return (arr) => {
+    const sorted = [...arr].sort((a, b) => a - b);
+    const target = minDiff(sorted);
+    return [...pairs(sorted)].filter(([a, b]) => a < b && b - a === target);
+  };
+})();
+
+const usingSorting = (() => {
+  // returns a copy of the array, sorted ascending.
+  const sortedAsc = (arr) => [...arr].sort((a, b) => a - b);
+
+  // returns the min abs different between any adjacent element of the array.
+  const minAbsDiff = (arr) =>
+    arr.reduce(
+      (acc, x, i) => (i ? Math.min(acc, x - arr[i - 1]) : acc),
+      Number.MAX_SAFE_INTEGER,
+    );
+
+  return (arr) => {
+    const result = [];
+    const sorted = sortedAsc(arr);
+    const target = minAbsDiff(sorted);
+    for (let i = 1; i < arr.length; i++) {
+      if (sorted[i] - sorted[i - 1] === target) {
+        result.push([sorted[i - 1], sorted[i]]);
+      }
+    }
+    return result;
+  };
+})();
+
 /**
  * @param {number[]} arr
  * @return {number[][]}
  */
-export const minimumAbsDifference = (arr) => {};
+export const minimumAbsDifference = usingSorting;
