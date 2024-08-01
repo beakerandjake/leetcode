27a3428067097c6411f81404b902c8f88c9cc00a
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-01 11:00:33 -0700

    solve 2623

--- src/2632.js
+++ src/2632.js
@@ -82,12 +82,9 @@
  * @param {Function} fn
  * @return {Function}
  */
-var curry = function (fn) {
-  return function curried(...args) {};
-};
-
-/**
- * function sum(a, b) { return a + b; }
- * const csum = curry(sum);
- * csum(1)(2) // 3
- */
+export const curry = (fn) =>
+  function curried(...args) {
+    return args.length >= fn.length
+      ? fn(...args)
+      : (...nextArgs) => curried(...args, ...nextArgs);
+  };
--- tests/2632.test.js
+++ tests/2632.test.js
@@ -2,10 +2,13 @@ import { curry } from '../src/2632.js';
 import { generateTestName } from './util.js';
 
 describe('2632. Curry', () => {
-  [].forEach((args) => {
-    const [, expected] = args;
+  [
+    [(a, b, c) => a + b + c, [[1], [2], [3]], 6],
+    [(a, b, c) => a + b + c, [[1, 2], [3]], 6],
+  ].forEach((args) => {
+    const [fn, input, expected] = args;
     test(generateTestName(curry, ...args), () => {
-      const result = curry();
+      const result = input.reduce((acc, x) => acc(...x), curry(fn));
       expect(result).toBe(expected);
     });
   });
