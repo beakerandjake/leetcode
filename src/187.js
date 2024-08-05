1bc99124450d5b3e985992e2249006027697d62f
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-05 11:28:10 -0700

    solve 187

--- src/187.js
+++ src/187.js
@@ -36,8 +36,26 @@
  * https://leetcode.com/problems/repeated-dna-sequences
  */
 
+// iterates the string using a sliding window of size 'windowSize' and yields each window
+const slidingWindow = function* (str, windowSize) {
+  const end = str.length - windowSize;
+  for (let i = 0; i <= end; i++) {
+    yield str.slice(i, i + windowSize);
+  }
+};
+
 /**
  * @param {string} s
  * @return {string[]}
  */
-export const findRepeatedDnaSequences = (s) => {};
+export const findRepeatedDnaSequences = (s) => {
+  const result = new Set();
+  const encountered = new Set();
+  for (const sequence of slidingWindow(s, 10)) {
+    if (encountered.has(sequence)) {
+      result.add(sequence);
+    }
+    encountered.add(sequence);
+  }
+  return [...result];
+};
