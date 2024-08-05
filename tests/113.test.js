5e63369392f1e7b62388964f1de925ff48749260
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-05 10:40:26 -0700

    touch 113

---
+++ tests/113.test.js
@@ -0,0 +1,23 @@
+import { pathSum } from '../src/113.js';
+import { generateTestName } from './util.js';
+
+describe('113. Path Sum II', () => {
+  [
+    [
+      [5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1],
+      22,
+      [
+        [5, 4, 11, 2],
+        [5, 8, 4, 5],
+      ],
+    ],
+    [[1, 2, 3], 5, []],
+    [[1, 2], 0, []],
+  ].forEach((args) => {
+    const [root, targetSum, expected] = args;
+    test(generateTestName(pathSum, ...args), () => {
+      const result = pathSum(root, targetSum);
+      expect(result).toEqual(expected);
+    });
+  });
+});
