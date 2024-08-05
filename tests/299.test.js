75e9b1a2f04745e9dd483ec6b839128e0cd96050
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-05 11:34:44 -0700

    touch 299

---
+++ tests/299.test.js
@@ -0,0 +1,15 @@
+import { getHint } from '../src/299.js';
+import { generateTestName } from './util.js';
+
+describe('299. Bulls and Cows', () => {
+  [
+    ['1807', '7810', '1A3B'],
+    ['1123', '0111', '1A1B'],
+  ].forEach((args) => {
+    const [secret, guess, expected] = args;
+    test(generateTestName(getHint, ...args), () => {
+      const result = getHint(secret, guess);
+      expect(result).toBe(expected);
+    });
+  });
+});
