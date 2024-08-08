a3828f0aec11e77e036c7500507c0b49f2bf6a78
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-08 09:33:59 -0700

    solve 885

--- src/885.js
+++ src/885.js
@@ -44,6 +44,59 @@
  * https://leetcode.com/problems/spiral-matrix-iii
  */
 
+// returns a new vector.
+const vector2 = (y, x) => [y, x];
+
+// returns the y component of the vector.
+const y = (v) => v[0];
+
+// returns the x component of the vector.
+const x = (v) => v[1];
+
+// adds two vectors together and returns the result.
+const add = (v1, v2) => vector2(y(v1) + y(v2), x(v1) + x(v2));
+
+// returns an iterator which will infinitely circle between the items of the array.
+const circularIterator = function* (arr) {
+  let i = 0;
+  while (true) {
+    yield arr[i++ % arr.length];
+  }
+};
+
+// returns an iterator which infinitely moves in a clockwise spiral pattern from an origin position of (0,0)
+const spiralIterator = function* () {
+  // clockwise movement: right, down, left, up
+  const directionIterator = circularIterator([
+    vector2(0, 1),
+    vector2(1, 0),
+    vector2(0, -1),
+    vector2(-1, 0),
+  ]);
+  let position = vector2(0, 0);
+  let direction = directionIterator.next().value;
+  let movedDistance = 0;
+  let targetDistance = 1;
+  let turns = 0;
+  while (true) {
+    yield position;
+    position = add(position, direction);
+    // turn to face new direction when traveled far enough
+    if (++movedDistance >= targetDistance) {
+      direction = directionIterator.next().value;
+      movedDistance = 0;
+      // spiral pattern means we move further every two times we turn
+      if (++turns % 2 === 0) {
+        targetDistance++;
+      }
+    }
+  }
+};
+
+// returns a function which returns true if the vector is contained within a 2d matrix of size (rows x cols)
+const inBoundsChecker = (rows, cols) => (v) =>
+  y(v) >= 0 && y(v) < rows && x(v) >= 0 && x(v) < cols;
+
 /**
  * @param {number} rows
  * @param {number} cols
@@ -51,4 +104,17 @@
  * @param {number} cStart
  * @return {number[][]}
  */
-export const spiralMatrixIII = (rows, cols, rStart, cStart) => {};
+export const spiralMatrixIII = (rows, cols, rStart, cStart) => {
+  const result = [];
+  const target = rows * cols;
+  const origin = vector2(rStart, cStart);
+  const spiralWalker = spiralIterator();
+  const inBounds = inBoundsChecker(rows, cols);
+  while (result.length < target) {
+    const position = add(origin, spiralWalker.next().value);
+    if (inBounds(position)) {
+      result.push(position);
+    }
+  }
+  return result;
+};
