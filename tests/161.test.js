a2bd8fc2f6a8b884e95d997029351a2531edaf14
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-09 14:51:03 -0700

    touch 161

---
+++ tests/161.test.js
@@ -0,0 +1,15 @@
+import { isOneEditDistance } from '../src/161.js';
+import { generateTestName } from './util.js';
+
+describe('161. One Edit Distance', () => {
+  [
+    ['ab', 'acb', true],
+    ['', '', false],
+  ].forEach((args) => {
+    const [s, t, expected] = args;
+    test(generateTestName(isOneEditDistance, ...args), () => {
+      const result = isOneEditDistance(s, t);
+      expect(result).toBe(expected);
+    });
+  });
+});
