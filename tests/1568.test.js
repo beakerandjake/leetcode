b417b4f1d8c8a584e5b16bb7bb1de33b722025a5
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-11 09:20:00 -0700

    touch 1568

---
+++ tests/1568.test.js
@@ -0,0 +1,22 @@
+import { minDays } from '../src/1568.js';
+import { generateTestName } from './util.js';
+
+describe('1568. Minimum Number of Days to Disconnect Island', () => {
+  [
+    [
+      [
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+      ],
+      2,
+    ],
+    [[[1, 1]], 2],
+  ].forEach((args) => {
+    const [grid, expected] = args;
+    test(generateTestName(minDays, ...args), () => {
+      const result = minDays(grid);
+      expect(result).toBe(expected);
+    });
+  });
+});
