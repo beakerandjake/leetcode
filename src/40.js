6fec4fae95ff7dd533b709e91fbe855eacd18ebc
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-13 09:49:11 -0700

    touch 40

--- src/40.js
+++ src/40.js
@@ -1,7 +1,49 @@
 /**
- * Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+ * Given a collection of candidate numbers (candidates) and a target number
+ * (target), find all unique combinations in candidates where the candidate numbers
+ * sum to target.
+ *
  * Each number in candidates may only be used once in the combination.
+ *
  * Note: The solution set must not contain duplicate combinations.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: candidates = [10,1,2,7,6,1,5], target = 8
+ * Output:
+ * [
+ * [1,1,6],
+ * [1,2,5],
+ * [1,7],
+ * [2,6]
+ * ]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: candidates = [2,5,2,1,2], target = 5
+ * Output:
+ * [
+ * [1,2,2],
+ * [5]
+ * ]
+ *
+ *
+ *
+ *
+ * Constraints:
+ *
+ *  * 1 <= candidates.length <= 100
+ *  * 1 <= candidates[i] <= 50
+ *  * 1 <= target <= 30
+ *
+ *
+ *
+ * https://leetcode.com/problems/combination-sum-ii
  */
 
 /**
@@ -9,28 +51,4 @@
  * @param {number} target
  * @return {number[][]}
  */
-export const combinationSum2 = (candidates, target) => {
-  const combinations = [];
-  const sortedCandidates = [...candidates].sort((a, b) => a - b);
-  const recursive = (combination, sum, startIndex) => {
-    if (sum === target) {
-      combinations.push([...combination]);
-      return;
-    }
-
-    for (let i = startIndex; i < sortedCandidates.length; i++) {
-      // skip next if same as current
-      if (i > startIndex && sortedCandidates[i] === sortedCandidates[i - 1]) {
-        continue;
-      }
-      const newSum = sum + sortedCandidates[i];
-      if (newSum <= target) {
-        combination.push(sortedCandidates[i]);
-        recursive(combination, newSum, i + 1);
-        combination.pop();
-      }
-    }
-  };
-  recursive([], 0, 0);
-  return combinations;
-};
+export const combinationSum2 = (candidates, target) => {};
