771a5df642ddd9f95f0fcfe26998640ccc53a37d
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-14 07:06:42 -0700

    touch 719

---
+++ tests/719.test.js
@@ -0,0 +1,16 @@
+import { smallestDistancePair } from '../src/719.js';
+import { generateTestName } from './util.js';
+
+describe('719. Find K-th Smallest Pair Distance', () => {
+  [
+    [[1, 3, 1], 1, 0],
+    [[1, 1, 1], 2, 0],
+    [[1, 6, 1], 3, 5],
+  ].forEach((args) => {
+    const [nums, k, expected] = args;
+    test(generateTestName(smallestDistancePair, ...args), () => {
+      const result = smallestDistancePair(nums, k);
+      expect(result).toBe(expected);
+    });
+  });
+});
