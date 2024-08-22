3020a623bf667b77d603f4d9292c75272f0a55c5
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-22 06:57:23 -0700

    touch 476

---
+++ tests/476.test.js
@@ -0,0 +1,15 @@
+import { findComplement } from '../src/476.js';
+import { generateTestName } from './util.js';
+
+describe('476. Number Complement', () => {
+  [
+    [5, 2],
+    [1, 0],
+  ].forEach((args) => {
+    const [num, expected] = args;
+    test(generateTestName(findComplement, ...args), () => {
+      const result = findComplement(num);
+      expect(result).toBe(expected);
+    });
+  });
+});
