66d22747996f9bcb1385a063fe237e7783d0174b
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-23 10:23:40 -0700

    solve 592

--- src/592.js
+++ src/592.js
@@ -51,8 +51,43 @@
  * https://leetcode.com/problems/fraction-addition-and-subtraction
  */
 
+// returns the greatest common divisor between the two numbers
+const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+
+// constructs a new fraction from the numerator and denominator
+const fraction = (numer, denom) => {
+  const g = Math.abs(gcd(numer, denom));
+  return [numer / g, denom / g];
+};
+
+// returns the numerator of the fraction
+const numer = (f) => f[0];
+
+// returns the denominator of the fraction
+const denom = (f) => f[1];
+
+// parse the raw expression string and returns an array of fractions
+const parse = (expression) =>
+  expression.match(/([+-]{1}\d+\/\d+)/g).reduce((acc, m) => {
+    const sign = m[0] === '+' ? 1 : -1;
+    const [n, d] = m.slice(1).split('/');
+    acc.push(fraction(Number(n) * sign, Number(d)));
+    return acc;
+  }, []);
+
+// adds the two fractions together and returns a new fraction
+const add = (a, b) =>
+  fraction(numer(a) * denom(b) + numer(b) * denom(a), denom(a) * denom(b));
+
+// converts the fraction to its string representation
+const toStr = (f) => `${numer(f)}/${denom(f)}`;
+
 /**
  * @param {string} expression
  * @return {string}
  */
-export const fractionAddition = (expression) => {};
+export const fractionAddition = (expression) => {
+  // append a leading + sign if the first fraction in the expression is positive.
+  const fractions = parse(expression.startsWith('-') ? expression : `+${expression}`);
+  return toStr(fractions.reduce((acc, f) => add(acc, f), fraction(0, 1)));
+};
--- tests/592.test.js
+++ tests/592.test.js
@@ -6,6 +6,8 @@ describe('592. Fraction Addition and Subtraction', () => {
     ['-1/2+1/2', '0/1'],
     ['-1/2+1/2+1/3', '1/3'],
     ['1/3-1/2', '-1/6'],
+    ['10/2+15/3', '10/1'],
+    ['7/3+5/2-3/10', '68/15'],
   ].forEach((args) => {
     const [expression, expected] = args;
     test(generateTestName(fractionAddition, ...args), () => {
