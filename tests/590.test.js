2f47b7421e0f7f95a58cc0821700a02503384677
beakerandjake <beakerandjake@users.noreply.github.com>
2024-08-26 07:03:04 -0700

    touch 590

---
+++ tests/590.test.js
@@ -0,0 +1,47 @@
+import { postorder } from '../src/590.js';
+import { generateTestName } from './util.js';
+
+describe('590. N-ary Tree Postorder Traversal', () => {
+  [
+    [
+      [1, null, 3, 2, 4, null, 5, 6],
+      [5, 6, 3, 2, 4, 1],
+    ],
+    [
+      [
+        1,
+        null,
+        2,
+        3,
+        4,
+        5,
+        null,
+        null,
+        6,
+        7,
+        null,
+        8,
+        null,
+        9,
+        10,
+        null,
+        null,
+        11,
+        null,
+        12,
+        null,
+        13,
+        null,
+        null,
+        14,
+      ],
+      [2, 6, 14, 11, 7, 3, 12, 8, 4, 13, 9, 10, 5, 1],
+    ],
+  ].forEach((args) => {
+    const [root, expected] = args;
+    test(generateTestName(postorder, ...args), () => {
+      const result = postorder(root);
+      expect(result).toEqual(expected);
+    });
+  });
+});
