c9018c795f3d56036c6981b9c026000f05b15421
beakerandjake <beakerandjake@users.noreply.github.com>
2024-09-19 08:13:54 -0700

    touch 241

---
+++ tests/241.test.js
@@ -0,0 +1,15 @@
+import { diffWaysToCompute } from '../src/241.js';
+import { generateTestName } from './util.js';
+
+describe('241. Different Ways to Add Parentheses', () => {
+  [
+    ['2-1-1', [0, 2]],
+    ['2*3-4*5', [-34, -14, -10, -10, 10]],
+  ].forEach((args) => {
+    const [expression, expected] = args;
+    test(generateTestName(diffWaysToCompute, ...args), () => {
+      const result = diffWaysToCompute(expression);
+      expect(result).toEqual(expected);
+    });
+  });
+});
