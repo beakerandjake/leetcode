e4f2fedfab798a3659eb39f898aa659f2590ac55
beakerandjake <beakerandjake@users.noreply.github.com>
2024-09-30 07:39:01 -0700

    add missing test

--- tests/432.test.js
+++ tests/432.test.js
@@ -2,11 +2,20 @@ import { AllOne } from '../src/432.js';
 import { generateTestName } from './util.js';
 
 describe('432. All O`one Data Structure', () => {
-  [].forEach((args) => {
-    const [, expected] = args;
+  [
+    [
+      ['inc', 'inc', 'getMaxKey', 'getMinKey', 'inc', 'getMaxKey', 'getMinKey'],
+      ['hello', 'hello', undefined, undefined, 'leet', undefined, undefined],
+      [undefined, undefined, 'hello', 'hello', undefined, 'hello', 'leet'],
+    ],
+  ].forEach((args) => {
+    const [fns, inputs, expected] = args;
     test(generateTestName(AllOne, ...args), () => {
-      const result = AllOne();
-      expect(result).toBe(expected);
+      const subject = new AllOne();
+      fns.forEach((fn, i) => {
+        const result = subject[fn](inputs[i]);
+        expect(result).toBe(expected[i]);
+      });
     });
   });
 });
