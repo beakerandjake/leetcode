0a73d76df5c416d9cef4eb9f6f1b0e3e46012261
beakerandjake <beakerandjake@users.noreply.github.com>
2024-10-06 09:11:39 -0700

    touch 1813

---
+++ tests/1813.test.js
@@ -0,0 +1,16 @@
+import { areSentencesSimilar } from '../src/1813.js';
+import { generateTestName } from './util.js';
+
+describe('1813. Sentence Similarity III', () => {
+  [
+    ['My name is Haley', 'My Haley', true],
+    ['of', 'A lot of words', false],
+    ['Eating right now', 'Eating', true],
+  ].forEach((args) => {
+    const [sentence1, sentence2, expected] = args;
+    test(generateTestName(areSentencesSimilar, ...args), () => {
+      const result = areSentencesSimilar(sentence1, sentence2);
+      expect(result).toBe(expected);
+    });
+  });
+});
