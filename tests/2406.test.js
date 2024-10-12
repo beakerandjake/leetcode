6573926a278c16334679a795dd5a2a88ad7ef483
beakerandjake <beakerandjake@users.noreply.github.com>
2024-10-12 05:04:55 -0700

    touch 2406

---
+++ tests/2406.test.js
@@ -0,0 +1,32 @@
+import { minGroups } from '../src/2406.js';
+import { generateTestName } from './util.js';
+
+describe('2406. Divide Intervals Into Minimum Number of Groups', () => {
+  [
+    [
+      [
+        [5, 10],
+        [6, 8],
+        [1, 5],
+        [2, 3],
+        [1, 10],
+      ],
+      3,
+    ],
+    [
+      [
+        [1, 3],
+        [5, 6],
+        [8, 10],
+        [11, 13],
+      ],
+      1,
+    ],
+  ].forEach((args) => {
+    const [intervals, expected] = args;
+    test(generateTestName(minGroups, ...args), () => {
+      const result = minGroups(intervals);
+      expect(result).toBe(expected);
+    });
+  });
+});
